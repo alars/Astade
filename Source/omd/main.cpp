8f9eedc431a3311ffd1125a6ef49c7c2a3b9291e
Thomas Spitzer <projektleiter@astade.de>
2005-06-17 19:34:15 +0000

    initial version for omd
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@22 aaa30fad-34f9-0310-8aa9-877cc762ee2e

---
+++ Source/omd/main.cpp
@@ -0,0 +1,180 @@
+#include <cstdlib>
+#include <iostream>
+#include <wx/string.h>
+#include <wx/dir.h>
+#include <wx/filename.h>
+#include <wx/utils.h>
+#include "../treeview/AstadeDef.h"
+#include <map>
+
+
+using namespace std;
+std::map<wxString,bool> nodelist;
+
+void Listnodes(int depth, const char* Parentname,const char* pathname)
+{
+    wxFileName dirname(pathname);
+    dirname.SetFullName("Desktop.ini");
+    int type=0;
+    wxGetResource("Astade","Type",&type,dirname.GetFullPath());
+    if ((0xFF00000 & type) == ITEM_IS_CLASS) 
+    {
+        wxChar* name = new wxChar[200];
+        wxGetResource("Astade","Name", &name, dirname.GetFullPath());
+        wxString prename;
+        
+        if (strlen(Parentname))
+        {
+            prename = Parentname;
+            prename = prename +":"+name;
+        }
+        else
+           prename = name; 
+
+        for (int i=0;i<depth;++i)
+            printf("\t");
+            
+        wxString nodename = dirname.GetDirs()[dirname.GetDirCount()-1];
+        nodelist[nodename] = true;
+        
+        printf("%s [label=\"%s\", style=filled, fillcolor=grey95, color=black];\n",dirname.GetDirs()[dirname.GetDirCount()-1].c_str(),prename.c_str());
+           
+        delete [] name;
+        
+        wxString filename;
+        wxDir dir(dirname.GetPath());
+
+        bool cont = dir.GetFirst(&filename, "*.*", wxDIR_DIRS);
+        while ( cont )
+        {
+            wxFileName FullName = dirname;
+            FullName.AppendDir(filename);
+            Listnodes(depth,prename.c_str(),FullName.GetFullPath().c_str());
+            cont = dir.GetNext(&filename);
+        }
+    }    
+    
+    if ((0xFF00000 & type) == ITEM_IS_CLASSES) 
+    {
+        wxString filename;
+        wxDir dir(dirname.GetPath());
+    
+        bool cont = dir.GetFirst(&filename, "*.*", wxDIR_DIRS);
+        while ( cont )
+        {
+            wxFileName FullName = dirname;
+            FullName.AppendDir(filename);
+            Listnodes(depth,Parentname,FullName.GetFullPath().c_str());
+            cont = dir.GetNext(&filename);
+        }
+    }    
+
+    if ((0xFF00000 & type) == ITEM_IS_PACKAGE) 
+    {
+        wxString filename;
+        wxDir dir(dirname.GetPath());
+    
+        for (int i=0;i<depth;++i)
+            printf("\t");
+            
+        printf("subgraph cluster%s {\n",dirname.GetDirs()[dirname.GetDirCount()-1].c_str());
+
+        wxChar* name = new wxChar[200];
+        wxGetResource("Astade","Name", &name, dirname.GetFullPath());
+        for (int i=0;i<depth+1;++i)
+            printf("\t");
+        printf("label = \"Package: %s\", fontname=arial, fontsize=10, color=red\n",name);
+            
+        delete [] name;
+        
+        bool cont = dir.GetFirst(&filename, "*.*", wxDIR_DIRS);
+        while ( cont )
+        {
+            wxFileName FullName = dirname;
+            FullName.AppendDir(filename);
+            Listnodes(depth+1,Parentname,FullName.GetFullPath().c_str());
+            cont = dir.GetNext(&filename);
+        }
+        for (int i=0;i<depth;++i)
+            printf("\t");
+            
+        printf("}\n");
+     }    
+}    
+void ListEdges(const char* inClass, const char* pathname)
+{
+    wxFileName dirname(pathname);
+    dirname.SetFullName("Desktop.ini");
+    int type=0;
+    wxGetResource("Astade","Type",&type,dirname.GetFullPath());
+    if (((0xFF00000 & type) == ITEM_IS_CLASS) ||
+        ((0xFF00000 & type) == ITEM_IS_CLASSES) ||
+        ((0xFF00000 & type) == ITEM_IS_PACKAGE) )
+         
+    {
+        wxString activeElement = dirname.GetDirs()[dirname.GetDirCount()-1];
+               
+        wxString filename;
+        wxDir dir(dirname.GetPath());
+        bool cont = dir.GetFirst(&filename, "*.*", wxDIR_DIRS);
+        while ( cont )
+        {
+            wxFileName FullName = dirname;
+            FullName.AppendDir(filename);
+            ListEdges(activeElement.c_str(),FullName.GetFullPath().c_str());
+            cont = dir.GetNext(&filename);
+        }
+    }    
+   
+    if ((0xFF00000 & type) == ITEM_IS_RELATIONS)
+    {
+        wxString filename;
+        wxDir dir(dirname.GetPath());
+        bool cont = dir.GetFirst(&filename, "*.ini");
+        while ( cont )
+        {
+            wxFileName FullName = dirname;
+            FullName.SetFullName(filename);
+            wxChar* name = new wxChar[200];
+            wxGetResource("Astade","Type",&type,FullName.GetFullPath());
+            if ((0xFF00000 & type) == ITEM_IS_RELATION)
+            {
+                wxChar* name = new wxChar[2000];
+                wxGetResource("Relation","PartnerPath",&name,FullName.GetFullPath());
+                wxFileName PartnerDir(name);
+                if (nodelist.find(PartnerDir.GetDirs()[PartnerDir.GetDirCount()-2])==nodelist.end())
+                {
+                    wxFileName partnerName = PartnerDir;
+                    int i = partnerName.GetDirCount();
+                    partnerName.RemoveDir(i-1);
+                    partnerName.SetName("Desktop"); 
+                    partnerName.SetExt("ini");
+                    wxGetResource("Astade","Name", &name, partnerName.GetFullPath());
+                          
+                    printf("%s [label=\"%s\", color=black];\n",PartnerDir.GetDirs()[PartnerDir.GetDirCount()-2].c_str(),name);
+                }    
+                printf("\"%s\" -> \"%s\" [color=blue, style=dashed, arrowhead=vee];\n",inClass,PartnerDir.GetDirs()[PartnerDir.GetDirCount()-2].c_str());
+                delete [] name;
+            }    
+            cont = dir.GetNext(&filename);
+        }
+    }    
+}    
+
+int main(int argc, char *argv[])
+{
+    if (argc!=2)
+    {
+        printf("Call the programm with the target dir:\n");
+        printf("OMD <path>\n");
+    } 
+    else
+    {
+        printf("digraph G {\n");
+        printf("\tnode [shape=box, fontname=arial, fontsize=12]\n");
+        Listnodes(1,"",argv[1]);
+        ListEdges("",argv[1]);
+        printf("}\n");
+    }       
+    return EXIT_SUCCESS;
+}
