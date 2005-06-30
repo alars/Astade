98495daa4aa78a8845bad77456ed112567dc11b2
Thomas Spitzer <projektleiter@astade.de>
2005-06-30 16:10:32 +0000

    Begin of API Model added
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@41 aaa30fad-34f9-0310-8aa9-877cc762ee2e

---
+++ Model/package_SVSWQPEV/package_OPTPAIKK/package_CISPFMDB/package_UPBJDSOK/classes/class_NQRUBFXN/AdeModelElement.cpp
@@ -0,0 +1,16 @@
+//******************************************************
+//** Coding is done with Astade Quick and Dirty Coder **
+//** Filename: AdeModelElement.cpp                    **
+//******************************************************
+
+#include AdeModelElement.h // own header
+
+
+
+int	AdeModelElement::Type()
+{
+}
+
+wxSring	AdeModelElement::GetFeature()
+{
+}
---
+++ Model/package_SVSWQPEV/package_OPTPAIKK/package_CISPFMDB/package_UPBJDSOK/classes/class_NQRUBFXN/AdeModelElement.h
@@ -0,0 +1,23 @@
+//******************************************************
+//** Coding is done with Astade Quick and Dirty Coder **
+//** Filename: AdeModelElement.h                      **
+//******************************************************
+
+#ifndef ADEMODELELEMENT_H
+#define ADEMODELELEMENT_H
+
+
+class AdeModelElement
+{
+	public:
+	int	Type();
+
+	protected:
+
+	// Relations:
+	wxSring	GetFeature();
+
+	private:
+};
+
+#endif
