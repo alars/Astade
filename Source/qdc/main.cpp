bdb2d3f5b6e1d6f6fc0bd078322f14f783011911
Thomas Spitzer <projektleiter@astade.de>
2005-08-04 20:16:37 +0000

    Now it should compile in windows and linux
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@86 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Source/qdc/main.cpp
+++ Source/qdc/main.cpp
@@ -10,11 +10,12 @@
 #include <wx/cmdline.h>
 #include "../treeview/AstadeDef.h"
 
-// In windows, this doesn't compile
-//WXDLLEXPORT bool wxGetResource(const wxString& section, const wxString& entry, wxChar **value, const wxString& file = wxEmptyString);
-//WXDLLEXPORT bool wxGetResource(const wxString& section, const wxString& entry, float *value, const wxString& file = wxEmptyString);
-//WXDLLEXPORT bool wxGetResource(const wxString& section, const wxString& entry, long *value, const wxString& file = wxEmptyString);
-//WXDLLEXPORT bool wxGetResource(const wxString& section, const wxString& entry, int *value, const wxString& file = wxEmptyString);
+#ifdef __WXGTK__
+WXDLLEXPORT bool wxGetResource(const wxString& section, const wxString& entry, wxChar **value, const wxString& file = wxEmptyString);
+WXDLLEXPORT bool wxGetResource(const wxString& section, const wxString& entry, float *value, const wxString& file = wxEmptyString);
+WXDLLEXPORT bool wxGetResource(const wxString& section, const wxString& entry, long *value, const wxString& file = wxEmptyString);
+WXDLLEXPORT bool wxGetResource(const wxString& section, const wxString& entry, int *value, const wxString& file = wxEmptyString);
+#endif
 
 wxString theClassname;
 wxString theAdditionalClasses;
