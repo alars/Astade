8b3036403b670e943e848e35c945eb19cf59d40f
Thomas Spitzer <projektleiter@astade.de>
2005-08-05 05:41:21 +0000

    #ifdef __WXGTK__
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@87 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Source/omd/main.cpp
+++ Source/omd/main.cpp
@@ -10,11 +10,12 @@
 
 #include "../treeview/AstadeDef.h"
 
-// in Windows this doesn't compile
-//WXDLLEXPORT bool wxGetResource(const wxString& section, const wxString& entry, wxChar **value, const wxString& file = wxEmptyString);
-//WXDLLEXPORT bool wxGetResource(const wxString& section, const wxString& entry, float *value, const wxString& file = wxEmptyString);
-//WXDLLEXPORT bool wxGetResource(const wxString& section, const wxString& entry, long *value, const wxString& file = wxEmptyString);
-//WXDLLEXPORT bool wxGetResource(const wxString& section, const wxString& entry, int *value, const wxString& file = wxEmptyString);
+#ifdef __WXGTK__
+WXDLLEXPORT bool wxGetResource(const wxString& section, const wxString& entry, wxChar **value, const wxString& file = wxEmptyString);
+WXDLLEXPORT bool wxGetResource(const wxString& section, const wxString& entry, float *value, const wxString& file = wxEmptyString);
+WXDLLEXPORT bool wxGetResource(const wxString& section, const wxString& entry, long *value, const wxString& file = wxEmptyString);
+WXDLLEXPORT bool wxGetResource(const wxString& section, const wxString& entry, int *value, const wxString& file = wxEmptyString);
+#endif
 
 std::map<wxString,bool> nodelist;
 
