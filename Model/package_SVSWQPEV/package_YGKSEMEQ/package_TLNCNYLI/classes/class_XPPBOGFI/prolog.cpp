b93478b8fefeecf9b49882a042cb975b8a522132
Anders Larsen <al@alarsen.net>
2005-09-14 22:16:34 +0000

    use wxID_CANCEL instead of IDCANCEL
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@161 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/package_SVSWQPEV/package_YGKSEMEQ/package_TLNCNYLI/classes/class_XPPBOGFI/prolog.cpp
+++ Model/package_SVSWQPEV/package_YGKSEMEQ/package_TLNCNYLI/classes/class_XPPBOGFI/prolog.cpp
@@ -1,7 +1,7 @@
 #include "AstadeDirDialog.h"
 BEGIN_EVENT_TABLE(AstadeDirDialog,wxDialog)
 	EVT_CLOSE(AstadeDirDialog::DlgClose)
-	EVT_BUTTON(IDCANCEL, AstadeDirDialog::DlgCancel)
+	EVT_BUTTON(wxID_CANCEL, AstadeDirDialog::DlgCancel)
 	EVT_BUTTON(IDSAVEEXIT, AstadeDirDialog::DlgSaveAndExit)
 	EVT_BUTTON(IDMODELPATHBROWSE, AstadeDirDialog::ModelPathBrowse)
 END_EVENT_TABLE()
