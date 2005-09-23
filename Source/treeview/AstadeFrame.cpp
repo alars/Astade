e3540538072f1e46519ed731b104e19a3a224653
Anders Larsen <al@alarsen.net>
2005-09-23 21:38:12 +0000

    GUID generation corrected
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@190 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Source/treeview/AstadeFrame.cpp
+++ Source/treeview/AstadeFrame.cpp
@@ -2264,7 +2264,7 @@ wxString AstadeFrame::GUID()
     char range = 'Z' - 'A' + 1;
     for (int i=0;i<8;i++)
     {
-        char c = 'A' + char(range * rand()/(RAND_MAX + 1.0));
+        char c = 'A' + static_cast<char>(range * (rand()/(RAND_MAX + 1.0)));
         s = s + c;
     }        
     return s;
