c77824bc99eb8fd5086249dd736c94a9744fbed9
Thomas Spitzer <projektleiter@astade.de>
2005-10-02 11:03:28 +0000

    git-svn-id: http://astade.tigris.org/svn/astade/trunk@223 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/package_SVSWQPEV/package_YGKSEMEQ/package_TLNCNYLI/classes/class_ODHKSPMR/operations/operation_EVWEOBRR/code.cpp
+++
@@ -1,6 +0,0 @@
-myToolBar = CreateToolBar();
-
-regenerateButton = new wxButton(myToolBar, ID_REGENERATE,  "Regenerate all" ,wxPoint(1,1),wxSize(180,20));
-
-myToolBar->AddControl(regenerateButton);
-myToolBar->Realize();
