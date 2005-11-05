1da65557b0f178bba5f49f4daf1df52a34aae570
Thomas Spitzer <projektleiter@astade.de>
2005-11-05 12:33:08 +0000

    changed the website - a description, where Astade is different
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@271 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- www/Download/omd.bat
+++
@@ -1,5 +0,0 @@
-"F:\Dokumente und Einstellungen\Thomas\Eigene Dateien\Astade\Source\omd\omd.exe" %1 > "%TMP%\omd.dot"
-dot "%TMP%\omd.dot" -Tpng -o"%TMP%\omd.png"
-"%TMP%\omd.png"
-del "%TMP%\omd.png"
-del "%TMP%\omd.dot"
