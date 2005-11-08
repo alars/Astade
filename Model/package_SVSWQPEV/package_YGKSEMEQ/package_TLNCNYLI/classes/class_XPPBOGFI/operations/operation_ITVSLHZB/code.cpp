215abcbacbc428508cc7d80ef33dfd03c9e63b6e
Anders Larsen <al@alarsen.net>
2005-11-08 20:49:20 +0000

    Wildcards in file dialogues fixed - again
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@275 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/package_SVSWQPEV/package_YGKSEMEQ/package_TLNCNYLI/classes/class_XPPBOGFI/operations/operation_ITVSLHZB/code.cpp
+++ Model/package_SVSWQPEV/package_YGKSEMEQ/package_TLNCNYLI/classes/class_XPPBOGFI/operations/operation_ITVSLHZB/code.cpp
@@ -1,7 +1,7 @@
 wxConfigBase* theConfig = wxConfigBase::Get();
 wxFileName theFileName(theConfig->Read("Tools/Make"));
 
-const wxString& dir = wxFileSelector("Set make",theFileName.GetPath(),theFileName.GetFullName(),"","*.*",0,this);
+const wxString& dir = wxFileSelector("Set make",theFileName.GetPath(),theFileName.GetFullName(),"","*",0,this);
 
 wxFileName filename(dir);
 
--- Model/package_SVSWQPEV/package_YGKSEMEQ/package_TLNCNYLI/classes/class_XPPBOGFI/operations/operation_KJGUJSLW/code.cpp
+++ Model/package_SVSWQPEV/package_YGKSEMEQ/package_TLNCNYLI/classes/class_XPPBOGFI/operations/operation_KJGUJSLW/code.cpp
@@ -1,7 +1,7 @@
 wxConfigBase* theConfig = wxConfigBase::Get();
 wxFileName theFileName(theConfig->Read("TreeView/Helpfile"));
 
-const wxString& dir = wxFileSelector("Set Helpfile",theFileName.GetPath(),theFileName.GetFullName(),"","*.*",0,this);
+const wxString& dir = wxFileSelector("Set Helpfile",theFileName.GetPath(),theFileName.GetFullName(),"","*.zip",0,this);
 wxFileName filename(dir);
 
 if ( !dir.empty() )
