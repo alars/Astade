31b33c597a1c7da77f464fd6a76f285fb56b45de
Thomas Spitzer <projektleiter@astade.de>
2005-11-18 19:05:51 +0000

    git-svn-id: http://astade.tigris.org/svn/astade/trunk@293 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/package_SVSWQPEV/package_OPTPAIKK/package_CISPFMDB/package_JNUGYMDI/classes/class_VBCBPOYC/operations/operation_ZJTAISXK/code.cpp
+++
@@ -1,8 +0,0 @@
-wxConfigBase* theConfig = wxConfigBase::Get();
-wxFileName activeComponentName = theConfig->Read("TreeView/ActiveComponent");
-
-activeComponentName.AppendDir("auto");
-activeComponentName.SetName(GetName());
-activeComponentName.SetExt("cpp");
-
-return activeComponentName;
