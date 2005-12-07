68b631703d7a41be6d60e8f1be85887ff8ebcfd9
Thomas Spitzer <projektleiter@astade.de>
2005-12-07 19:22:01 +0000

    State chart coder is growing.
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@331 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/package_SVSWQPEV/package_YGKSEMEQ/Package_DAJEXPHO/classes/class_ZTLSKJBT/operations/operation_PEOJXPMX/code.cpp
+++ Model/package_SVSWQPEV/package_YGKSEMEQ/Package_DAJEXPHO/classes/class_ZTLSKJBT/operations/operation_PEOJXPMX/code.cpp
@@ -5,8 +5,29 @@ if (!event.empty())
 	fprintf(implementationFile,"\t// %s\n",theTransition.GetLabel().c_str());
 	fprintf(implementationFile,"\tif (itsID == ID_%s)\n\t{\n",event.c_str());
 
-	fprintf(implementationFile,"\t\tEnterState(theEvent);\n");
-	fprintf(implementationFile,"\t\treturn true;\n");
+	std::set<wxString> aSet = theTransition.GetActions();
 
+	if (!aSet.empty())
+		fprintf(implementationFile,"\t\t// Actions\n");
+
+	for (std::set<wxString>::iterator iter = aSet.begin(); iter!=aSet.end(); iter++)
+		fprintf(implementationFile,"\t\t%s(theEvent);\n",(*iter).c_str());
+
+	wxString nextState = theTransition.GetDestination();
+
+	if (!nextState.empty())
+	{
+		if (!theState.GetExitAction().empty())
+		{
+			fprintf(implementationFile,"\t\t// exit action\n");
+			fprintf(implementationFile,"\t\t%s(theEvent);\n",theState.GetExitAction().c_str());
+		}
+		fprintf(implementationFile,"\t\t// next state\n");
+		fprintf(implementationFile,"\t\tnextState = &%s::Enter_%s;\n",theStatechart.GetName().c_str(),nextState.c_str());
+	}
+	else
+		fprintf(implementationFile,"\t\t// internal state\n");
+
+	fprintf(implementationFile,"\t\treturn true;\n");
 	fprintf(implementationFile,"\t}\n\telse\n");
 }
