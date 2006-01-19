5772110e30f70eba74dcd6f015d5c73138897f38
Thomas Spitzer <projektleiter@astade.de>
2006-01-19 17:03:48 +0000

    A new dialog for changing the model directory
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@395 aaa30fad-34f9-0310-8aa9-877cc762ee2e

---
+++ Model/package_SVSWQPEV/package_YGKSEMEQ/package_TLNCNYLI/classes/class_ODHKSPMR/operations/operation_APQCJYTZ/code.cpp
@@ -0,0 +1,18 @@
+wxConfigBase* theConfig = wxConfigBase::Get();
+const wxString& dir = wxDirSelector("Set Modelpath",theConfig->Read("TreeView/ModelPath"));
+wxFileName filename(dir);
+
+if ( !dir.empty() )
+{
+	wxConfigBase* theConfig = wxConfigBase::Get();
+	theConfig->Write("TreeView/ModelPath",filename.GetFullPath());
+	theConfig->Write("TreeView/ActiveComponent","none");
+	theConfig->Flush();
+	delete myTree;
+	myTree =  new AstadeTree(this);
+
+	int x,y;
+	GetSize(&x,&y);
+	SetSize(x-1,y);
+	SetSize(x,y);
+}
--- Model/package_SVSWQPEV/package_YGKSEMEQ/package_TLNCNYLI/classes/class_ODHKSPMR/operations/operation_WEHKZHEU/code.cpp
+++ Model/package_SVSWQPEV/package_YGKSEMEQ/package_TLNCNYLI/classes/class_ODHKSPMR/operations/operation_WEHKZHEU/code.cpp
@@ -1,3 +1,6 @@
+wxMenu* aFileMenu = new wxMenu(0);
+aFileMenu->Append(ID_SELECTMODEL,"select model directory","", wxITEM_NORMAL);
+
 wxMenu* aCustomizeMenu = new wxMenu(0);
 aCustomizeMenu->Append(ID_SETEDITORS,"set directories","", wxITEM_NORMAL);
 
@@ -6,5 +9,6 @@ aHelpMenu->Append(ID_MNU_HELP,"help");
 aHelpMenu->Append(ID_MNU_INFO,"info");
 aHelpMenu->Append(ID_MNU_ABOUT,"about");
 
+myMenuBar.Append(aFileMenu,"Model");
 myMenuBar.Append(aCustomizeMenu,"Customize");
 myMenuBar.Append(aHelpMenu,"Help");
