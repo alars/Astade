706bc2762988fa039ca9cf651e191b936562497a
Thomas Spitzer <projektleiter@astade.de>
2006-01-23 19:33:17 +0000

    Added the dialog for selecting a revision control. No functionality, yet.
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@399 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/package_SVSWQPEV/package_YGKSEMEQ/package_TLNCNYLI/classes/class_ODHKSPMR/operations/operation_WEHKZHEU/code.cpp
+++ Model/package_SVSWQPEV/package_YGKSEMEQ/package_TLNCNYLI/classes/class_ODHKSPMR/operations/operation_WEHKZHEU/code.cpp
@@ -1,8 +1,15 @@
+wxMenu* aRevisionControlMenu = new wxMenu(0);
+aRevisionControlMenu->AppendRadioItem(ID_SELECTNONE,"none");
+aRevisionControlMenu->AppendRadioItem(ID_SELECTSVN,"SVN");
+aRevisionControlMenu->AppendRadioItem(ID_SELECTCVS,"CVS");
+aRevisionControlMenu->Enable(ID_SELECTCVS,false);
+
 wxMenu* aFileMenu = new wxMenu(0);
 aFileMenu->Append(ID_SELECTMODEL,"select model directory","", wxITEM_NORMAL);
 
 wxMenu* aCustomizeMenu = new wxMenu(0);
 aCustomizeMenu->Append(ID_SETEDITORS,"set directories","", wxITEM_NORMAL);
+aCustomizeMenu->Append(-1,"select revision control",aRevisionControlMenu);
 
 wxMenu* aHelpMenu = new wxMenu(0);
 aHelpMenu->Append(ID_MNU_HELP,"help");
