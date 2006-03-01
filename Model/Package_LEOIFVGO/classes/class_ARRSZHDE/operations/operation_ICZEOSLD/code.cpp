676b896963d2aae27bc205dcb6725f532918483d
Thomas Spitzer <projektleiter@astade.de>
2006-03-01 19:47:42 +0000

    use a "save" dialog for saving files
    git-svn-id: http://trace2uml.tigris.org/svn/trace2uml/trunk@36 89f8ffb8-d1fc-0310-92ca-d9a8b2f27bcf

--- Model/Package_LEOIFVGO/classes/class_ARRSZHDE/operations/operation_ICZEOSLD/code.cpp
+++ Model/Package_LEOIFVGO/classes/class_ARRSZHDE/operations/operation_ICZEOSLD/code.cpp
@@ -1,7 +1,7 @@
 wxFileName aFile(currentFile);
 
 //const wxString& dir = wxFileSelector("Save to",aFile.GetPath(),aFile.GetFullName(),"","*.seq",0,this);
-const wxString& dir = wxFileSelector("Save to",aFile.GetPath(),wxEmptyString,"seq","*.seq",wxOVERWRITE_PROMPT,this);
+const wxString& dir = wxFileSelector("Save to",aFile.GetPath(),wxEmptyString,"seq","*.seq",wxOVERWRITE_PROMPT|wxSAVE,this);
 
 if ( !dir.empty() )
 {
