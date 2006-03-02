f5e90c6aba78650341fa961298c64bb430752fab
Thomas Spitzer <projektleiter@astade.de>
2006-03-02 20:11:38 +0000

    There was a bug, when you delete a parameter of an operation and thereafter add one, than the order of the last two parameters could not be changed any more. This bug is fixed.
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@430 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/package_SVSWQPEV/package_OPTPAIKK/package_CISPFMDB/package_JNUGYMDI/classes/class_UCRROVEF/operations/operation_EVWOMLKI/code.cpp
+++ Model/package_SVSWQPEV/package_OPTPAIKK/package_CISPFMDB/package_JNUGYMDI/classes/class_UCRROVEF/operations/operation_EVWOMLKI/code.cpp
@@ -8,9 +8,16 @@ AdeElementIterator end;
 int count=0;
 while (it!=end)
 {
+	AdeModelElement* aElement = it.CreateNewElement();
+	int tmp = 0xff & aElement->GetType();
+	if (tmp>count)
+		count = tmp;
+	delete aElement;
 	++it;
-	++count;
 }
+
+count++;
+count &= 0xff;
 
 theConfig.Write("Astade/Type", ITEM_IS_PARAMETER + count);
 theConfig.Write("Astade/CodingType", "int");
