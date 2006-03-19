4418bf0bace51f1c8dc3b3be4b2176a59e216656
Jonatan Antoni <jantoni@web.de>
2006-03-19 10:28:10 +0000

    fixed bug: sigsegv in AstadeTree::DoUpdateItem
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@493 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/package_SVSWQPEV/package_YGKSEMEQ/package_TLNCNYLI/classes/class_ODHKSPMR/operations/operation_QKQJVTZE/code.cpp
+++ Model/package_SVSWQPEV/package_YGKSEMEQ/package_TLNCNYLI/classes/class_ODHKSPMR/operations/operation_QKQJVTZE/code.cpp
@@ -1,4 +1,13 @@
 RelationStart = wxTreeItemId();
 copySource = wxTreeItemId();
 wxTreeItemId aID = event.GetItem();
+
+// cancel the update process of all children to be delete
+wxTreeItemIdValue aCookie;
+wxTreeItemId aChildID = myTree->GetFirstChild(aID, aCookie);
+while(aChildID.IsOk()) {
+	myTree->CancelUpdate(aChildID);
+	aChildID = myTree->GetNextChild(aID, aCookie);
+}
+
 myTree->DeleteChildren(aID);
---
+++ Model/package_SVSWQPEV/package_YGKSEMEQ/package_TLNCNYLI/classes/class_ZZKPVCBY/operations/operation_NYCJECGL/code.cpp
@@ -0,0 +1,1 @@
+theUpdateItemList.remove(theID);
