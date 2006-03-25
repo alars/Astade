842a1ff50c5efe968868f0df2fa131c337462e71
Thomas Spitzer <projektleiter@astade.de>
2006-03-25 08:33:41 +0000

    You get a "save" dialog, when opening a new file and having something changed.
    git-svn-id: http://trace2uml.tigris.org/svn/trace2uml/trunk@45 89f8ffb8-d1fc-0310-92ca-d9a8b2f27bcf

--- Model/Package_LEOIFVGO/classes/class_ARRSZHDE/operations/operation_FWFUUBDS/code.cpp
+++ Model/Package_LEOIFVGO/classes/class_ARRSZHDE/operations/operation_FWFUUBDS/code.cpp
@@ -1,2 +1,15 @@
+if (textTab->IsModified())
+{
+	int answere = wxMessageDialog(this,"You made changes. Save?","Save Changes?",wxCANCEL | wxYES | wxNO | wxICON_QUESTION).ShowModal();
+
+	wxCommandEvent anEvent;
+
+	if (answere==wxID_YES)
+		Save(anEvent);
+
+	if (answere==wxID_CANCEL)
+		return;
+}
+
 const wxString& dir = wxFileSelector("File to open","","","","*.seq",0,this);
 Load(dir);
