3642e31a62068252cf4c21eece99d18735fec0c0
Thomas Spitzer <projektleiter@astade.de>
2006-04-09 11:45:01 +0000

    regular expressions are used to identify error line.
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@507 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/package_SVSWQPEV/package_YGKSEMEQ/package_TLNCNYLI/classes/class_OJLITQRT/operations/operation_UIEDCSRF/code.cpp
+++ Model/package_SVSWQPEV/package_YGKSEMEQ/package_TLNCNYLI/classes/class_OJLITQRT/operations/operation_UIEDCSRF/code.cpp
@@ -1,1 +1,16 @@
-return true;
+wxConfigBase* theConfig = wxConfigBase::Get();
+
+wxRegEx regEx(theConfig->Read("TreeView/RegEx/identifyExpression"));
+if (!regEx.IsValid())
+{ // regular expression has syntax error
+/*	wxLogError("The regular expression %s has a syntax error",
+		theConfig->Read("TreeView/RegEx/identifyExpression").c_str());
+*/
+	return false;
+}
+
+
+if (regEx.Matches(parseLine.c_str()))
+	return true;
+else
+	return false;
--- Model/package_SVSWQPEV/package_YGKSEMEQ/package_TLNCNYLI/classes/class_ZZKPVCBY/operations/operation_BHVSKYNK/code.cpp
+++ Model/package_SVSWQPEV/package_YGKSEMEQ/package_TLNCNYLI/classes/class_ZZKPVCBY/operations/operation_BHVSKYNK/code.cpp
@@ -1,1 +1,2 @@
+theUpdateItemList.remove(theID); // to avoid, that an id can be in the list more than once.
 theUpdateItemList.push_back(theID);
