e434cd6fa8192b6f080f26b2a8b9190ca208eddd
Thomas Spitzer <projektleiter@astade.de>
2006-08-05 08:06:29 +0000

    Started a tracing framework
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@621 aaa30fad-34f9-0310-8aa9-877cc762ee2e

---
+++ Model/package_SVSWQPEV/Package_OXXWDRJY/Package_HQQBXEUL/classes/class_CFVCKIUW/operations/constructor_UYPIJUCX/code.cpp
@@ -0,0 +1,6 @@
+m_PreviousRunningObject = m_RunningObject;
+m_RunningObject = objectName;
+
+if (notificationType==CALL_NOTIFICATION)
+{
+}
---
+++ Model/package_SVSWQPEV/Package_OXXWDRJY/Package_HQQBXEUL/classes/class_CFVCKIUW/operations/destructor/code.cpp
@@ -0,0 +1,1 @@
+m_RunningObject = m_PreviousRunningObject;
