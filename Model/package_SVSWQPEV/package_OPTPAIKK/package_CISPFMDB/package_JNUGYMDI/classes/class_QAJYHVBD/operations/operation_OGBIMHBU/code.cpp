8142a087fca833b12a27930d9433bf81a0669766
Anders Larsen <al@alarsen.net>
2006-08-23 22:04:02 +0000

    use wxString::c_str() instead of cast
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@645 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/package_SVSWQPEV/package_OPTPAIKK/package_CISPFMDB/package_JNUGYMDI/classes/class_QAJYHVBD/operations/operation_OGBIMHBU/code.cpp
+++ Model/package_SVSWQPEV/package_OPTPAIKK/package_CISPFMDB/package_JNUGYMDI/classes/class_QAJYHVBD/operations/operation_OGBIMHBU/code.cpp
@@ -1,2 +1,2 @@
 wxFileConfig theConfig(wxEmptyString, wxEmptyString, myFileName.GetFullPath());
-return wxString::Format(theConfig.Read("Astade/Declaration"), (const char*)GetName());
+return wxString::Format(theConfig.Read("Astade/Declaration"), GetName().c_str());
