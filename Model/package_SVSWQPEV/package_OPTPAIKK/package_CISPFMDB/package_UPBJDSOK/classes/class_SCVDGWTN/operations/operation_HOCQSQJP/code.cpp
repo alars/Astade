50fafa74783e3bb6e2b333f6baf9e5009a34f3f9
Jonatan Antoni <jantoni@web.de>
2006-10-05 20:51:42 +0000

    fixed more missing files
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@661 aaa30fad-34f9-0310-8aa9-877cc762ee2e

---
+++ Model/package_SVSWQPEV/package_OPTPAIKK/package_CISPFMDB/package_UPBJDSOK/classes/class_SCVDGWTN/operations/operation_HOCQSQJP/code.cpp
@@ -0,0 +1,18 @@
+if(PartnerExists())
+{
+	wxConfigBase* aConfig = wxConfigBase::Get();
+	wxString modelPath = aConfig->Read("TreeView/ModelPath");
+
+	wxFileName toFileName(parentPath.GetPath(), myFileName.GetFullName());
+	toFileName.MakeRelativeTo(modelPath);
+
+	wxFileName partnerFileName = GetPartnerFile();
+	
+
+	/* 
+	 * Save the new location in the Partners ConfigFile, so that 
+	 * the Partner can find us in both places
+ 	 */
+	wxFileConfig partnerConfig(wxEmptyString, wxEmptyString, partnerFileName.GetFullPath());
+	partnerConfig.Write("Astade/NewPartnerPath", toFileName.GetFullPath(wxPATH_UNIX));
+}
