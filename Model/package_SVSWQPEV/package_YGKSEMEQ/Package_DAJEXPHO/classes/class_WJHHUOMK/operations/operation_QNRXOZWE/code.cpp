867ff9db1a68cb76a98bd37faa1ecd43f1e86684
Anders Larsen <al@alarsen.net>
2006-10-05 10:52:12 +0000

    Eliminated some compiler warnings
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@659 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/package_SVSWQPEV/package_YGKSEMEQ/Package_DAJEXPHO/classes/class_WJHHUOMK/operations/operation_QNRXOZWE/code.cpp
+++ Model/package_SVSWQPEV/package_YGKSEMEQ/Package_DAJEXPHO/classes/class_WJHHUOMK/operations/operation_QNRXOZWE/code.cpp
@@ -1,31 +1,31 @@
-fprintf(specificationFile,"\t\t//! \\brief This is the state function for state %s.\n",theState.GetName().c_str());
-fprintf(specificationFile,"\t\tvoid %s(CMessage& message);\n\n",theState.GetName().c_str(),theStatechart.GetEventType().c_str());
+fprintf(specificationFile, "\t\t//! \\brief This is the state function for state %s.\n", theState.GetName().c_str());
+fprintf(specificationFile, "\t\tvoid %s(CMessage& message);\n\n", theState.GetName().c_str());
 
-fprintf(implementationFile,"void %s::%s(CMessage& message)\n{\n",theStatechart.GetName().c_str(),theState.GetName().c_str(),theStatechart.GetEventType().c_str());
+fprintf(implementationFile, "void %s::%s(CMessage& message)\n{\n", theStatechart.GetName().c_str(), theState.GetName().c_str());
 
 AdeElementIterator it;
-for (it=theState.begin();it!=theState.end();++it)
+for (it = theState.begin(); it != theState.end(); ++it)
 {
 	AdeModelElement* aElement = it.CreateNewElement();
-	if ((aElement->GetType() & 0x7F00000) == ITEM_IS_TRANSITION)
+	if ((aElement->GetType() & ITEM_TYPE_MASK) == ITEM_IS_TRANSITION)
 	{
 		AdeTransition* aTransition = static_cast<AdeTransition*>(aElement);
 		if (!aTransition->GetGuard().empty())
-			CodeTransition(theStatechart,theState,*aTransition);
+			CodeTransition(theStatechart, theState, *aTransition);
 	}
 	delete aElement;
 }
 
-for (it=theState.begin();it!=theState.end();++it)
+for (it = theState.begin(); it != theState.end(); ++it)
 {
 	AdeModelElement* aElement = it.CreateNewElement();
-	if ((aElement->GetType() & 0x7F00000) == ITEM_IS_TRANSITION)
+	if ((aElement->GetType() & ITEM_TYPE_MASK) == ITEM_IS_TRANSITION)
 	{
 		AdeTransition* aTransition = static_cast<AdeTransition*>(aElement);
 		if (aTransition->GetGuard().empty())
-			CodeTransition(theStatechart,theState,*aTransition);
+			CodeTransition(theStatechart, theState, *aTransition);
 	}
 	delete aElement;
 }
 
-fprintf(implementationFile,"\t{\n\t\t// not handled\n\t\tmessage.NotHandeled();\n\t}\n\tEnterState(message);\n}\n\n");
+fprintf(implementationFile, "\t{\n\t\t// not handled\n\t\tmessage.NotHandeled();\n\t}\n\tEnterState(message);\n}\n\n");
--- Model/package_SVSWQPEV/package_YGKSEMEQ/Package_DAJEXPHO/classes/class_ZTLSKJBT/operations/operation_VFFMJPNL/code.cpp
+++ Model/package_SVSWQPEV/package_YGKSEMEQ/Package_DAJEXPHO/classes/class_ZTLSKJBT/operations/operation_VFFMJPNL/code.cpp
@@ -1,10 +1,10 @@
-fprintf(specificationFile,"\t\t//! \\brief Constructor.\n");
-fprintf(specificationFile,"\t\t%s();\n\n",theStatechart.GetName().c_str());
+fprintf(specificationFile, "\t\t//! \\brief Constructor.\n");
+fprintf(specificationFile, "\t\t%s();\n\n", theStatechart.GetName().c_str());
 
-fprintf(specificationFile,"\t\t//! \\brief Destructor.\n");
-fprintf(specificationFile,"\t\t~%s();\n\n",theStatechart.GetName().c_str());
+fprintf(specificationFile, "\t\t//! \\brief Destructor.\n");
+fprintf(specificationFile, "\t\t~%s();\n\n", theStatechart.GetName().c_str());
 
-fprintf(implementationFile,"%s::%s()\n{\n\ttheState = &%s::NoState;\n",theStatechart.GetName().c_str(),theStatechart.GetName().c_str(),theStatechart.GetName().c_str());
-fprintf(implementationFile,"\tnextState = 0;\n}\n\n");
+fprintf(implementationFile, "%s::%s()\n{\n\ttheState = &%s::NoState;\n", theStatechart.GetName().c_str(), theStatechart.GetName().c_str(), theStatechart.GetName().c_str());
+fprintf(implementationFile, "\tnextState = 0;\n}\n\n");
 
-fprintf(implementationFile,"%s::~%s()\n{\n}\n\n",theStatechart.GetName().c_str(),theStatechart.GetName().c_str(),theStatechart.GetName().c_str());
+fprintf(implementationFile, "%s::~%s()\n{\n}\n\n", theStatechart.GetName().c_str(), theStatechart.GetName().c_str());
