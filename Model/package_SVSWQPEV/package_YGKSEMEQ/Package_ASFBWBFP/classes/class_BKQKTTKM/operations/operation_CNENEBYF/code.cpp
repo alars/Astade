15398b1d0128548607b67987ed7352247e7d9c18
Anders Larsen <al@alarsen.net>
2006-10-25 14:46:50 +0000

    Allow Open/Save files without absolute path
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@675 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/package_SVSWQPEV/package_YGKSEMEQ/Package_ASFBWBFP/classes/class_BKQKTTKM/operations/operation_CNENEBYF/code.cpp
+++ Model/package_SVSWQPEV/package_YGKSEMEQ/Package_ASFBWBFP/classes/class_BKQKTTKM/operations/operation_CNENEBYF/code.cpp
@@ -1,3 +1,5 @@
+/* vi: set tabstop=4: */
+
 wxFileName aFile(currentFile);
 
 if (aFile.GetExt() != "ucm")
@@ -6,7 +8,8 @@ if (aFile.GetExt() != "ucm")
 	return;
 }
 
-wxFileConfig aConfig(wxEmptyString, wxEmptyString, aFile.GetFullPath());
+wxFileConfig aConfig(wxEmptyString, wxEmptyString, aFile.GetFullPath(),
+	wxEmptyString, wxCONFIG_USE_LOCAL_FILE | wxCONFIG_USE_RELATIVE_PATH);
 
 myUsePanel.Save(aConfig);
 
--- Model/package_SVSWQPEV/package_YGKSEMEQ/Package_ASFBWBFP/classes/class_BKQKTTKM/operations/operation_EDEWXZSO/code.cpp
+++ Model/package_SVSWQPEV/package_YGKSEMEQ/Package_ASFBWBFP/classes/class_BKQKTTKM/operations/operation_EDEWXZSO/code.cpp
@@ -1,8 +1,11 @@
-if ( !dir.empty() )
+/* vi: set tabstop=4: */
+
+if (!dir.empty())
 {
 	currentFile = dir;
 	wxFileName aFile(currentFile);
-	wxFileConfig aConfig(wxEmptyString, wxEmptyString, aFile.GetFullPath());
+	wxFileConfig aConfig(wxEmptyString, wxEmptyString, aFile.GetFullPath(),
+		wxEmptyString, wxCONFIG_USE_LOCAL_FILE | wxCONFIG_USE_RELATIVE_PATH);
 	myUsePanel.Load(aConfig);
 	SetTitle(currentFile);
 	isChanged = false;
