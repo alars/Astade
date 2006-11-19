342546da7496b65ce6af38afd63e2c1090ddd8d1
Thomas Spitzer <projektleiter@astade.de>
2006-11-19 14:45:35 +0000

    Continued ACDrawer
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@693 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/package_SVSWQPEV/package_YGKSEMEQ/Package_OJDGPJRY/classes/class_VDFRDCDT/operations/operation_FYZPIQFM/code.cpp
+++ Model/package_SVSWQPEV/package_YGKSEMEQ/Package_OJDGPJRY/classes/class_VDFRDCDT/operations/operation_FYZPIQFM/code.cpp
@@ -1,7 +1,6 @@
 wxConfigBase::Set(new wxFileConfig("ACDrawer.ini"));
-wxConfigBase::Get()->Write("Nodes/Node_1/Label","Test_1");
-wxConfigBase::Get()->Write("Nodes/Node_2/Label","Test_2");
 myFrame = new  ACDrawerFrame;
+myFrame->Init();
 SetTopWindow(myFrame);
 myFrame->Show(true);
 return true;
