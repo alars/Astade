76274afbd6d6bc90cabe91109df970fb71788706
Anders Larsen <al@alarsen.net>
2006-11-20 16:19:45 +0000

    Display '\n' in class/object names as newline
    git-svn-id: http://trace2uml.tigris.org/svn/trace2uml/trunk@54 89f8ffb8-d1fc-0310-92ca-d9a8b2f27bcf

--- Model/Package_LEOIFVGO/classes/class_GQADNYHS/operations/operation_ZJEHAUCV/code.cpp
+++ Model/Package_LEOIFVGO/classes/class_GQADNYHS/operations/operation_ZJEHAUCV/code.cpp
@@ -1,6 +1,9 @@
-dc.SetPen(*wxThePenList->FindOrCreatePen(wxTheColourDatabase->Find("SEA GREEN"),1,wxSOLID));
+/* vi: set tabstop=4: */
 
-wxString name = dataBase->GetClassName(objectNumber);
+dc.SetPen(*wxThePenList->FindOrCreatePen(wxTheColourDatabase->Find("SEA GREEN"), 1, wxSOLID));
+
+wxString name(dataBase->GetClassName(objectNumber));
+name.Replace("\\n", "\n", true);
 
 int pos = name.Find(':');
 if (pos >= 0 && pos != name.Find("::"))
@@ -12,11 +15,12 @@ wxCoord w, h;
 dc.GetTextExtent(name, &w, &h);
 int width = dataBase->GetClassBoxWidth(objectNumber);
 
-dc.DrawRectangle(dataBase->GetClassMiddle(objectNumber)-width/2,
-				dataBase->GetTime2Y(eventNumber)-dataBase->GetClassBoxHeight(),
+dc.DrawRectangle(dataBase->GetClassMiddle(objectNumber) - width / 2,
+				dataBase->GetTime2Y(eventNumber) - dataBase->GetClassBoxHeight(),
 				width,
 				dataBase->GetClassBoxHeight());
 
-dc.DrawText(name, dataBase->GetClassMiddle(objectNumber)-(w/2), dataBase->GetTime2Y(eventNumber)-(h/2)-(dataBase->GetClassBoxHeight()/2));
+dc.DrawText(name, dataBase->GetClassMiddle(objectNumber) - w / 2,
+			dataBase->GetTime2Y(eventNumber) - h / 2 - dataBase->GetClassBoxHeight() / 2);
 
 dc.SetFont(normalFont);
