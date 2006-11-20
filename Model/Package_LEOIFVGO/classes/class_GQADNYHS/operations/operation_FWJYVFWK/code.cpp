68143082b1c4495e93c73aaccc246a395070fe34
Thomas Spitzer <projektleiter@astade.de>
2006-11-20 19:59:00 +0000

    Did some recalculation of box sizes (they where calculated with the wrong Font). Changed the positioning when 2 line box texts (Anders change did not work under windows) (Hopefully mine works under Linux ;-)
    git-svn-id: http://trace2uml.tigris.org/svn/trace2uml/trunk@56 89f8ffb8-d1fc-0310-92ca-d9a8b2f27bcf

--- Model/Package_LEOIFVGO/classes/class_GQADNYHS/operations/operation_FWJYVFWK/code.cpp
+++ Model/Package_LEOIFVGO/classes/class_GQADNYHS/operations/operation_FWJYVFWK/code.cpp
@@ -19,13 +19,40 @@ for (i = 0; i < dataBase->GetClassCount(); i++)
 for (i = 0; i < dataBase->GetClassCount(); i++)
 {
 	wxString name = dataBase->GetClassName(i);
+
+	int pos = name.Find(':');
+	if (pos >= 0 && pos != name.Find("::"))
+		dc.SetFont(objectFont);
+	else
+		dc.SetFont(classFont);
+
 	wxCoord w,h;
-	dc.GetTextExtent(name, &w, &h);
+	wxCoord w2, h2;
+	wxString name2;
+
+	if (name.Find("\\n")!=-1)
+	{
+		int pos = name.Find("\\n");
+		name2 = name.Mid(pos+2);
+		name = name.Left(pos);
+
+		dc.GetTextExtent(name, &w, &h);
+		dc.GetTextExtent(name2, &w2, &h2);
+		if (w2>w)
+			w = w2;
+	}
+	else
+	{
+		dc.GetTextExtent(name, &w, &h);
+	}
+
 	int width = w+10;
 	if (width < 80)
 		width = 80;
 	dataBase->SetClassBoxWidth(i, width);
 }
+
+dc.SetFont(normalFont);
 
 // Now all the events
 for (i = 0; i < eventsCount; i++)
--- Model/Package_LEOIFVGO/classes/class_GQADNYHS/operations/operation_ZJEHAUCV/code.cpp
+++ Model/Package_LEOIFVGO/classes/class_GQADNYHS/operations/operation_ZJEHAUCV/code.cpp
@@ -3,7 +3,6 @@
 dc.SetPen(*wxThePenList->FindOrCreatePen(wxTheColourDatabase->Find("SEA GREEN"), 1, wxSOLID));
 
 wxString name(dataBase->GetClassName(objectNumber));
-name.Replace("\\n", "\n", true);
 
 int pos = name.Find(':');
 if (pos >= 0 && pos != name.Find("::"))
@@ -12,7 +11,23 @@ else
 	dc.SetFont(classFont);
 
 wxCoord w, h;
-dc.GetTextExtent(name, &w, &h);
+wxCoord w2, h2;
+wxString name2;
+
+if (name.Find("\\n")!=-1)
+{
+	int pos = name.Find("\\n");
+	name2 = name.Mid(pos+2);
+	name = name.Left(pos);
+
+	dc.GetTextExtent(name, &w, &h);
+	dc.GetTextExtent(name2, &w2, &h2);
+}
+else
+{
+	dc.GetTextExtent(name, &w, &h);
+}
+
 int width = dataBase->GetClassBoxWidth(objectNumber);
 
 dc.DrawRectangle(dataBase->GetClassMiddle(objectNumber) - width / 2,
@@ -20,7 +35,17 @@ dc.DrawRectangle(dataBase->GetClassMiddle(objectNumber) - width / 2,
 				width,
 				dataBase->GetClassBoxHeight());
 
-dc.DrawText(name, dataBase->GetClassMiddle(objectNumber) - w / 2,
-			dataBase->GetTime2Y(eventNumber) - h / 2 - dataBase->GetClassBoxHeight() / 2);
+if (name2.empty())
+{
+	dc.DrawText(name, dataBase->GetClassMiddle(objectNumber) - w / 2,
+				dataBase->GetTime2Y(eventNumber) - h / 2 - dataBase->GetClassBoxHeight() / 2);
+}
+else
+{
+	dc.DrawText(name2, dataBase->GetClassMiddle(objectNumber) - w2 / 2,
+				dataBase->GetTime2Y(eventNumber) - dataBase->GetClassBoxHeight() / 2 );
+	dc.DrawText(name, dataBase->GetClassMiddle(objectNumber) - w / 2,
+				dataBase->GetTime2Y(eventNumber) - h - dataBase->GetClassBoxHeight() / 2);
+}
 
 dc.SetFont(normalFont);
