c99b855a0f83339d3b7007a90c052d67626f9cc9
Thomas Spitzer <projektleiter@astade.de>
2006-12-16 07:32:48 +0000

    The entries of the jump table (from component to class) are sorted, now.
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@710 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/package_SVSWQPEV/package_YGKSEMEQ/package_TLNCNYLI/classes/class_ODHKSPMR/operations/operation_JPPOMGGL/code.cpp
+++ Model/package_SVSWQPEV/package_YGKSEMEQ/package_TLNCNYLI/classes/class_ODHKSPMR/operations/operation_JPPOMGGL/code.cpp
@@ -2,25 +2,31 @@ wxMenu* aSubUp = new wxMenu(wxEmptyString);
 
 int id = ID_JUMP;
 AdeElementIterator it;
+std::map<wxString,int> menuItems;
 
 // Generating the classes
 for (it = component.GetFirstBelongingClass(); it != component.end(); ++it)
 {
-	AdeModelElement* anElement = it.CreateNewElement();
-	aSubUp->Append(id++, anElement->GetName());
-	delete anElement;
 	if (id >= ID_JUMPMAX)
-		return aSubUp;
+		break;
+	AdeModelElement* anElement = it.CreateNewElement();
+	menuItems[anElement->GetName()] = id++;
+	delete anElement;
 }
 
 // Generating the statecharts
 for (it = component.GetFirstBelongingStatechart(); it != component.end(); ++it)
 {
-	AdeModelElement* anElement = it.CreateNewElement();
-	aSubUp->Append(id++, anElement->GetName());
-	delete anElement;
 	if (id >= ID_JUMPMAX)
-		return aSubUp;
+		break;
+	AdeModelElement* anElement = it.CreateNewElement();
+	menuItems[anElement->GetName()] = id++;
+	delete anElement;
+}
+
+for (std::map<wxString,int>::iterator iter = menuItems.begin(); iter != menuItems.end(); iter++)
+{
+	aSubUp->Append((*iter).second, (*iter).first);
 }
 
 return aSubUp;
