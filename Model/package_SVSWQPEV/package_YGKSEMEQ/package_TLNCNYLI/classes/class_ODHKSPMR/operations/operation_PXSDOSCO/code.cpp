799a7dd2efc2bfd2108752734320d11730f97cb7
Kyung Un Choi <projectopen@gmail.com>
2006-12-27 22:09:11 +0000

    More keyboard short cut. See short_key_layout.txt for more detail
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@718 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/package_SVSWQPEV/package_YGKSEMEQ/package_TLNCNYLI/classes/class_ODHKSPMR/operations/operation_PXSDOSCO/code.cpp
+++ Model/package_SVSWQPEV/package_YGKSEMEQ/package_TLNCNYLI/classes/class_ODHKSPMR/operations/operation_PXSDOSCO/code.cpp
@@ -1,8 +1,14 @@
 //keyboard shortcut
-wxAcceleratorEntry entries[4];
+int count = 9;
+wxAcceleratorEntry entries[count];
   entries[0].Set(wxACCEL_CTRL,  (int) 'C',     ID_COPY);
   entries[1].Set(wxACCEL_CTRL,  (int) 'V',     ID_PASTE);
   entries[2].Set(wxACCEL_CTRL, (int) 'X',     ID_CUT);
   entries[3].Set(wxACCEL_NORMAL,  WXK_DELETE,  ID_DELETE);
-  wxAcceleratorTable accel(4, entries);
+  entries[4].Set(wxACCEL_CTRL, (int) 'G', ID_REGENERATE);
+  entries[5].Set(wxACCEL_CTRL, (int) 'A', ID_ACTIVECONFIGURATION);
+  entries[6].Set(wxACCEL_NORMAL, WXK_F5, ID_RUN);
+  entries[7].Set(wxACCEL_NORMAL, WXK_F6, ID_MAKE);
+  entries[8].Set(wxACCEL_NORMAL, WXK_F7, ID_MAKEALL);
+  wxAcceleratorTable accel(count, entries);
   this->SetAcceleratorTable(accel);
