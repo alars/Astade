e3ce58394bc5a8322fc5872f0aee40d45e240d2f
Thomas Spitzer <projektleiter@astade.de>
2006-12-30 09:51:49 +0000

    Changed the Label of GrafUseCase, because it does not look correct in windows. hopefully I did not mess up the Linux build again.
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@724 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/package_SVSWQPEV/package_YGKSEMEQ/Package_ASFBWBFP/Package_DKNBOEDV/classes/class_RMRUUORU/operations/operation_EVJBPFDC/code.cpp
+++ Model/package_SVSWQPEV/package_YGKSEMEQ/Package_ASFBWBFP/Package_DKNBOEDV/classes/class_RMRUUORU/operations/operation_EVJBPFDC/code.cpp
@@ -1,9 +1,9 @@
 /* vi: set tabstop=4: */
 
-wxString aLabel = myLabel->GetLineText(0).Trim(false).Trim(true) + "\n";
+wxString aLabel = myLabel->GetLineText(0).Trim(false).Trim(true) + " ";
 if (myLabel->GetNumberOfLines() > 1)
 	aLabel += myLabel->GetLineText(1).Trim(false).Trim(true);
-aLabel += "\n";
+aLabel += " ";
 if (myLabel->GetNumberOfLines() > 2)
 	aLabel += myLabel->GetLineText(2).Trim(false).Trim(true);
 
