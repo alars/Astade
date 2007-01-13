47930c43da8bf95ab1473481b171d3b689be86ad
Thomas Spitzer <projektleiter@astade.de>
2007-01-13 08:16:43 +0000

    Added a "SaveVersion" to the Model. This gives Astade the possibility to detect, weather a Model is modified by a newer (incompatible) version of Astade. Astade checks at program start and gives a warning, if the model is incompatible. We don't have this, yet. But it has to be implement in the "old" version, if there will be a version with incompatible save formate.
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@738 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/package_SVSWQPEV/package_YGKSEMEQ/package_TLNCNYLI/classes/class_ZZKPVCBY/operations/constructor_BCOGWGWQ/code.cpp
+++ Model/package_SVSWQPEV/package_YGKSEMEQ/package_TLNCNYLI/classes/class_ZZKPVCBY/operations/constructor_BCOGWGWQ/code.cpp
@@ -9,6 +9,26 @@ modelDir.AssignDir(modelPath);
 modelDir.MakeAbsolute();
 
 AdeModel* aModel = new AdeModel(modelDir);
+
+// Check wether the model is newer (modified by a newer version of Astade)
+int saveVersion = aModel->GetSaveVersion();
+if (saveVersion==0)
+{
+	aModel->SetSaveVersion(1);
+}
+else
+{
+	if (saveVersion > 1) // Model is not kompatibel to this Astade version
+	{
+		if (wxMessageBox("This model seems to be modified by a newer version of Astade. If you continue, things might not work propperly.\nBetter you get the newest version at http://Astade.tigris.org.\n\nDo You really want to try working with this Astade version (on your own risk)?",
+				"Model has newer Version!", wxICON_QUESTION  | wxYES_NO) == wxNO)
+		{
+			DeleteAllItems();
+			return;
+		}
+	}
+}
+
 SetItemData(myRootItem, aModel);
 
 if (aModel->GetRepository() == "SVN")
