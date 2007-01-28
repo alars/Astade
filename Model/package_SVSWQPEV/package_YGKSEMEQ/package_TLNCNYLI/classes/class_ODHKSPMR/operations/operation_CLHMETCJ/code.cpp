ec8ae1e28a63cdae2bb061b1886c40607b6463aa
Jonatan Antoni <jantoni@web.de>
2007-01-28 20:38:07 +0000

    started workin on drag'n'drop. one can now drag any modelelement from the treeviewer and drop it anywere a file can be dropped. in terms of an element represented by a modelnode.ini, the ini-file is used.
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@766 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/package_SVSWQPEV/package_YGKSEMEQ/package_TLNCNYLI/classes/class_ODHKSPMR/operations/operation_CLHMETCJ/code.cpp
+++ Model/package_SVSWQPEV/package_YGKSEMEQ/package_TLNCNYLI/classes/class_ODHKSPMR/operations/operation_CLHMETCJ/code.cpp
@@ -1,2 +1,9 @@
 event.Allow();
-copySource = myTree->GetSelection();
+//copySource = myTree->GetSelection();
+wxTreeItemId aID = myTree->GetSelection();
+wxFileName aFilename = myTree->GetItem(aID)->GetFileName();
+wxFileDataObject dragData;
+dragData.AddFile(aFilename.GetFullPath());
+wxDropSource dragSource(this);
+dragSource.SetData(dragData);
+dragSource.DoDragDrop();
