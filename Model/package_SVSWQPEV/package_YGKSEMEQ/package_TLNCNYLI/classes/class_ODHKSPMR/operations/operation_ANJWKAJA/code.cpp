0f77f1ce232ef598f046021768c22e7dbfa64e83
Jonatan Antoni <jantoni@web.de>
2007-01-29 17:24:30 +0000

    used the same way to transport data for the copy-action. one may now copy a modelelement and paste its file to the filemanager. this is only for testing purposes. within astade the old way is still used.
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@768 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/package_SVSWQPEV/package_YGKSEMEQ/package_TLNCNYLI/classes/class_ODHKSPMR/operations/operation_ANJWKAJA/code.cpp
+++ Model/package_SVSWQPEV/package_YGKSEMEQ/package_TLNCNYLI/classes/class_ODHKSPMR/operations/operation_ANJWKAJA/code.cpp
@@ -1,2 +1,17 @@
 copySource = myTree->GetSelection();
 doCut = false;
+
+/* copy file to clipboard as well */
+if(wxTheClipboard->Open())
+{
+	wxTreeItemId aID = myTree->GetSelection();
+	wxFileName aFilename = myTree->GetItem(aID)->GetFileName();
+	wxFileDataObject* copyData = new wxFileDataObject();
+	copyData->AddFile(aFilename.GetFullPath());
+	wxTheClipboard->SetData(copyData);
+	wxTheClipboard->Close();
+}
+else
+{
+	wxLogMessage("Cannot open the Clipboard!");
+}
