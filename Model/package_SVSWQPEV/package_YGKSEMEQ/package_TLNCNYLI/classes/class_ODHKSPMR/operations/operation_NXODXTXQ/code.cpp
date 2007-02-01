c60f1552b5a2967b40067b939bf442479cadcfbf
Jonatan Antoni <jantoni@web.de>
2007-02-01 23:12:17 +0000

    forbit to copy an element to itself per drag drop
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@774 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/package_SVSWQPEV/package_YGKSEMEQ/package_TLNCNYLI/classes/class_ODHKSPMR/operations/operation_NXODXTXQ/code.cpp
+++ Model/package_SVSWQPEV/package_YGKSEMEQ/package_TLNCNYLI/classes/class_ODHKSPMR/operations/operation_NXODXTXQ/code.cpp
@@ -10,14 +10,29 @@ if(filenames.GetCount()==1)
 {
 	wxFileName aFile(filenames[0]); // take first filename
 	AdeModelElement* aElement = AdeModelElement::CreateNewElement(aFile);
-	// did this type of element fit here?
-	if(dest->CanContain(*aElement))
+	// did this type of element fit here?	
+	if(aElement->GetFileName() != parentPath)
 	{
-		aElement->CreateCopy(parentPath); // copy the element here
-		UpdateSubtree(aID);
-		retVal = true;
+		if(dest->CanContain(*aElement))
+		{
+			aElement->CreateCopy(parentPath); // copy the element here
+			UpdateSubtree(aID);
+			retVal = true;
+		}
+		else
+		{
+			wxLogMessage("This type of element cannot be dropped here!");
+		}
+	}
+	else
+	{
+		wxLogMessage("The element cannot be copied to itself!");
 	}
 	delete(aElement);
+}
+else
+{
+	wxLogMessage("Only one file can be dropped to Astade at once!");
 }
 
 return retVal;
