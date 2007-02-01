1ac9aa1ab672922101919ce87dfdec95849106c4
Jonatan Antoni <jantoni@web.de>
2007-02-01 22:55:29 +0000

    implemented createcopy for adefileelement
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@773 aaa30fad-34f9-0310-8aa9-877cc762ee2e

---
+++ Model/package_SVSWQPEV/package_OPTPAIKK/package_CISPFMDB/package_UPBJDSOK/classes/class_OJVPCTEI/operations/operation_MOFIDQRS/code.cpp
@@ -0,0 +1,24 @@
+wxFileName source = myFileName;
+wxString sourceFileName = source.GetFullName();  //Get source file
+
+wxFileName aFileName = parentPath;
+aFileName.SetFullName(sourceFileName);  //Set destination file name same as source.
+
+wxCopyFile(source.GetFullPath(), aFileName.GetFullPath());
+
+
+AdeRevisionControlBase* theRevisionControl = AdeRevisionControlBase::GetRevisionControlObject();  //additional code for version control.
+if (theRevisionControl->IsAddSupported())
+{
+	int ret = theRevisionControl->Add(aFileName);
+	wxArrayString output = theRevisionControl->GetOutput();
+
+	if (ret!=0)
+	{
+		wxString message;
+		for(size_t i=0; i<output.GetCount(); i++) message += output[i]+"\n";
+		wxMessageBox(message, "Operation failed",wxOK | wxICON_ERROR);
+	}
+}
+
+return aFileName;
