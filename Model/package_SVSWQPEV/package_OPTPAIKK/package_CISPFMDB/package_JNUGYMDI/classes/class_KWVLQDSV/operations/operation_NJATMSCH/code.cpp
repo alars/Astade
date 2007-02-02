afc54c369c4d64c7b42cd9ad52f359f0b9f3a8a1
Thomas Spitzer <projektleiter@astade.de>
2007-02-02 16:36:14 +0000

    The component gets a red dot now, when it is not up to date. No cache implemented, yet. Lets see, if it is fast enough.
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@775 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/package_SVSWQPEV/package_OPTPAIKK/package_CISPFMDB/package_JNUGYMDI/classes/class_KWVLQDSV/operations/operation_NJATMSCH/code.cpp
+++ Model/package_SVSWQPEV/package_OPTPAIKK/package_CISPFMDB/package_JNUGYMDI/classes/class_KWVLQDSV/operations/operation_NJATMSCH/code.cpp
@@ -1,4 +1,31 @@
 if (IsActiveComponent())
+{
+	AdeElementIterator it;
+	for (it = GetFirstBelongingClass(); it != end(); ++it)
+	{
+		AdeModelElement* anElement = it.CreateNewElement();
+		AdeClass* aClass = dynamic_cast<AdeClass*>(anElement);
+		if (aClass)
+		{
+			if (aClass->GetModificationTime() > aClass->GetImpGenerationTime())
+				return 69;
+		}
+		delete anElement;
+	}
+
+	for (it = GetFirstBelongingStatechart(); it != end(); ++it)
+	{
+		AdeModelElement* anElement = it.CreateNewElement();
+		AdeClass* aClass = dynamic_cast<AdeClass*>(anElement);
+		if (aClass)
+		{
+			if (aClass->GetModificationTime() > aClass->GetImpGenerationTime())
+				return 69;
+		}
+		delete anElement;
+	}
+
 	return 42;
+}
 else
 	return 6;
