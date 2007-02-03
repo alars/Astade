ea0908d459b9f05307bc1985d53ffa07269f7691
Jonatan Antoni <jantoni@web.de>
2007-02-03 14:21:26 +0000

    Now a class may be dropped to a relations-folder, this creates a new relation.
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@778 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/package_SVSWQPEV/package_YGKSEMEQ/package_TLNCNYLI/classes/class_ODHKSPMR/operations/operation_NXODXTXQ/code.cpp
+++ Model/package_SVSWQPEV/package_YGKSEMEQ/package_TLNCNYLI/classes/class_ODHKSPMR/operations/operation_NXODXTXQ/code.cpp
@@ -10,7 +10,7 @@ if(filenames.GetCount()==1)
 {
 	wxFileName aFile(filenames[0]); // take first filename
 	AdeModelElement* aElement = AdeModelElement::CreateNewElement(aFile);
-	// did this type of element fit here?	
+	// did this type of element fit here?
 	if(aElement->GetFileName() != parentPath)
 	{
 		if(dest->CanContain(*aElement))
@@ -18,6 +18,15 @@ if(filenames.GetCount()==1)
 			aElement->CreateCopy(parentPath); // copy the element here
 			UpdateSubtree(aID);
 			retVal = true;
+		}
+		// if the source is a class and the destination is a relations-folder => create a relation
+		else if(   ((aElement->GetType() & ITEM_TYPE_MASK) == ITEM_IS_CLASS)
+				&& ((dest->GetType()     & ITEM_TYPE_MASK) == ITEM_IS_RELATIONS) )
+		{
+			wxTreeItemId startId = myTree->GetItemParent(aID);
+			wxFileName theNewRelation = AdeRelation::CreateNewElement(myTree->GetItem(startId)->GetFileName(), aElement->GetFileName());
+			UpdateSubtree(aID);
+			myTree->ShowNode(theNewRelation);
 		}
 		else
 		{
