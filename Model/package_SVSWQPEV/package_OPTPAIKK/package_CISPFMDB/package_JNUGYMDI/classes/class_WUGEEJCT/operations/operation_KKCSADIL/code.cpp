2f0f3ec48bf154cf2bdc91332ce342389152c63f
Anders Larsen <al@alarsen.net>
2007-04-08 23:16:55 +0000

    Fixed the CppGenerator to add attribute initializers to _all_ constructors
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@815 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/package_SVSWQPEV/package_OPTPAIKK/package_CISPFMDB/package_JNUGYMDI/classes/class_WUGEEJCT/operations/operation_KKCSADIL/code.cpp
+++ Model/package_SVSWQPEV/package_OPTPAIKK/package_CISPFMDB/package_JNUGYMDI/classes/class_WUGEEJCT/operations/operation_KKCSADIL/code.cpp
@@ -1,11 +1,9 @@
 wxFileName classFileName = myFileName;
 
 int i = classFileName.GetDirCount();
-classFileName.RemoveDir(i);
 classFileName.RemoveDir(i-1);
 classFileName.RemoveDir(i-2);
 classFileName.SetFullName("ModelNode.ini");
-
 
 wxFileConfig theConfig(wxEmptyString,wxEmptyString,wxEmptyString,classFileName.GetFullPath());
 
--- Model/package_SVSWQPEV/package_YGKSEMEQ/Package_CodeGenerators/classes/class_RFBUBSJJ/operations/operation_IEXWNJYD/code.cpp
+++ Model/package_SVSWQPEV/package_YGKSEMEQ/Package_CodeGenerators/classes/class_RFBUBSJJ/operations/operation_IEXWNJYD/code.cpp
@@ -10,16 +10,17 @@ if ((po->GetType() & (ITEM_IS_NORMALOP|ITEM_IS_DEST)) == 0)
 	if (!paramlist.empty())
 		paramlist = " :\n\t" + paramlist;
 
-	while (!AttributeList.empty())
+	std::list<wxString>::iterator it;
+
+	for (it = AttributeList.begin(); it != AttributeList.end(); ++it)
 	{
-		if (!memberDefaults[AttributeList.front()].empty())
+		if (!memberDefaults[*it].empty())
 		{
 			if (paramlist.empty())
-				paramlist = " :\n\t" + AttributeList.front() + "(" + memberDefaults[AttributeList.front()] + ")";
+				paramlist = " :\n\t" + *it + "(" + memberDefaults[*it] + ")";
 			else
-				paramlist = paramlist + ",\n\t" + AttributeList.front() + "(" + memberDefaults[AttributeList.front()] + ")";
+				paramlist = paramlist + ",\n\t" + *it + "(" + memberDefaults[*it] + ")";
 		}
-		AttributeList.pop_front();
 	}
 }
 return paramlist;
