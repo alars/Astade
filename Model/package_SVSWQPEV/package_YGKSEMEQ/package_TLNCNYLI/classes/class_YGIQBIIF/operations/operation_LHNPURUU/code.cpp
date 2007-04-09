016dd249df513606e3c9fe10ba02e2f7f47deabd
Anders Larsen <al@alarsen.net>
2007-04-09 22:01:37 +0000

    Added a quick-hack to work around an initialisation race
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@819 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/package_SVSWQPEV/package_YGKSEMEQ/package_TLNCNYLI/classes/class_YGIQBIIF/operations/operation_LHNPURUU/code.cpp
+++ Model/package_SVSWQPEV/package_YGKSEMEQ/package_TLNCNYLI/classes/class_YGIQBIIF/operations/operation_LHNPURUU/code.cpp
@@ -4,7 +4,10 @@ if (theElement)
 {
 	int iconIndex = theElement->GetIconIndex();
 	AstadeIconEvent aEvent(theID,iconIndex);
-	Astade::me->GetMyFrame()->AddPendingEvent(aEvent);
+	wxFrame* theFrame;
+	while (!(theFrame = Astade::me->GetMyFrame()))
+		wxMilliSleep(10);
+	theFrame->AddPendingEvent(aEvent);
 	delete theElement;
 }
 return 0;
