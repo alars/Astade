63d19d95a7ae95670af246f49a5b20639e6736b9
Thomas Spitzer <projektleiter@astade.de>
2007-05-18 06:31:50 +0000

    The "Rename model tree" is only possible, when the repository supports it (only Repository "none", yet)
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@848 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/components_WGNBOFKH/component_KSEQOEET/manual/AstadeVersion.h
+++ Model/components_WGNBOFKH/component_KSEQOEET/manual/AstadeVersion.h
@@ -1,1 +1,1 @@
-#define AstadeVersion "0.7.2"
+#define AstadeVersion "0.7.3 test"
--- Model/package_SVSWQPEV/package_YGKSEMEQ/package_TLNCNYLI/classes/class_ODHKSPMR/operations/operation_WEHKZHEU/code.cpp
+++ Model/package_SVSWQPEV/package_YGKSEMEQ/package_TLNCNYLI/classes/class_ODHKSPMR/operations/operation_WEHKZHEU/code.cpp
@@ -2,6 +2,10 @@ aFileMenu = new wxMenu(0);
 aFileMenu->Append(ID_SELECTMODEL, "&open model...", "", wxITEM_NORMAL);
 aFileMenu->AppendSeparator();
 aFileMenu->Append(ID_RENAME, "rename model tree", "", wxITEM_NORMAL);
+
+AdeRevisionControlBase* theRevisionControl = AdeRevisionControlBase::GetRevisionControlObject();
+aFileMenu->Enable(ID_RENAME,theRevisionControl->IsRenameSupported());
+
 aFileMenu->AppendSeparator();
 aFileMenu->Append(ID_EXIT, "E&xit", "", wxITEM_NORMAL);
 aFileMenu->AppendSeparator();
