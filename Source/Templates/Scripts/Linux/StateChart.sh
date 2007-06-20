886f8b2810297386abe31ffb6c06040a6a05277b
Andreas Becker <andi.becker@gmx.de>
2007-06-20 14:03:36 +0000

    added Option and Parameters
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@872 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Source/Templates/Scripts/Linux/StateChart.sh
+++ Source/Templates/Scripts/Linux/StateChart.sh
@@ -1,14 +1,73 @@
 #!/bin/sh
-
 VIEWER=display
+IMGTYPE=png
 IMGDIR=/tmp
-IMGTYPE=gif
 
-: "$@"
-NAME=$_
-BASE=${NAME%/*}
-CHART=${IMGDIR}/${BASE##*/}.${IMGTYPE}
+if [ $# == 0 ]; then
+   # Without Parameter
+   echo "Please use one of old or new style calls."
+   echo "Old Style: $0 DIRNAME/ModelNode.ini"
+   echo "           this will display the statechart in $IMGTYPE - Format"
+   echo "New Style: $0 -m DIRNAME/ModelNode.ini" 
+   echo "           [-o Directory\Output_File] (/$IMGDIR/sc.$IMGTYPE)" 
+   echo "           [-f Imageformat] png|gif|... ($IMGTYPE) "
+   echo "           [-v Viewer] ($VIEWER)"
 
-StateChartDrawer $* |dot -T${IMGTYPE} -o ${CHART}
-${VIEWER} ${CHART}
-rm -f ${CHART}
+else  
+   if [ $# == 1 ]; then
+      # Parametercount == 1 -> Old Style Call
+      
+      : "$@"
+      NAME=$_
+      BASE=${NAME%/*}
+      CHART=${IMGDIR}/${BASE##*/}.${IMGTYPE}
+   else
+      while getopts "m:o:i:v:" optname
+      do
+         case "$optname" in
+            "i")
+#                echo "using Imageformat : $OPTARG"
+               IMGTYPE=$OPTARG
+               ;;
+            "o")
+#                echo "using Outputfile : $OPTARG"
+               CHART_TMP=$OPTARG
+               ;;
+            "v")
+#                echo "using viewer : $OPTARG"
+               VIEWER=$OPTARG
+               ;;
+            "m")
+#                echo "using Model Directory : $OPTARG"
+               NAME=$OPTARG
+               ;;
+            "?")
+               echo "Unknown option $OPTARG"
+               ;;
+            ":")
+               echo "No argument value for option $OPTARG"
+               ;;
+            *)
+               # Should not occur
+               echo "Unknown error while processing options"
+               ;;
+            esac
+      done
+      if [[ -n $CHART_TMP ]]; then
+         # if $CHART_TMP is not zero
+
+         # is the extension ot the Outfile the same than the Imagetype?
+         if [[ ${CHART_TMP##*.} == ${IMGTYPE} ]]; then
+                  CHART=${CHART_TMP}
+         else
+                  CHART=${CHART_TMP}.${IMGTYPE}
+         fi
+      else
+         BASE=${NAME%/*}
+         CHART=${IMGDIR}/${BASE##*/}.${IMGTYPE}
+      fi
+      echo "using Outputfile : $CHART"
+   fi
+   StateChartDrawer ${NAME} |dot -T${IMGTYPE} -o ${CHART}
+   ${VIEWER} ${CHART}
+fi
