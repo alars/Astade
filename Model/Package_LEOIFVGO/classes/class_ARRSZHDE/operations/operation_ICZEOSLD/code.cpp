b85d73dd6f6ab83246fd43fa619fc8b15644a997
Anders Larsen <al@alarsen.net>
2007-07-14 20:21:55 +0000

    Fixed to build with wxWidgets 2.6 and 2.8+ without 2.6 complicatibility
    git-svn-id: http://trace2uml.tigris.org/svn/trace2uml/trunk@77 89f8ffb8-d1fc-0310-92ca-d9a8b2f27bcf

--- Model/Package_LEOIFVGO/classes/class_ARRSZHDE/operations/operation_ICZEOSLD/code.cpp
+++ Model/Package_LEOIFVGO/classes/class_ARRSZHDE/operations/operation_ICZEOSLD/code.cpp
@@ -1,12 +1,22 @@
 wxFileName aFile(currentFile);
 
-const wxString& dir = wxFileSelector("Save Text",aFile.GetPath(),wxEmptyString,"seq","*.seq",wxOVERWRITE_PROMPT|wxSAVE,this);
+const wxString& dir = wxFileSelector( _T("Save Text"),
+                                      aFile.GetPath(),
+                                      wxEmptyString,
+                                      _T("seq"),
+                                      _T("*.seq"),
+#if wxCHECK_VERSION(2,8,0)
+                                      wxFD_OVERWRITE_PROMPT | wxFD_SAVE,
+#else
+                                      wxOVERWRITE_PROMPT | wxSAVE,
+#endif
+                                      this);
 
 if (!dir.empty())
 {
 	currentFile = dir;
 	if (isChanged)
-		SetTitle(wxString("* ")+currentFile);
+		SetTitle(wxString("* ") + currentFile);
 	else
 		SetTitle(currentFile);
 	Save(event);
