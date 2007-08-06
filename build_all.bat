1d0b227f09438cf37bb90fb1efd09dd24ec80e95
Andreas Pflanz <apflanz@gmx.de>
2007-08-06 11:42:34 +0000

    calls all Makefiles of configuration Windows REA in sequence - for now, no error checking is done !
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@923 aaa30fad-34f9-0310-8aa9-877cc762ee2e

---
+++ build_all.bat
@@ -0,0 +1,42 @@
+pushd .\Model\components_WGNBOFKH\Component_AstadeAPI\Config_ZMSNZKTV\
+make %1
+popd
+pushd .\Model\components_WGNBOFKH\Component_BZBVDSZR\Windows_Rea\
+make %1 
+popd
+pushd .\Model\components_WGNBOFKH\Component_CppGenerator\Config_CPGULUJR\
+make %1 
+popd
+pushd .\Model\components_WGNBOFKH\Component_EIFKVZMS\Windows_Rea\
+make %1 
+popd
+pushd .\Model\components_WGNBOFKH\Component_IXHOAQJM\Windows_Rea\
+make %1 
+popd
+pushd .\Model\components_WGNBOFKH\component_KSEQOEET\Config_LXNVUSPP\
+make %1 
+popd
+pushd .\Model\components_WGNBOFKH\Component_OMDgenerator\Windows_Rea\
+make %1 
+popd
+pushd .\Model\components_WGNBOFKH\Component_RAVGAUPS\Config_PATMRBOG\
+make %1 
+popd
+pushd .\Model\components_WGNBOFKH\Component_XZAWFDAN\Config_OPEOVEFK\
+make %1 
+popd
+pushd .\Model\components_WGNBOFKH\Component_YIIYAPSY\Windows_Rea\
+make %1 
+popd
+pushd .\Model\Components_ZFTMQEHP\Component_RIXFSVSY\Config_NWJYUSSR\
+make %1 
+popd
+pushd .\Model\Components_ZLRQZKQB\Component_DJLKKOUQ\Config_DKQBNWAT\
+make %1 
+popd
+pushd .\Model\Components_ZLRQZKQB\Component_LUAJBDMB\Config_UDUMBDGY\
+make %1 
+popd
+pushd .\Model\Components_ZLRQZKQB\Component_StateChartCoderCdSys\Config_WEYFLKNX\
+make %1 
+popd
