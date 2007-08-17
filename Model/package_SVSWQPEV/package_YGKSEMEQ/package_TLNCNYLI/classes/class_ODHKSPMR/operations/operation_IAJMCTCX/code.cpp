5ac9058053d655cbd05ce2a927df8132468890c4
Thomas Spitzer <projektleiter@astade.de>
2007-08-17 19:57:31 +0000

    Improved tree behavior when performing an element add.
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@962 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/package_SVSWQPEV/package_YGKSEMEQ/package_TLNCNYLI/classes/class_ODHKSPMR/operations/operation_IAJMCTCX/code.cpp
+++ Model/package_SVSWQPEV/package_YGKSEMEQ/package_TLNCNYLI/classes/class_ODHKSPMR/operations/operation_IAJMCTCX/code.cpp
@@ -4,5 +4,18 @@ if (!aID.IsOk())
 	return;
 
 wxFileName theNewElement = myTree->GetItemObject(aID)->AddSubElement(event.GetId());
-UpdateSubtree(aID);
-myTree->ShowNode(theNewElement);
+
+AdeModelElement* aElement = AdeModelElement::CreateNewElement(theNewElement);
+
+wxTreeItemId newItem = myTree->AppendItem(aID,"loading ...", 48);
+myTree->SetItem(newItem,aElement);
+
+myTree->SetItemHasChildren(aID);
+myTree->SelectItem(newItem);
+myTree->EnsureVisible(newItem);
+
+while (newItem.IsOk())
+{
+	myTree->UpdateItem(newItem);
+	newItem = myTree->GetItemParent(newItem);
+}
