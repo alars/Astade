7c2d4e3d28eeff5a7a07f6633997e8aab39b2240
Thomas Spitzer <projektleiter@astade.de>
2007-08-19 13:03:56 +0000

    Bug fix: the tree update did not work for classes without operations.
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@965 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/package_SVSWQPEV/package_OPTPAIKK/package_CISPFMDB/package_JNUGYMDI/classes/class_WWBMFKYE/operations/operation_NWPJWMYS/code.cpp
+++ Model/package_SVSWQPEV/package_OPTPAIKK/package_CISPFMDB/package_JNUGYMDI/classes/class_WWBMFKYE/operations/operation_NWPJWMYS/code.cpp
@@ -1,12 +1,29 @@
-wxDateTime lastModified = AdeModelElement::GetModificationTime();
+wxDateTime lastModified = wxDateTime(static_cast<time_t>(0));
 
 wxString currentFilename;
 wxFileName dirName(myFileName);
+
+wxDir myDir(dirName.GetPath());
+
+if (myDir.GetFirst(&currentFilename, wxEmptyString, wxDIR_FILES))
+	do
+	{
+		wxFileName aOperation(dirName);
+		aOperation.SetFullName(currentFilename);
+		if (aOperation.FileExists())
+		{
+			wxDateTime aModified = aOperation.GetModificationTime();
+			if (aModified > lastModified)
+				lastModified = aModified;
+		}
+	} while (myDir.GetNext(&currentFilename));
+
 dirName.AppendDir("operations");
+dirName.SetFullName(wxEmptyString);
 
 wxDir aDir(dirName.GetPath());
 
-if (aDir.GetFirst(&currentFilename, wxEmptyString, wxDIR_DIRS))
+if (dirName.DirExists() && aDir.GetFirst(&currentFilename, wxEmptyString, wxDIR_DIRS))
 	do
 	{
 		wxFileName aOperation(dirName);
