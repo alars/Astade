532d680d247c2c82c9289347a926324d6ec27825
Jonatan Antoni <jantoni@web.de>
2007-08-22 10:10:11 +0000

    Added some help text to the treeviewer menu.
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@977 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/package_SVSWQPEV/package_YGKSEMEQ/package_TLNCNYLI/classes/class_ODHKSPMR/operations/operation_WEHKZHEU/code.cpp
+++ Model/package_SVSWQPEV/package_YGKSEMEQ/package_TLNCNYLI/classes/class_ODHKSPMR/operations/operation_WEHKZHEU/code.cpp
@@ -1,16 +1,16 @@
 aFileMenu = new wxMenu(0);
-aFileMenu->Append(ID_SELECTMODEL, "&open model...", "", wxITEM_NORMAL);
+aFileMenu->Append(ID_SELECTMODEL, "&open model...", "Open a model directory.", wxITEM_NORMAL);
 
 aFileMenu->AppendSeparator();
-aFileMenu->Append(ID_EXIT, "E&xit", "", wxITEM_NORMAL);
+aFileMenu->Append(ID_EXIT, "E&xit", "Close Astade.", wxITEM_NORMAL);
 aFileMenu->AppendSeparator();
 
 AddRecentList();
 
 wxMenu* aCustomizeMenu = new wxMenu(0);
-aCustomizeMenu->Append(ID_SETEDITORS, "&set directories...", "", wxITEM_NORMAL);
-aCustomizeMenu->Append(ID_SETAPPS, "&set user-applications...", "", wxITEM_NORMAL);
-aCustomizeMenu->Append(ID_SETREGEX, "set &regular expressions...", "", wxITEM_NORMAL);
+aCustomizeMenu->Append(ID_SETEDITORS, "&set directories...", "Change user specific path settings.", wxITEM_NORMAL);
+aCustomizeMenu->Append(ID_SETAPPS, "&set user-applications...", "Change user specific add-on applications.", wxITEM_NORMAL);
+aCustomizeMenu->Append(ID_SETREGEX, "set &regular expressions...", "Change regular expressions for parsing the make output.", wxITEM_NORMAL);
 
 wxMenu* aHelpMenu = new wxMenu(0);
 aHelpMenu->Append(ID_MNU_HELP, "&help");
