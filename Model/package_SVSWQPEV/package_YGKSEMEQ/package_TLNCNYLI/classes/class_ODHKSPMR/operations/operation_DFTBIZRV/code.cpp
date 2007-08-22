a08d11eb918451ca149be39c051241608555908e
Thomas Spitzer <projektleiter@astade.de>
2007-08-22 07:39:15 +0000

    fixed the tree update problems with "complete relations"
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@975 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/package_SVSWQPEV/package_YGKSEMEQ/package_TLNCNYLI/classes/class_ODHKSPMR/operations/operation_DFTBIZRV/code.cpp
+++ Model/package_SVSWQPEV/package_YGKSEMEQ/package_TLNCNYLI/classes/class_ODHKSPMR/operations/operation_DFTBIZRV/code.cpp
@@ -2,12 +2,27 @@ wxTreeItemId aID = myTree->GetSelection();
 wxFileName destination = myTree->GetItem(aID)->GetFileName();
 
 wxFileName source = myTree->GetItem(relationStart)->GetFileName();
+wxFileName relations(source);
 
 wxFileName theNewRelation = AdeRelation::CreateNewElement(source,destination);
 
-myTree->GetItemObject(relationStart)->Touch();
+relations.SetFullName("ModelNode.ini");
+relations.AppendDir("relations");
+
+myTree->SetItemHasChildren(relationStart);
+wxTreeItemId relationsID = myTree->ShowNode(relations);
+
+if (myTree->GetItem(relationsID)->GetFileName() != relations)
+{
+
+	AdeModelElement* aElement = AdeModelElement::CreateNewElement(relations);
+	wxTreeItemId newItem = myTree->AppendItem(relationStart,"loading ...", 48);
+	myTree->EnsureVisible(newItem);
+	myTree->SetItem(newItem,aElement);
+	relationsID = myTree->ShowNode(relations);
+}
+
+myTree->AppendItem(relationsID, theNewRelation);
 
 relationStart = wxTreeItemId();
 AstadeClass::SetRelationSourceName(wxEmptyString);
-
-myTree->ShowNode(theNewRelation);
