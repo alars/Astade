d54fabcc8a49e2816b72b2413e1739f0a5117e42
Thomas Spitzer <projektleiter@astade.de>
2007-08-27 11:55:58 +0000

    Changed the rescaling of the icons from 26x20 back to 20x20 (like it was before)
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@988 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/package_SVSWQPEV/package_YGKSEMEQ/Package_MOGAHMXN/classes/class_BOHWESTM/operations/operation_ZFPWEHCB/code.cpp
+++ Model/package_SVSWQPEV/package_YGKSEMEQ/Package_MOGAHMXN/classes/class_BOHWESTM/operations/operation_ZFPWEHCB/code.cpp
@@ -1,3 +1,12 @@
 wxIcon aIcon;
-aIcon.CopyFromBitmap(wxBitmap(wxImage(wxBitmap(XPM_Data).ConvertToImage()).Rescale(ASTADEICONWIDTH,ASTADEICONHIGHT)));
+
+wxBitmap aBitmap(ASTADEICONWIDTH,ASTADEICONHIGHT);
+wxMemoryDC dc;
+dc.SelectObject(aBitmap);
+dc.SetBrush(*wxWHITE_BRUSH);
+
+dc.Clear();
+dc.DrawBitmap(wxBitmap(wxBitmap(XPM_Data).ConvertToImage().Rescale(ASTADEICONHIGHT,ASTADEICONHIGHT)),4,0,true);
+
+aIcon.CopyFromBitmap(wxBitmap(aBitmap.ConvertToImage()));
 return aIcon;
