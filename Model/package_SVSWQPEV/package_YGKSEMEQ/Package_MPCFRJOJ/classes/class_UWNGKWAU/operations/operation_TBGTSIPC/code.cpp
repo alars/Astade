14554e1de55d6ecd2cc73d22b2d439ec283fc750
Thomas Spitzer <projektleiter@astade.de>
2007-09-30 14:27:04 +0000

    CodeEdit: "Find" works
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@1074 aaa30fad-34f9-0310-8aa9-877cc762ee2e

---
+++ Model/package_SVSWQPEV/package_YGKSEMEQ/Package_MPCFRJOJ/classes/class_UWNGKWAU/operations/operation_TBGTSIPC/code.cpp
@@ -0,0 +1,29 @@
+int flags = 0;
+
+if (event.GetFlags() & wxFR_WHOLEWORD)
+    flags |= wxSTC_FIND_WHOLEWORD;
+
+if (event.GetFlags() & wxFR_MATCHCASE )
+    flags |= wxSTC_FIND_MATCHCASE;
+
+int minPos, maxPos;
+
+if (event.GetFlags() & wxFR_DOWN)
+{
+    minPos = myEditControl->GetCurrentPos();
+    maxPos = 0xFFFFF;
+}
+else
+{
+    minPos = myEditControl->GetCurrentPos();
+    if (minPos)
+        minPos--;
+    maxPos = 0;
+}
+
+int found = myEditControl->FindText(minPos,maxPos,event.GetFindString(),flags);
+
+if (found != -1) 
+    myEditControl->SetSelection(found, found + event.GetFindString().size());
+
+myEditControl->SetFocus();
