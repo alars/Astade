f396c5fab392b49209eaf4535fa4d9a04bc2bd8e
Andreas Pflanz <apflanz@gmx.de>
2007-11-01 11:56:22 +0000

    - modified Windows-Toolchain Makefiles to build again - enhanced build_all.bat to stop if an error occurred
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@1085 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/components_WGNBOFKH/component_KSEQOEET/manual/AstadeVersion.h
+++ Model/components_WGNBOFKH/component_KSEQOEET/manual/AstadeVersion.h
@@ -1,1 +1,1 @@
-#define AstadeVersion "0.8.2"
+#define AstadeVersion "0.8.3"
--- build_all.bat
+++ build_all.bat
@@ -1,48 +1,114 @@
+@echo off
 pushd .\Model\components_WGNBOFKH\Component_AstadeAPI\Config_ZMSNZKTV\
+echo **********************************
+echo Building Astade API
+echo **********************************
 make %1
+@if %errorlevel% NEQ 0 goto ERROR_MAKE
 popd
 pushd .\Model\components_WGNBOFKH\Component_BZBVDSZR\Windows_Rea\
+echo **********************************
+echo Building ResourceEdit
+echo **********************************
 make %1 
+@if %errorlevel% NEQ 0 goto ERROR_MAKE
 popd
 pushd .\Model\components_WGNBOFKH\Component_CppGenerator\Config_CPGULUJR\
+echo **********************************
+echo Building CppGenerator
+echo **********************************
 make %1 
+@if %errorlevel% NEQ 0 goto ERROR_MAKE
 popd
 pushd .\Model\components_WGNBOFKH\Component_EIFKVZMS\Windows_Rea\
+echo **********************************
+echo Building StateChartDrawer
+echo **********************************
 make %1 
+@if %errorlevel% NEQ 0 goto ERROR_MAKE
 popd
 pushd .\Model\components_WGNBOFKH\Component_IXHOAQJM\Windows_Rea\
+echo **********************************
+echo Building UseCaseEdit
+echo **********************************
 make %1 
+@if %errorlevel% NEQ 0 goto ERROR_MAKE
 popd
 pushd .\Model\components_WGNBOFKH\component_KSEQOEET\Config_LXNVUSPP\
+echo **********************************
+echo Building Astade
+echo **********************************
 make %1 
+@if %errorlevel% NEQ 0 goto ERROR_MAKE
 popd
 pushd .\Model\components_WGNBOFKH\Component_OMDgenerator\Windows_Rea\
+echo **********************************
+echo Building OMDgenerator
+echo **********************************
 make %1 
+@if %errorlevel% NEQ 0 goto ERROR_MAKE
 popd
 pushd .\Model\components_WGNBOFKH\Component_RAVGAUPS\Config_PATMRBOG\
+echo **********************************
+echo Building StateChartCoder
+echo **********************************
 make %1 
+@if %errorlevel% NEQ 0 goto ERROR_MAKE
 popd
 pushd .\Model\components_WGNBOFKH\Component_XZAWFDAN\Config_OPEOVEFK\
+echo **********************************
+echo Building AstadeGenerate
+echo **********************************
 make %1 
+@if %errorlevel% NEQ 0 goto ERROR_MAKE
 popd
 pushd .\Model\components_WGNBOFKH\Component_YIIYAPSY\Windows_Rea\
+echo **********************************
+echo Building ActivityChartDrawer
+echo **********************************
 make %1 
+@if %errorlevel% NEQ 0 goto ERROR_MAKE
 popd
 pushd .\Model\Components_ZFTMQEHP\Component_RIXFSVSY\Config_NWJYUSSR\
+echo **********************************
+echo Building WindowsInstallHelper
+echo **********************************
 make %1 
+@if %errorlevel% NEQ 0 goto ERROR_MAKE
 popd
 pushd .\Model\Components_ZLRQZKQB\Component_DJLKKOUQ\Config_DKQBNWAT\
+echo **********************************
+echo Building StateChartCoderVSM
+echo **********************************
 make %1 
+@if %errorlevel% NEQ 0 goto ERROR_MAKE
 popd
 pushd .\Model\Components_ZLRQZKQB\Component_LUAJBDMB\Config_UDUMBDGY\
+echo **********************************
+echo Building StateChartCoderWx
+echo **********************************
 make %1 
+@if %errorlevel% NEQ 0 goto ERROR_MAKE
 popd
 pushd .\Model\Components_ZLRQZKQB\Component_StateChartCoderCdSys\Config_WEYFLKNX\
+echo **********************************
+echo Building StateChartCoderCdSys
+echo **********************************
 make %1 
+@if %errorlevel% NEQ 0 goto ERROR_MAKE
 popd
+echo **********************************
+echo Building Installer Executable
+echo **********************************
 call iscc Astade_REA.iss
-@if errorlevel 1 goto ERROR
-goto END
+@if %errorlevel% NEQ 0 goto ERROR
+@goto END
+:ERROR_MAKE
+echo ##################################
+@echo MAKE failed failed
+@goto END
 :ERROR
-echo Building installer failed
+echo ##################################
+@echo Building installer failed
 :END
+popd
