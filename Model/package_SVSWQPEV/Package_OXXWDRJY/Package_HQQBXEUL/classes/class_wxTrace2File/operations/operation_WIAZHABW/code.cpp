ca183f47396c49ced6115770ef9892cbdee91deb
Anders Larsen <al@alarsen.net>
2008-04-16 19:57:41 +0000

    Tracing of the trace-classes disabled
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@1134 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/package_SVSWQPEV/Package_OXXWDRJY/Package_HQQBXEUL/classes/class_wxTrace2File/operations/operation_WIAZHABW/code.cpp
+++ Model/package_SVSWQPEV/Package_OXXWDRJY/Package_HQQBXEUL/classes/class_wxTrace2File/operations/operation_WIAZHABW/code.cpp
@@ -1,23 +1,24 @@
 #define LOG_OUT_BUF_SIZE 1024
-	// platz für den Ausgabestring
-	char OutBuffer[LOG_OUT_BUF_SIZE];
-	memset(OutBuffer,0,LOG_OUT_BUF_SIZE);
-	int nRet;
 
-	// Argumenteliste vür eine variable Anzahl von
-	// Funktionsargumenten erzeugen
-	va_list ap;
-	va_start(ap, fmt);
+// platz für den Ausgabestring
+char OutBuffer[LOG_OUT_BUF_SIZE];
+memset(OutBuffer,0,LOG_OUT_BUF_SIZE);
+int nRet;
 
-	// Ausgeben mit Lib-Funktion
-	nRet=vsprintf(OutBuffer, fmt, ap );
+// Argumenteliste vür eine variable Anzahl von
+// Funktionsargumenten erzeugen
+va_list ap;
+va_start(ap, fmt);
 
-	// Argumenteliste aufräumen
-	va_end( ap);              /* Reset variable arguments.      */
-	// ausgeben
-	OutBuffer[nRet]=0;
-	std::string s(OutBuffer);
+// Ausgeben mit Lib-Funktion
+nRet=vsprintf(OutBuffer, fmt, ap);
+// Argumenteliste aufräumen
+va_end( ap);
 
-	if (!pause_output && ms_ofile.is_open())
-		ms_ofile  << "#" << s << std::endl;
-	return nRet;
+// ausgeben
+OutBuffer[nRet]=0;
+std::string s(OutBuffer);
+
+if (!pause_output && ms_ofile.is_open())
+	ms_ofile  << "#" << s << std::endl;
+return nRet;
--- Model/package_SVSWQPEV/Package_OXXWDRJY/Package_HQQBXEUL/classes/class_wxTrace2File/prolog.cpp
+++
@@ -1,3 +0,0 @@
-#ifdef _TRACE_
-	#undef _TRACE_
-#endif
