8efed6ddd6392100ce7e78046e200de0b7a4f48c
Thomas Spitzer <projektleiter@astade.de>
2008-05-21 20:02:32 +0000

    If you modify the generated code, it is visualized also at the class, now.
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@1147 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/package_SVSWQPEV/package_YGKSEMEQ/Package_MOGAHMXN/classes/class_XMCTNSGN/operations/operation_AOYTGKQW/code.cpp
+++ Model/package_SVSWQPEV/package_YGKSEMEQ/Package_MOGAHMXN/classes/class_XMCTNSGN/operations/operation_AOYTGKQW/code.cpp
@@ -3,18 +3,43 @@ if (indexBase == -1) // not yet initialized
 
 int ret = indexBase;
 
+if (static_cast<AdeClass*>(myModelElement)->GetIsCCoded())
+	ret += 5;
+
+wxFileName aName = static_cast<AdeClass*>(myModelElement)->GetImpFileName();
+
+if (aName.FileExists())
+{   
+    wxDateTime access,mod,create;
+    aName.GetTimes(&access,&mod,&create);
+    if (mod > create)
+    {
+        ret += 4;
+        return ret;
+    }
+}
+
+aName.SetExt("h");
+
+if (aName.FileExists())
+{   
+    wxDateTime access,mod,create;
+    aName.GetTimes(&access,&mod,&create);
+    if (mod > create)
+    {
+        ret += 4;
+        return ret;
+    }
+}
+
 if (static_cast<AdeClass*>(myModelElement)->GetIsLibClass())
 	ret += 3;
-
-if (static_cast<AdeClass*>(myModelElement)->GetIsInActiveComponent())
+else if (static_cast<AdeClass*>(myModelElement)->GetIsInActiveComponent())
 {
 	if (static_cast<AdeClass*>(myModelElement)->GetImpGenerationTime() >= static_cast<AdeClass*>(myModelElement)->GetModificationTime())
 		ret += 1;
 	else
 		ret += 2;
 }
-
-if (static_cast<AdeClass*>(myModelElement)->GetIsCCoded())
-	ret += 4;
 
 return ret;
