877d3da05d38ef9efa9b80c0185715440dbc169d
Thomas Spitzer <projektleiter@astade.de>
2008-05-24 20:38:55 +0000

    Icons change correct when roundtripping
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@1151 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/package_SVSWQPEV/package_YGKSEMEQ/package_TLNCNYLI/classes/class_ODHKSPMR/operations/operation_LZLWFMET/code.cpp
+++ Model/package_SVSWQPEV/package_YGKSEMEQ/package_TLNCNYLI/classes/class_ODHKSPMR/operations/operation_LZLWFMET/code.cpp
@@ -9,70 +9,43 @@ component.SetExt("c");
 if (!component.FileExists())
     component.SetExt("cpp");
 
-if (!component.FileExists())
-    return;
-
+for (int i=0; i<2; i++)
 {
-    wxTextFile aTextFile(component.GetFullPath());
-    aTextFile.Open();
-
-    for ( wxString str = aTextFile.GetFirstLine(); !aTextFile.Eof(); str = aTextFile.GetNextLine() )
+    if (component.FileExists())
     {
-        long end;
-        if (str.Find(_T("//[")) == 0 && (end = str.Find(_T("]"))) > 3)
+        wxTextFile aTextFile(component.GetFullPath());
+        aTextFile.Open();
+
+        for ( wxString str = aTextFile.GetFirstLine(); !aTextFile.Eof(); str = aTextFile.GetNextLine() )
         {
-            wxTextFile outFile(str.Mid(3, end - 3));
-            outFile.Open();
-            outFile.Clear();
-            unsigned int search = aTextFile.GetCurrentLine()+1;
-            while (search < aTextFile.GetLineCount() && (aTextFile[search].Find(_T("//[")) != 0))
+            long end;
+            if (str.Find(_T("//[")) == 0 && (end = str.Find(_T("]"))) > 3)
             {
-                wxString aLine = aTextFile[search];
-                
-                if (aLine.Find("\t")==0)
-                    aLine.Remove(0,1);
-                else if (aLine.Find("    ")==0)
-                    aLine.Remove(0,4);
-                
-                outFile.AddLine(aLine);
-                search++;
+                wxTextFile outFile(str.Mid(3, end - 3));
+                outFile.Open();
+                outFile.Clear();
+                unsigned int search = aTextFile.GetCurrentLine()+1;
+                while (search < aTextFile.GetLineCount() && (aTextFile[search].Find(_T("//[")) != 0))
+                {
+                    wxString aLine = aTextFile[search];
+                    
+                    if (aLine.Find("\t")==0)
+                        aLine.Remove(0,1);
+                    else if (aLine.Find("    ")==0)
+                        aLine.Remove(0,4);
+                    
+                    outFile.AddLine(aLine.Trim());
+                    search++;
+                }
+                while(outFile.GetLineCount()>0 && outFile.GetLastLine()==wxEmptyString)
+                    outFile.RemoveLine(outFile.GetCurrentLine());
+
+                outFile.Write();
             }
-            outFile.Write();
         }
+        component.SetExt("h");
     }
 }
 
-component.SetExt("h");
-
-if (!component.FileExists())
-    return;
-
-{
-    wxTextFile aTextFile(component.GetFullPath());
-    aTextFile.Open();
-
-    for ( wxString str = aTextFile.GetFirstLine(); !aTextFile.Eof(); str = aTextFile.GetNextLine() )
-    {
-        long end;
-        if (str.Find(_T("//[")) == 0 && (end = str.Find(_T("]"))) > 3)
-        {
-            wxTextFile outFile(str.Mid(3, end - 3));
-            outFile.Open();
-            outFile.Clear();
-            unsigned int search = aTextFile.GetCurrentLine()+1;
-            while (search < aTextFile.GetLineCount() && (aTextFile[search].Find(_T("//[")) != 0))
-            {
-                wxString aLine = aTextFile[search];
-                
-                if (aLine.Find("\t")==0)
-                    aLine.Remove(0,1);
-                else if (aLine.Find("    ")==0)
-                    aLine.Remove(0,4);
-                
-                outFile.AddLine(aLine);
-                search++;
-            }
-            outFile.Write();
-        }
-    }
-}
+wxSleep(1);
+CallCoder(event);
