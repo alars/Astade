c2f4d0bfa881fdd9551c7bfcc15e224c9d0aa8b4
Anders Larsen <al@alarsen.net>
2008-09-09 13:57:06 +0000

    + scripts to build Debian (binary) package
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@1210 aaa30fad-34f9-0310-8aa9-877cc762ee2e

---
+++ build_deb.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -e
+
+BUILD=1
+
+DEBDIR=Source/Packages/deb
+VERSION=`awk -F '"' '{print $2}' Model/components_WGNBOFKH/component_KSEQOEET/manual/AstadeVersion.h`
+
+ARCH=`uname -m`
+if [ "${ARCH}" = "i686" ]; then
+	ARCH=i386
+elif [ "${ARCH}" = "x86_64" ]; then
+	ARCH=amd64
+fi
+
+rm -rf ${DEBDIR}/tmp
+mkdir -p ${DEBDIR}/tmp/{DEBIAN,usr/{bin,share/Astade,share/doc/astade}}
+sed -e s/VERSION-BUILD/${VERSION}-${BUILD}/ -e s/ARCH/${ARCH}/ \
+	<${DEBDIR}/control >${DEBDIR}/tmp/DEBIAN/control
+
+cp -p License.txt ${DEBDIR}/tmp/usr/share/doc/astade/copyright
+
+env prefix=`pwd`/${DEBDIR}/tmp/usr ./build_all.sh install
+
+pushd Source >/dev/null
+find Templates -name .svn -prune -o -type f -exec rsync -av --relative {} Packages/deb/tmp/usr/share/Astade/ \;
+popd >/dev/null
+
+pushd ${DEBDIR} >/dev/null
+fakeroot dpkg-deb --build tmp astade-${VERSION}-${BUILD}.deb
+popd >/dev/null
+
+rm -rf ${DEBDIR}/tmp
