2c3e9e3e0d91b03001e11a4652b3d4474b2f4442
Anders Larsen <al@alarsen.net>
2008-09-10 15:26:27 +0000

    added a script to pack a source tarball
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@1215 aaa30fad-34f9-0310-8aa9-877cc762ee2e

---
+++ pack-source.sh
@@ -0,0 +1,10 @@
+#!/bin/bash --
+
+VERSION=`awk -F '"' '{print $2}' Model/components_WGNBOFKH/component_KSEQOEET/manual/AstadeVersion.h`
+
+find ${PWD}/{Model,Source/{Icons,icons_png,Templates},build_all*,*.txt} \
+	\( -name .svn -o -name html \) -prune -o \
+	-type l -print -o \
+	-type f \! -name ".*" \! -name "*.[ao]" -a \
+	\( -name "*.sh" -o \! -perm +111 \) -print \
+	|pax -ws,^${PWD},astade-${VERSION}, |gzip >astade-${VERSION}.tar.gz
