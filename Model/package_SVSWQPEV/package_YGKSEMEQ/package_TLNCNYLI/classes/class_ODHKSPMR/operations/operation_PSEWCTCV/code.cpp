594f08924145dc392bf8d84e634159f858f1ca1e
Anders Larsen <al@alarsen.net>
2008-09-25 16:20:55 +0000

    Changed "New Model" to create the directory with the wxDirDialog instead of prompting for a name as a second step.
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@1261 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/package_SVSWQPEV/package_YGKSEMEQ/package_TLNCNYLI/classes/class_ODHKSPMR/operations/operation_PSEWCTCV/code.cpp
+++ Model/package_SVSWQPEV/package_YGKSEMEQ/package_TLNCNYLI/classes/class_ODHKSPMR/operations/operation_PSEWCTCV/code.cpp
@@ -1,43 +1,24 @@
 wxConfigBase* theConfig = wxConfigBase::Get();
+wxString workspace(wxDirSelector("Create Model", theConfig->Read("TreeView/ModelPath"), wxDD_DEFAULT_STYLE));
+if (workspace.empty())
+	return;
 
-wxString workspace = wxDirSelector("Select Astade Workspace", theConfig->Read("TreeView/ModelPath"), wxDD_DEFAULT_STYLE | wxDD_DIR_MUST_EXIST);
-
-if(workspace.empty()) return;
-
-//input dialog to get input
-wxString newModelName = wxGetTextFromUser("Enter a name of the New Astade Model");
-
-if(newModelName.empty()){
- return;
-}
-
-const wxString& dir = workspace + "/" + newModelName;
-
-//Create dir and default Model dir
-if(wxFileName::DirExists(dir)){
-	wxMessageBox("The new model directory name already exists");
+wxDir dir(workspace);
+if (dir.HasFiles() || dir.HasSubDirs())
+{
+	wxMessageBox("The new model directory is not empty!");
 	return;
 }
 
-const wxString& newFullModelName = dir + "/Model";
+wxFileName filename(workspace);
+theConfig->Write("TreeView/ModelPath", filename.GetFullPath());
+AddToRecentList(filename);
+theConfig->Write("TreeView/ActiveComponent", "none");
+theConfig->Flush();
+delete myTree;
+myTree = new AstadeTree(this);
 
-wxFileName::Mkdir(newFullModelName, 0777, wxPATH_MKDIR_FULL);
-
-wxFileName filename(newFullModelName);
-
-
-if (!newModelName.empty())
-{
-	wxConfigBase* theConfig = wxConfigBase::Get();
-	theConfig->Write("TreeView/ModelPath", filename.GetFullPath());
-	AddToRecentList(filename);
-	theConfig->Write("TreeView/ActiveComponent", "none");
-	theConfig->Flush();
-	delete myTree;
-	myTree =  new AstadeTree(this);
-
-	int x,y;
-	GetSize(&x, &y);
-	SetSize(x-1, y);
-	SetSize(x, y);
-}
+int x,y;
+GetSize(&x, &y);
+SetSize(x-1, y);
+SetSize(x, y);
