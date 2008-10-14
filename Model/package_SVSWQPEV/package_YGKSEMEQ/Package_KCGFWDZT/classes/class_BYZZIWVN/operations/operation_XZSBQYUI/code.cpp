44f608c7fa3392ec99be75f7018ef2dc2556fa8e
Andreas Becker <andi.becker@gmx.de>
2008-10-14 12:01:33 +0000

    more Space for Actions
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@1281 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/package_SVSWQPEV/package_YGKSEMEQ/Package_KCGFWDZT/classes/class_BYZZIWVN/operations/operation_XZSBQYUI/code.cpp
+++ Model/package_SVSWQPEV/package_YGKSEMEQ/Package_KCGFWDZT/classes/class_BYZZIWVN/operations/operation_XZSBQYUI/code.cpp
@@ -1,6 +1,6 @@
 wxBoxSizer* box = new wxBoxSizer(wxHORIZONTAL);
 
-box->Add(new wxStaticText(this, -1 ,"Action 1:",wxDefaultPosition,wxSize(50,15)));
+box->Add(new wxStaticText(this, -1 ,"Action 1:",wxDefaultPosition,wxSize(70,15)));
 
 action1 =  new wxTextCtrl(this, -1);
 action1->SetMaxLength(128);
@@ -8,7 +8,7 @@ action1->SetMaxLength(128);
 box->Add(action1,1);
 action1->SetValue(wxConfigBase::Get()->Read("Astade/Action1",wxEmptyString));
 
-box->Add(new wxStaticText(this, -1 ,"Action 2:",wxDefaultPosition,wxSize(50,15)),0,wxLEFT,10);
+box->Add(new wxStaticText(this, -1 ,"Action 2:",wxDefaultPosition,wxSize(70,15)),0,wxLEFT,10);
 
 action2 =  new wxTextCtrl(this, -1);
 action2->SetMaxLength(128);
@@ -22,7 +22,7 @@ topSizer->Add(box,0,wxEXPAND|wxRIGHT|wxLEFT|wxTOP,10);
 
 box = new wxBoxSizer(wxHORIZONTAL);
 
-box->Add(new wxStaticText(this, -1 ,"Action 3:",wxDefaultPosition,wxSize(50,15)));
+box->Add(new wxStaticText(this, -1 ,"Action 3:",wxDefaultPosition,wxSize(70,15)));
 
 action3 =  new wxTextCtrl(this, -1);
 action3->SetMaxLength(128);
@@ -30,7 +30,7 @@ action3->SetMaxLength(128);
 box->Add(action3,1);
 action3->SetValue(wxConfigBase::Get()->Read("Astade/Action3",wxEmptyString));
 
-box->Add(new wxStaticText(this, -1 ,"Action 4:",wxDefaultPosition,wxSize(50,15)),0,wxLEFT,10);
+box->Add(new wxStaticText(this, -1 ,"Action 4:",wxDefaultPosition,wxSize(70,15)),0,wxLEFT,10);
 
 action4 =  new wxTextCtrl(this, -1);
 action4->SetMaxLength(128);
