209079a3b0628317f9bcde9c5f15a375b2ee2bba
Jonatan Antoni <jantoni@web.de>
2008-12-06 22:01:12 +0000

    Added shell script to wrap around the cppcoder. Generated files are only placed to the auto folder, if they have changes. This saves a lot of compile time.
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@1345 aaa30fad-34f9-0310-8aa9-877cc762ee2e

---
+++ Source/Templates/Scripts/Linux/CppGenerator.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+echo "CppGenerator.sh $1 $2 $3" > ~/tmp/CppGenerator.log
+
+TargetDir=`dirname $2`
+TargetFile=`basename $2 | cut -d'.' -f1`
+Target=${TargetDir}/${TargetFile}
+Temp=/tmp/${TargetFile}
+
+echo "CppGenerator $1 ${Temp}.c $3" >> ~/tmp/CppGenerator.log
+CppGenerator $1 ${Temp}.c $3
+
+if `diff -I ".*@date.*UTC.*" ${Temp}.cpp ${Target}.cpp`; then
+	echo "rm ${Temp}.cpp" >> ~/tmp/CppGenerator.log
+	rm ${Temp}.cpp
+else
+	echo "mv ${Temp}.cpp ${Target}.cpp" >> ~/tmp/CppGenerator.log
+	mv ${Temp}.cpp ${Target}.cpp
+fi
+	
+if `diff -I ".*@date.*UTC.*" ${Temp}.h ${Target}.h `; then
+	echo "rm ${Temp}.h" >> ~/tmp/CppGenerator.log
+	rm ${Temp}.h
+else
+	echo "mv ${Temp}.h ${Target}.h" >> ~/tmp/CppGenerator.log
+	mv ${Temp}.h ${Target}.h
+fi
+
+exit 0;
