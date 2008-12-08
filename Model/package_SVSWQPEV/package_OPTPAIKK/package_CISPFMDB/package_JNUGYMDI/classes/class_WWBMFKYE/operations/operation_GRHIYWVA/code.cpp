aaeb79e33c1be4b566265e790ca70bedf48054b1
Jonatan Antoni <jantoni@web.de>
2008-12-08 16:04:20 +0000

    enabled the lib class folder to be a universal drop target
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@1351 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/package_SVSWQPEV/package_OPTPAIKK/package_CISPFMDB/package_JNUGYMDI/classes/class_WWBMFKYE/operations/operation_GRHIYWVA/code.cpp
+++ Model/package_SVSWQPEV/package_OPTPAIKK/package_CISPFMDB/package_JNUGYMDI/classes/class_WWBMFKYE/operations/operation_GRHIYWVA/code.cpp
@@ -1,11 +1,19 @@
 bool retVal = false;
 
-switch(aElement.GetType() & ITEM_TYPE_MASK)
+if(IsLibClass())
 {
-	case ITEM_IS_ATTRIBUTES:
-	case ITEM_IS_OPERATIONS:
-	case ITEM_IS_TYPES:	
-		retVal = true;
+	// TODO: Is this really a good idea? For now it is okay, but...
+	retVal = true;
+}
+else
+{
+	switch(aElement.GetType() & ITEM_TYPE_MASK)
+	{
+		case ITEM_IS_ATTRIBUTES:
+		case ITEM_IS_OPERATIONS:
+		case ITEM_IS_TYPES:	
+			retVal = true;
+	}
 }
 
 return retVal;
