c2022211effaf74ee44b49667f0ffe50b872810a
Jonatan Antoni <jantoni@web.de>
2008-12-08 11:14:30 +0000

    Bugfix CppGenerator.bat
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@1349 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Source/Templates/Scripts/Windows/CppGenerator.bat
+++ Source/Templates/Scripts/Windows/CppGenerator.bat
@@ -6,21 +6,20 @@ time /T >> %TEMP%\CppGenerator.log
 echo Called with 'CppGenerator.bat %1 %2 %3' >> %TEMP%\CppGenerator.log
 echo Running 'CppGenerator.exe %1 %TEMP%\%~n2.c' >> %TEMP%\CppGenerator.log
 C:\Programme\Astade\CppGenerator.exe %1 %TEMP%\%~n2.c
-if not errorlevel 0 goto end
+if not %errorlevel%==0 goto end
 
 :cppfile
 echo diff -I ".*@date.*UTC.*" -N %TEMP%\%~n2.cpp %~d2%~p2%~n2.cpp >> %TEMP%\CppGenerator.log
 diff -I ".*@date.*UTC.*" -N %TEMP%\%~n2.cpp %~d2%~p2%~n2.cpp >> %TEMP%\CppGenerator.log
-if errorlevel 0 goto hfile
+if %errorlevel%==0 goto hfile
 echo copy /y %TEMP%\%~n2.cpp %~d2%~p2%~n2.cpp >> %TEMP%\CppGenerator.log
 copy /y %TEMP%\%~n2.cpp %~d2%~p2%~n2.cpp
 
 :hfile
 echo diff -I ".*@date.*UTC.*" -N %TEMP%\%~n2.h %~d2%~p2%~n2.h  >> %TEMP%\CppGenerator.log
 diff -I ".*@date.*UTC.*" -N %TEMP%\%~n2.h %~d2%~p2%~n2.h  >> %TEMP%\CppGenerator.log
-if errorlevel 0 goto end
+if %errorlevel%==0 goto end
 echo copy /y %TEMP%\%~n2.h %~d2%~p2%~n2.h >> %TEMP%\CppGenerator.log
 copy /y %TEMP%\%~n2.h %~d2%~p2%~n2.h
-
 
 :end
