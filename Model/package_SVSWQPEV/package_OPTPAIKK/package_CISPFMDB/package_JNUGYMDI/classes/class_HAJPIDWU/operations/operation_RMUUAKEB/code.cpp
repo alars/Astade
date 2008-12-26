c97ac853c951e7b6cc019391983ff04fd92ffef4
Thomas Spitzer <projektleiter@astade.de>
2008-12-26 18:03:08 +0000

    Moved some functionality into base class and fixed a bug when searching whole words.
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@1401 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/package_SVSWQPEV/package_OPTPAIKK/package_CISPFMDB/package_JNUGYMDI/classes/class_HAJPIDWU/operations/operation_RMUUAKEB/code.cpp
+++ Model/package_SVSWQPEV/package_OPTPAIKK/package_CISPFMDB/package_JNUGYMDI/classes/class_HAJPIDWU/operations/operation_RMUUAKEB/code.cpp
@@ -1,16 +1,10 @@
 if (!options.isSet(AdeSearch::Attribute))
 	return AdeSearch::notfound;
 
-if (options.isSet(AdeSearch::Name) && options.RegEx().Matches(GetName()))
+if (options.isSet(AdeSearch::theType) && options.RegEx().Matches(" "+GetCodingType()+" "))
 	return AdeSearch::found;
 
-if (options.isSet(AdeSearch::Default) && options.RegEx().Matches(GetDefault()))
+if (options.isSet(AdeSearch::Default) && options.RegEx().Matches(" "+GetDefault()+" "))
 	return AdeSearch::found;
 
-if (options.isSet(AdeSearch::Description) && options.RegEx().Matches(GetDescription()))
-	return AdeSearch::found;
-
-if (options.isSet(AdeSearch::theType) && options.RegEx().Matches(GetCodingType()))
-	return AdeSearch::found;
-
-return AdeSearch::notfound;
+return AdeFileElement::Search(options);
--- Model/package_SVSWQPEV/package_OPTPAIKK/package_CISPFMDB/package_UPBJDSOK/classes/class_NQRUBFXN/operations/operation_BZYQOEHK/code.cpp
+++ Model/package_SVSWQPEV/package_OPTPAIKK/package_CISPFMDB/package_UPBJDSOK/classes/class_NQRUBFXN/operations/operation_BZYQOEHK/code.cpp
@@ -1,1 +1,7 @@
+if (options.isSet(AdeSearch::Name) && options.RegEx().Matches(" "+GetName()+" "))
+	return AdeSearch::found;
+
+if (options.isSet(AdeSearch::Description) && options.RegEx().Matches(" "+GetDescription()+" "))
+	return AdeSearch::found;
+
 return AdeSearch::notfound;
