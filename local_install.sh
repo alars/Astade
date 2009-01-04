9526b1523fa8c65e320f899b2f9094976a1df39d
Andreas Pflanz <apflanz@gmx.de>
2009-01-04 13:53:13 +0000

    simple install script to quick-test the Astade suite on a local Linux account (installs to ~/bin/  )
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@1418 aaa30fad-34f9-0310-8aa9-877cc762ee2e

---
+++ local_install.sh
@@ -0,0 +1,75 @@
+#!/bin/bash -e
+
+cd Model
+echo "**********************************"
+echo "	bin2c"
+echo "**********************************"
+cp Components_ZFTMQEHP/Component_UIGKBCCK/Config_HZEMYJPW/bin2c ~/bin/
+#echo "**********************************"
+#echo "	Astade API"
+#echo "**********************************"
+#cp  components_WGNBOFKH/Component_AstadeAPI/Config_Linux_StatLib/AstadeAPI ~/bin/
+#echo "**********************************"
+#echo "	TraceLib"
+#echo "**********************************"
+#cp  Components_AstadeFramework/Component_TraceLib/Config_Linux_StatLib/TraceLib ~/bin/
+echo "**********************************"
+echo "	AstadeGenerate"
+echo "**********************************"
+cp  components_WGNBOFKH/Component_XZAWFDAN/Config_ZUJGZNUN/AstadeGenerate ~/bin/
+echo "**********************************"
+echo "	CGenerator"
+echo "**********************************"
+cp  components_WGNBOFKH/Component_CGenerator/Linux_Debug/CGenerator ~/bin/
+echo "**********************************"
+echo "	CppGenerator"
+echo "**********************************"
+cp  components_WGNBOFKH/Component_CppGenerator/Config_PPWOWKVA/CppGenerator ~/bin/
+echo "**********************************"
+echo "	StateChartCoder"
+echo "**********************************"
+cp  components_WGNBOFKH/Component_RAVGAUPS/Config_AXJYPYCU/StateChartCoder ~/bin/
+echo "**********************************"
+echo "	StateChartCoderC"
+echo "**********************************"
+cp  components_WGNBOFKH/Component_FOGYFECV/Config_HEYGDBJI/StateChartCoderC ~/bin/
+echo "**********************************"
+echo "	StateChartCoderVSM"
+echo "**********************************"
+cp  Components_ZLRQZKQB/Component_DJLKKOUQ/Config_XXQJRNKA/StateChartCoderVSM ~/bin/
+echo "**********************************"
+echo "	StateChartCoderWx"
+echo "**********************************"
+cp  Components_ZLRQZKQB/Component_LUAJBDMB/Config_Linux_Debug/StateChartCoderWx ~/bin/
+echo "**********************************"
+echo "	Astade"
+echo "**********************************"
+cp  components_WGNBOFKH/component_KSEQOEET/config_TLQNHUAH/Astade ~/bin/
+echo "**********************************"
+echo "	ResourceEdit"
+echo "**********************************"
+cp  components_WGNBOFKH/Component_BZBVDSZR/Config_SHHQELQC/ResourceEdit ~/bin/
+echo "**********************************"
+echo "	ActivityChartDrawer"
+echo "**********************************"
+cp  components_WGNBOFKH/Component_YIIYAPSY/Config_Linux_Debug/ActivityChartDrawer ~/bin/
+echo "**********************************"
+echo "	OMDgenerator"
+echo "**********************************"
+cp  components_WGNBOFKH/Component_OMDgenerator/Config_NDLLNZZZ/OMDgenerator ~/bin/
+echo "**********************************"
+echo "	StateChartDrawer"
+echo "**********************************"
+cp  components_WGNBOFKH/Component_EIFKVZMS/Config_NMHWSCLU/StateChartDrawer ~/bin/
+echo "**********************************"
+echo "	UseCaseEdit"
+echo "**********************************"
+cp  components_WGNBOFKH/Component_IXHOAQJM/Config_JQTGOPLR/UseCaseEdit ~/bin/
+echo "**********************************"
+echo "	Trace2UML"
+echo "**********************************"
+cp  Components_LGPLTGDP/Component_TYWOOIDA/Config_MMRNSTXM/Trace2UML ~/bin/
+echo "**********************************"
+echo "	instrument"
+echo "**********************************"
+cp  Components_LGPLTGDP/Component_instrument/Config_Linux/instrument ~/bin/
