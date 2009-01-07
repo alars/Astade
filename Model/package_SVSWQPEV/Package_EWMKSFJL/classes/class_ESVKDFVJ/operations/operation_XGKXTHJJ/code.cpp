f42a2c6e5eda2555bd1e081a48ea9ac2d98f58d6
Thomas Spitzer <projektleiter@astade.de>
2009-01-07 08:05:25 +0000

    GraphLibrary: The size of the virtual graph window is set correct
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@1427 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/package_SVSWQPEV/Package_EWMKSFJL/classes/class_ESVKDFVJ/operations/operation_XGKXTHJJ/code.cpp
+++ Model/package_SVSWQPEV/Package_EWMKSFJL/classes/class_ESVKDFVJ/operations/operation_XGKXTHJJ/code.cpp
@@ -2,3 +2,5 @@ wxString aLabel;
 aLabel.Printf("%d x %d pixel",xPixelSlider->GetValue(), yPixelSlider->GetValue());
 
 sliderSizer_staticbox->SetLabel(aLabel);
+
+graphicPanel->SetVirtualSize(xPixelSlider->GetValue(), yPixelSlider->GetValue());
