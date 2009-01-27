3d932598e380bf246ab93b8d31daa50a56c42db6
Anders Larsen <al@alarsen.net>
2009-01-27 14:54:11 +0000

    ResourceEdit: Automatic sizing further improved (hopefully without breaking too many Windows)
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@1483 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/package_SVSWQPEV/package_YGKSEMEQ/Package_KCGFWDZT/classes/class_BYZZIWVN/operations/operation_JMPWTDYB/code.cpp
+++ Model/package_SVSWQPEV/package_YGKSEMEQ/Package_KCGFWDZT/classes/class_BYZZIWVN/operations/operation_JMPWTDYB/code.cpp
@@ -103,5 +103,5 @@ if (wxConfigBase::Get()->Read("Astade/Type", &elementType));
     AddButtonSizer(topsizer,hasTraceLevel,canCCoded);
 	SetIcon();
 }
-SetMinSize(wxSize(450, 350));
+topsizer->SetMinSize(wxSize(450, 350));
 SetSizerAndFit(topsizer);
