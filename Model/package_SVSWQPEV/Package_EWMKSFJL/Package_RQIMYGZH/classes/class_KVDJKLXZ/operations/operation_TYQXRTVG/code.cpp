53b537c43f67d5102f0c09063861e2cbb5a68fa4
Thomas Spitzer <projektleiter@astade.de>
2009-03-27 06:53:59 +0000

    programmed the method "locate" in the new segment class
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@1527 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/package_SVSWQPEV/Package_EWMKSFJL/Package_RQIMYGZH/classes/class_KVDJKLXZ/operations/operation_TYQXRTVG/code.cpp
+++ Model/package_SVSWQPEV/Package_EWMKSFJL/Package_RQIMYGZH/classes/class_KVDJKLXZ/operations/operation_TYQXRTVG/code.cpp
@@ -1,1 +1,16 @@
-return unknown;
+glVector startToPoint = aVector-m_Start;
+
+if (startToPoint.ScalarProduct(m_Diff) <= 0)
+	return nearStart;
+
+glVector endToPoint = aVector-m_End;
+
+if (endToPoint.ScalarProduct(m_Diff) >= 0)
+	return nearEnd;
+
+glVector rotated = m_Diff.Rotate90Degree();
+
+if (rotated.ScalarProduct(startToPoint) <= 0)
+	return right;
+
+return left;
