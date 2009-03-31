cab7f14a9cbe793712c0cf527cb35c1c2a87076d
Thomas Spitzer <projektleiter@astade.de>
2009-03-31 09:47:02 +0000

    fixed some warnings
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@1549 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/package_SVSWQPEV/Package_EWMKSFJL/Package_KYPQHTJS/classes/class_AUHKWVJB/operations/operation_OQZBZOGA/code.cpp
+++ Model/package_SVSWQPEV/Package_EWMKSFJL/Package_KYPQHTJS/classes/class_AUHKWVJB/operations/operation_OQZBZOGA/code.cpp
@@ -8,7 +8,7 @@ int w,h;
 myParent->GetVirtualSize(&w,&h);
 
 AddSpeed(myGravityArea->GetBorderForce(w,h));
-AddSpeed(myGravityArea->GetRelationForce());
+AddSpeed(myGravityArea->GetForceFromAll());
 
 for (std::set<glEdge*>::iterator it = myEdges.begin(); it != myEdges.end(); it++)
 	AddSpeed((*it)->NodeForce(*this));
