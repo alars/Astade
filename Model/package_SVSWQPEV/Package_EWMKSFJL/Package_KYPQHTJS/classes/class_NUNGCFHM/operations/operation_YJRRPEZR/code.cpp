b7c3db0ce7317eaa241c97717c71c82d9628cfc3
Thomas Spitzer <projektleiter@astade.de>
2009-03-31 10:11:15 +0000

    moved functionality
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@1550 aaa30fad-34f9-0310-8aa9-877cc762ee2e

---
+++ Model/package_SVSWQPEV/Package_EWMKSFJL/Package_KYPQHTJS/classes/class_NUNGCFHM/operations/operation_YJRRPEZR/code.cpp
@@ -0,0 +1,10 @@
+glVector otherBorder = otherArea.absCalculateNearestToArea(this);
+glVector myBorder = absCalculateNearestToArea(&otherArea);
+
+glVector direction = (myBorder - otherBorder);
+double disance = direction.Mod();
+
+glVector ret = direction.Dir();
+ret *= 60.0 / (disance*disance);
+
+return ret;
--- Model/package_SVSWQPEV/Package_EWMKSFJL/Package_KYPQHTJS/classes/class_OJLARSMV/operations/operation_EBHJNKTD/code.cpp
+++ Model/package_SVSWQPEV/Package_EWMKSFJL/Package_KYPQHTJS/classes/class_OJLARSMV/operations/operation_EBHJNKTD/code.cpp
@@ -3,20 +3,7 @@ glVector forceSum;
 for (std::set<glGravityArea*>::iterator it = objectList.begin(); it != objectList.end(); it++)
 {
 	if ((*it) != this)
-	{
-		glVector myBorder = (*it)->absCalculateNearestToArea(this);
-		glVector itBorder = absCalculateNearestToArea(*it);
-
-		glVector direction = (itBorder - myBorder);
-		double disance = direction.Mod();
-
-		glVector help = direction.Dir();
-		help *= 1.0 / (disance*disance);
-
-		forceSum += help;
-	}
+		forceSum += GetForceFromArea(*(*it));
 }
-
-forceSum *= 60;
 
 return forceSum;
