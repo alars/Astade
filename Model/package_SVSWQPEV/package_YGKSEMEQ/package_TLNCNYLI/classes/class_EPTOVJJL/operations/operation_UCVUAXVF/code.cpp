9e0e959aaf976f7374b74e0aac002764302692eb
Thomas Spitzer <projektleiter@astade.de>
2009-04-06 19:00:24 +0000

    Added a Splash screen
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@1564 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/package_SVSWQPEV/package_YGKSEMEQ/package_TLNCNYLI/classes/class_EPTOVJJL/operations/operation_UCVUAXVF/code.cpp
+++ Model/package_SVSWQPEV/package_YGKSEMEQ/package_TLNCNYLI/classes/class_EPTOVJJL/operations/operation_UCVUAXVF/code.cpp
@@ -1,5 +1,16 @@
-wxConfigBase::Set(new wxFileConfig("Astade.ini"));
+#include "splash_png.h"
 wxInitAllImageHandlers();
 
+wxMemoryInputStream istream(splash_png, sizeof splash_png);
+wxImage myimage(istream, wxBITMAP_TYPE_PNG);
+
+wxSplashScreen splash(wxBitmap(myimage),
+          wxSPLASH_CENTRE_ON_SCREEN|wxSPLASH_TIMEOUT,
+          6000, NULL, -1, wxDefaultPosition, wxDefaultSize,
+          wxSIMPLE_BORDER|wxSTAY_ON_TOP);
+
+wxConfigBase::Set(new wxFileConfig("Astade.ini"));
+
 InitializeFrame();
+
 return true;
