9327af245b8cca7e1cf5e538ac6c71ab46736bf6
Thomas Spitzer <projektleiter@astade.de>
2009-04-09 12:21:39 +0000

    implemented "isInElement" for straight edges in GraphLib
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@1572 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/package_SVSWQPEV/Package_EWMKSFJL/Package_HMPGOZXK/classes/class_GMGNTRPY/operations/operation_MRDGNTJD/code.cpp
+++ Model/package_SVSWQPEV/Package_EWMKSFJL/Package_HMPGOZXK/classes/class_GMGNTRPY/operations/operation_MRDGNTJD/code.cpp
@@ -1,2 +1,3 @@
 dc.SetPen(*wxThePenList->FindOrCreatePen(wxTheColourDatabase->Find("BLUE"),1,wxSOLID));
+
 dc.DrawLine(absDrawnStartPoint.xCoord(),absDrawnStartPoint.yCoord(),absDrawnEndPoint.xCoord(),absDrawnEndPoint.yCoord());
---
+++ Model/package_SVSWQPEV/Package_EWMKSFJL/Package_KYPQHTJS/classes/class_USZZVDHV/operations/operation_AQWKAILM/code.cpp
@@ -0,0 +1,1 @@
+return ((absGetBotderPoint(position)-position).Mod() < 2)
