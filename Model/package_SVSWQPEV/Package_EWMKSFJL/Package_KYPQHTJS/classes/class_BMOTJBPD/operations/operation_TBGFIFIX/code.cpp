2048e5ea68452a16e816a2e9cd9268c46e6698c1
Thomas Spitzer <projektleiter@astade.de>
2009-04-12 06:34:16 +0000

    fixed the left border bug
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@1578 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/package_SVSWQPEV/Package_EWMKSFJL/Package_KYPQHTJS/classes/class_BMOTJBPD/operations/operation_TBGFIFIX/code.cpp
+++ Model/package_SVSWQPEV/Package_EWMKSFJL/Package_KYPQHTJS/classes/class_BMOTJBPD/operations/operation_TBGFIFIX/code.cpp
@@ -1,12 +1,22 @@
 glVector direction = (absDirection - absGetCenterPoint()).Dir();
 
 if ((direction.m_y < 0.0001) && (direction.m_y > -0.0001))
-	return absGetCenterPoint() +  glVector(my_X_Radius, 0);
+{
+    if (direction.m_x > 0)
+    	return absGetCenterPoint() +  glVector(my_X_Radius, 0);
+    else
+    	return absGetCenterPoint() +  glVector(-my_X_Radius, 0);
+}
 
 double m = direction.m_x/direction.m_y;
 
 if ((m < 0.0001) && (m > -0.0001))
-	return absGetCenterPoint() +  glVector(0, my_Y_Radius);
+{
+    if (direction.m_y > 0)
+	    return absGetCenterPoint() +  glVector(0, my_Y_Radius);
+    else
+	    return absGetCenterPoint() +  glVector(0, -my_Y_Radius);
+}
 
 double a2 = my_X_Radius * my_X_Radius;
 double b2 = my_Y_Radius * my_Y_Radius;
