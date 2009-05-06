596965cf192bf3df28e1857381d4b50a62cd5215
Thomas Spitzer <projektleiter@astade.de>
2009-05-06 08:34:48 +0000

    Components can have a label
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@1620 aaa30fad-34f9-0310-8aa9-877cc762ee2e

---
+++ Model/package_SVSWQPEV/Package_EWMKSFJL/Package_BTYQFKAB/classes/class_LHFEUSXN/operations/operation_EVEVWRCY/code.cpp
@@ -0,0 +1,12 @@
+wxString aString = wxGetTextFromUser("","Label",myLabel,myParent);
+aString.Trim(false);
+aString.Trim(true);
+aString.Replace("\t"," ");
+aString.Replace("\n"," ");
+aString.Replace("  "," ");
+if (aString != myLabel)
+{
+    myLabel = aString;
+    glChangeNotifier::Notify();
+    Refresh();
+}
--- Model/package_SVSWQPEV/Package_EWMKSFJL/Package_BTYQFKAB/classes/class_LHFEUSXN/operations/operation_VQNBWQLS/code.cpp
+++ Model/package_SVSWQPEV/Package_EWMKSFJL/Package_BTYQFKAB/classes/class_LHFEUSXN/operations/operation_VQNBWQLS/code.cpp
@@ -1,3 +1,5 @@
+const int middleBorder = 4;
+
 if (isMouseOver())
 	dc.SetPen(*wxThePenList->FindOrCreatePen(wxTheColourDatabase->Find("RED"),2,wxSOLID));
 else
@@ -20,4 +22,54 @@ dc.DrawRectangle(innerEdge.xCoord(),innerEdge.yCoord(),8,4);
 
 innerEdge += glVector(0,5);
 dc.DrawRectangle(innerEdge.xCoord(),innerEdge.yCoord(),8,4);
+
+wxCoord x,y;
+dc.GetTextExtent(myLabel,&x,&y);
+
+// Fits in one line?
+if (x < (2 * my_XRadius) - middleBorder)
+{
+    dc.DrawText(myLabel,absGetDrawPosition().m_x-(x/2),absGetDrawPosition().m_y - (y/2));
+    return;
+}
+
+// Fits in two lines?
+int first = 0;
+
+do
+{
+    dc.GetTextExtent(myLabel.Mid(0,++first),&x,&y);
+} while (x < (2 * my_XRadius) - middleBorder);
+
+wxString firstPart = myLabel.Mid(0,--first);
+
+if (firstPart.Find(' ',true) > (int)(firstPart.size()/2))
+    first = firstPart.Find(' ',true);
+else if (firstPart.Find('-',true) > (int)(firstPart.size()/2))
+    first = firstPart.Find('-',true)+1;
+
+firstPart = myLabel.Mid(0,first);
+firstPart.Trim(true);
+firstPart.Trim(false);
+dc.GetTextExtent(firstPart,&x,&y);
+
+wxString secondPart = myLabel.Mid(first);
+secondPart.Trim(true);
+secondPart.Trim(false);
+
+wxCoord x2,y2;
+dc.GetTextExtent(secondPart,&x2,&y2);
+
+if (x2 >= (2 * my_XRadius) - middleBorder)
+{
+    do
+    {
+        secondPart.RemoveLast();
+        dc.GetTextExtent(secondPart+"...",&x2,&y2);
+    } while(x2 >= (2 * my_XRadius) - middleBorder);
+    secondPart += "...";
+}
+
+dc.DrawText(firstPart,absGetDrawPosition().m_x-my_XRadius+(middleBorder/2),absGetDrawPosition().m_y - (y/2));
+dc.DrawText(secondPart,absGetDrawPosition().m_x-my_XRadius+(middleBorder/2),absGetDrawPosition().m_y + (y/2));
 
