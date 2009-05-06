7502377553660181172b09815477bfdc554360a2
Thomas Spitzer <projektleiter@astade.de>
2009-05-06 09:01:35 +0000

    You might change the label of interfaces
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@1621 aaa30fad-34f9-0310-8aa9-877cc762ee2e

---
+++ Model/package_SVSWQPEV/Package_EWMKSFJL/Package_BTYQFKAB/classes/class_QYZLEDJC/operations/operation_PASIAYSG/code.cpp
@@ -0,0 +1,12 @@
+wxString aString = wxGetTextFromUser("","Label",myFloatingLabel.myText,myParent);
+aString.Trim(false);
+aString.Trim(true);
+aString.Replace("\t"," ");
+aString.Replace("\n"," ");
+aString.Replace("  "," ");
+if (aString != myFloatingLabel.myText)
+{
+    myFloatingLabel.myText = aString;
+    glChangeNotifier::Notify();
+    Refresh();
+}
