f78587a2c3c088e7e21c91e7586b9eb1df942910
Thomas Spitzer <projektleiter@astade.de>
2009-05-06 12:08:19 +0000

    improved the zOrder problem. Not perfect, yet.
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@1623 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/package_SVSWQPEV/Package_EWMKSFJL/Package_BTYQFKAB/classes/class_QYZLEDJC/operations/operation_KERYXBQT/code.cpp
+++ Model/package_SVSWQPEV/Package_EWMKSFJL/Package_BTYQFKAB/classes/class_QYZLEDJC/operations/operation_KERYXBQT/code.cpp
@@ -12,7 +12,21 @@ else
     if (isMouseOver())
     {
 	    dc.SetPen(*wxThePenList->FindOrCreatePen(wxTheColourDatabase->Find("RED"),2,wxSOLID));
+    	dc.SetBrush(*wxTheBrushList->FindOrCreateBrush(wxTheColourDatabase->Find("RED")));
 			dc.DrawCircle(absGetDrawPosition().xCoord(),absGetDrawPosition().yCoord(),my_Radius);
+    }
+    else
+    {
+    	for (std::set<glEdge*>::iterator it = myEdges.begin(); it != myEdges.end(); it++)
+    	{
+    		if (dynamic_cast<glProvideInterface*>(*it) != 0)
+    		{
+	    		dc.SetPen(*wxThePenList->FindOrCreatePen(wxTheColourDatabase->Find("RED"),1,wxSOLID));
+    			dc.SetBrush(*wxTheBrushList->FindOrCreateBrush(wxTheColourDatabase->Find("WHITE")));
+					dc.DrawCircle(absGetDrawPosition().xCoord(),absGetDrawPosition().yCoord(),my_Radius);
+     			break;
+    		}
+    	}
     }
 }
 
--- Model/package_SVSWQPEV/Package_EWMKSFJL/Package_HMPGOZXK/classes/class_AOLVIWXG/operations/operation_WWMBZEAL/code.cpp
+++ Model/package_SVSWQPEV/Package_EWMKSFJL/Package_HMPGOZXK/classes/class_AOLVIWXG/operations/operation_WWMBZEAL/code.cpp
@@ -1,9 +1,6 @@
 if (isMouseOver())
 	dc.SetPen(*wxThePenList->FindOrCreatePen(wxTheColourDatabase->Find("RED"),2,wxSOLID));
 else
-    dc.SetPen(*wxThePenList->FindOrCreatePen(wxTheColourDatabase->Find("RED"),1,wxSOLID));
+  dc.SetPen(*wxThePenList->FindOrCreatePen(wxTheColourDatabase->Find("RED"),1,wxSOLID));
 
 dc.DrawLine(absDrawnStartPoint.xCoord(),absDrawnStartPoint.yCoord(),absDrawnEndPoint.xCoord(),absDrawnEndPoint.yCoord());
-
-glVector interfacePos = GetEndNode().absGetDrawPosition();
-dc.DrawCircle(interfacePos.xCoord(), interfacePos.yCoord(), 5);
