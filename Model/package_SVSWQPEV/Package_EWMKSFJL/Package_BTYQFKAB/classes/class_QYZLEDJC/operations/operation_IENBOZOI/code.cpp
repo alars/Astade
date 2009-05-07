c7d16fc21d1caf019eadb92a7826978b9c7ff3b6
Thomas Spitzer <projektleiter@astade.de>
2009-05-07 12:26:45 +0000

    the label position of interfaces is stored now
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@1627 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/package_SVSWQPEV/Package_EWMKSFJL/Package_BTYQFKAB/classes/class_QYZLEDJC/operations/operation_IENBOZOI/code.cpp
+++ Model/package_SVSWQPEV/Package_EWMKSFJL/Package_BTYQFKAB/classes/class_QYZLEDJC/operations/operation_IENBOZOI/code.cpp
@@ -1,3 +1,8 @@
 glNode::Save(configObject);
 configObject.Write("Label",myFloatingLabel.myText);
 configObject.Write("ClassName","interface");
+
+glVector lpos = myFloatingLabel.absGetDrawPosition() - absGetDrawPosition();
+
+configObject.Write("LabelPosX",lpos.xCoord());
+configObject.Write("LabelPosY",lpos.yCoord());
--- Model/package_SVSWQPEV/Package_EWMKSFJL/Package_BTYQFKAB/classes/class_QYZLEDJC/operations/operation_PTKYSWTL/code.cpp
+++ Model/package_SVSWQPEV/Package_EWMKSFJL/Package_BTYQFKAB/classes/class_QYZLEDJC/operations/operation_PTKYSWTL/code.cpp
@@ -1,3 +1,9 @@
+int x,y;
+configObject.Read("LabelPosX",&x);
+configObject.Read("LabelPosY",&y);
+
+myFloatingLabel.AddPosition(glVector(x,y-17));
+
 configObject.Read("Label",&myFloatingLabel.myText);
 myFloatingLabel.myText.Trim(false);
 myFloatingLabel.myText.Trim(true);
