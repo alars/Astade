c866c7e3144c0b145f01d3feaca829de9576574d
Thomas Spitzer <projektleiter@astade.de>
2009-05-11 12:13:05 +0000

    Load and save for Notes
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@1636 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/package_SVSWQPEV/Package_EWMKSFJL/Package_BTYQFKAB/classes/class_DHZQTXEH/operations/operation_EMDYZBTW/code.cpp
+++ Model/package_SVSWQPEV/Package_EWMKSFJL/Package_BTYQFKAB/classes/class_DHZQTXEH/operations/operation_EMDYZBTW/code.cpp
@@ -2,8 +2,17 @@ glNode::Save(configObject);
 configObject.Write("Label",myLabel);
 configObject.Write("ClassName","note");
 
+int attachCount = 0;
+
 for (std::set<glEdge*>::iterator it = myEdges.begin(); it != myEdges.end(); it++)
     if (&(*it)->GetStartNode() == this)
     {
-
+        glAttach* aAttach = dynamic_cast<glAttach*>(*it);
+        
+        if (aAttach)
+        {
+            wxString aString;
+            aString.Printf("Attach%03d",++attachCount);
+            configObject.Write(aString,aAttach->GetEndNode().id);
+        }
     }
--- Model/package_SVSWQPEV/Package_EWMKSFJL/Package_BTYQFKAB/classes/class_DHZQTXEH/operations/operation_OLWDQJCE/code.cpp
+++ Model/package_SVSWQPEV/Package_EWMKSFJL/Package_BTYQFKAB/classes/class_DHZQTXEH/operations/operation_OLWDQJCE/code.cpp
@@ -1,1 +1,19 @@
+int count = 1;
+
+wxString attachName;
+attachName.Printf("Attach%03d",count);
+
+while (configObject.Exists(attachName))
+{
+	int AttachID;
+	configObject.Read(attachName,&AttachID);
+	
+	glNode* partnerNode = glNode::getNodeById(AttachID);
+	
+	if (partnerNode)
+		new glAttach(myParent, *this, *partnerNode);
+	
+	count++;
+	attachName.Printf("Attach%03d",count);
+}
 
