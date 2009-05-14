1f5bac33df817f8c26aa263b548900a24c957ece
Thomas Spitzer <projektleiter@astade.de>
2009-05-14 08:08:47 +0000

    Note and component may have 3 line labels
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@1640 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/package_SVSWQPEV/Package_EWMKSFJL/Package_BTYQFKAB/classes/class_LHFEUSXN/operations/operation_VQNBWQLS/code.cpp
+++ Model/package_SVSWQPEV/Package_EWMKSFJL/Package_BTYQFKAB/classes/class_LHFEUSXN/operations/operation_VQNBWQLS/code.cpp
@@ -1,4 +1,5 @@
 const int middleBorder = 4;
+const int secondBorder = 20;
 
 if (isMouseOver())
 	dc.SetPen(*wxThePenList->FindOrCreatePen(wxTheColourDatabase->Find("RED"),2,wxSOLID));
@@ -60,16 +61,72 @@ secondPart.Trim(false);
 wxCoord x2,y2;
 dc.GetTextExtent(secondPart,&x2,&y2);
 
-if (x2 >= (2 * my_XRadius) - middleBorder)
+if (x2 < (2 * my_XRadius) - middleBorder)
+{
+	dc.DrawText(firstPart,absGetDrawPosition().m_x-my_XRadius+(middleBorder/2),absGetDrawPosition().m_y - (y/2));
+	dc.DrawText(secondPart,absGetDrawPosition().m_x-my_XRadius+(middleBorder/2),absGetDrawPosition().m_y + (y/2));
+	return;
+}
+
+// Fits in three lines?
+
+first = 0;
+
+do
+{
+    dc.GetTextExtent(myLabel.Mid(0,++first),&x,&y);
+} while (x < (2 * my_XRadius) - secondBorder);
+
+
+firstPart = myLabel.Mid(0,--first);
+
+if (firstPart.Find(' ',true) > (int)(firstPart.size()/2))
+    first = firstPart.Find(' ',true);
+else if (firstPart.Find('-',true) > (int)(firstPart.size()/2))
+    first = firstPart.Find('-',true)+1;
+
+firstPart = myLabel.Mid(0,first);
+firstPart.Trim(true);
+firstPart.Trim(false);
+dc.GetTextExtent(firstPart,&x,&y);
+
+int second = 0;
+
+do
+{
+    dc.GetTextExtent(myLabel.Mid(first,++second),&x2,&y2);
+} while (x2 < (2 * my_XRadius) - middleBorder);
+
+
+secondPart = myLabel.Mid(first,--second);
+
+if (secondPart.Find(' ',true) > (int)(secondPart.size()/2))
+    second = secondPart.Find(' ',true);
+else if (secondPart.Find('-',true) > (int)(secondPart.size()/2))
+    second = secondPart.Find('-',true)+1;
+
+secondPart = myLabel.Mid(first,second);
+secondPart.Trim(true);
+secondPart.Trim(false);
+dc.GetTextExtent(secondPart,&x2,&y2);
+
+wxString thirdPart = myLabel.Mid(first+second);
+thirdPart.Trim(true);
+thirdPart.Trim(false);
+wxCoord x3,y3;
+dc.GetTextExtent(thirdPart,&x3,&y3);
+
+if (x3 >= (2 * my_XRadius) - middleBorder)
 {
     do
     {
-        secondPart.RemoveLast();
-        dc.GetTextExtent(secondPart+"...",&x2,&y2);
-    } while(x2 >= (2 * my_XRadius) - middleBorder);
-    secondPart += "...";
+        thirdPart.RemoveLast();
+        dc.GetTextExtent(thirdPart+"...",&x3,&y3);
+    } while(x3 >= (2 * my_XRadius) - middleBorder);
+    thirdPart += "...";
 }
 
-dc.DrawText(firstPart,absGetDrawPosition().m_x-my_XRadius+(middleBorder/2),absGetDrawPosition().m_y - (y/2));
-dc.DrawText(secondPart,absGetDrawPosition().m_x-my_XRadius+(middleBorder/2),absGetDrawPosition().m_y + (y/2));
+dc.DrawText(firstPart,absGetDrawPosition().m_x-my_XRadius+(middleBorder/2),absGetDrawPosition().m_y - y -(y2/2));
+dc.DrawText(secondPart,absGetDrawPosition().m_x-my_XRadius+(middleBorder/2),absGetDrawPosition().m_y - (y2/2));
+dc.DrawText(thirdPart,absGetDrawPosition().m_x-my_XRadius+(middleBorder/2),absGetDrawPosition().m_y + (y2/2));
 
