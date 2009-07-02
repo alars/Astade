4ff268970050943e0c6db6b4eb23a2173d5ea5f8
Thomas Spitzer <projektleiter@astade.de>
2009-07-02 13:53:28 +0000

    Split astadegraphlib into a separate debian package. I've the reposoitory in this constallation in astade.homeip.net/apt, if you like to have a look at it.
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@1712 aaa30fad-34f9-0310-8aa9-877cc762ee2e

---
+++ build_astadegraphlib_deb.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -e
+
+BUILD=${1:-1}
+
+DEBDIR=Source/Packages/deb
+
+VERSION=`awk -F '"' '{print $2}' Model/components_WGNBOFKH/component_KSEQOEET/manual/AstadeVersion.h`
+
+ARCH=`uname -m`
+if [ "${ARCH}" = "i686" ]; then
+	ARCH=i386
+elif [ "${ARCH}" = "i586" ]; then
+	ARCH=i386
+elif [ "${ARCH}" = "x86_64" ]; then
+	ARCH=amd64
+fi
+
+rm -rf ${DEBDIR}/tmp
+mkdir -p ${DEBDIR}/tmp/{DEBIAN,usr/bin}
+sed -e s/VERSION-BUILD/${VERSION}-${BUILD}/ -e s/ARCH/${ARCH}/ \
+	<${DEBDIR}/astadegraphlib_control >${DEBDIR}/tmp/DEBIAN/control
+
+env prefix=`pwd`/${DEBDIR}/tmp/usr ./build_all.sh install
+
+rm -rf ${DEBDIR}/tmp/usr/bin/
+
+pushd ${DEBDIR} >/dev/null
+fakeroot dpkg-deb --build tmp astadegraphlib-${VERSION}-${BUILD}.deb
+popd >/dev/null
+
+rm -rf ${DEBDIR}/tmp
