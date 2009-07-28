f1dc2dcf1164d171f48184869a93a702ae152cca
Anders Larsen <al@alarsen.net>
2009-07-28 15:09:16 +0000

    Set a reasonable font size for the make window.
    On MSW the text in the make window was only two pixel high; this somewhat impaired the readability.
    
    
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@1742 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/package_SVSWQPEV/package_YGKSEMEQ/package_TLNCNYLI/classes/class_ESYEJAHR/operations/constructor_XFCYZSCU/code.cpp
+++ Model/package_SVSWQPEV/package_YGKSEMEQ/package_TLNCNYLI/classes/class_ESYEJAHR/operations/constructor_XFCYZSCU/code.cpp
@@ -1,7 +1,8 @@
-SetSize(wxSize(760,300));
+SetSize(wxSize(760, 300));
 
 wxFont aFont(theNormalFont.GetFont());
 aFont.SetFamily(wxFONTFAMILY_MODERN);
+aFont.SetPointSize(wxNORMAL_FONT->GetPointSize());
 
 theNormalFont.SetFont(aFont);
 theErrorFont.SetFont(aFont);
