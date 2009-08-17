b4c4819b0ef8a0114150aaeb450ab7373dc5f6c8
Anders Larsen <al@alarsen.net>
2009-08-17 21:07:36 +0000

    compile fix after r1758
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@1759 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/package_SVSWQPEV/package_YGKSEMEQ/package_TLNCNYLI/classes/class_ODHKSPMR/operations/operation_VBFXONOG/code.cpp
+++ Model/package_SVSWQPEV/package_YGKSEMEQ/package_TLNCNYLI/classes/class_ODHKSPMR/operations/operation_VBFXONOG/code.cpp
@@ -8,78 +8,80 @@ switch(event.GetId())
 	case ID_TOOL_GENERATE:
 		DoGenerate(false);
 	break;
-	
+
 	case ID_TOOL_FASTBUILD:
 		DoGenerate(false);
-
+	//no break
 	case ID_TOOL_BUILD:
 	{
 		/* get configuration to build */
 		int n = myConfigList->GetSelection();
-		if (n==wxNOT_FOUND)
+		if (n == wxNOT_FOUND)
 			return;
 		wxFileName* configFile = static_cast<wxFileName*>(myConfigList->GetClientData(n));
 		assert(configFile);
 		AdeConfiguration* aConfiguration = dynamic_cast<AdeConfiguration*>(AdeModelElement::CreateNewElement(*configFile));
 		assert(aConfiguration);
-	
-		/* get make target to build */		
+
+		/* get make target to build */
 		wxString target = myBuildTargetList->GetStringSelection();
-		if (target==wxNOT_FOUND)
+		if (target == wxEmptyString)
 			return;
-		
+
 		/* run make  with build target */
 		RunMake(*aConfiguration, target);
-				
+
 		delete aConfiguration;
-	}		
+	}
 	break;
-	
+
 	case ID_TOOL_FASTRUN:
 	{
 		DoGenerate(false);
-		
+
 		/* get configuration to build */
 		int n = myConfigList->GetSelection();
-		if (n==wxNOT_FOUND)
+		if (n == wxNOT_FOUND)
 			return;
 		wxFileName* configFile = static_cast<wxFileName*>(myConfigList->GetClientData(n));
 		assert(configFile);
 		AdeConfiguration* aConfiguration = dynamic_cast<AdeConfiguration*>(AdeModelElement::CreateNewElement(*configFile));
 		assert(aConfiguration);
 
-		/* get make target to build */		
+		/* get make target to build */
 		wxString target = myBuildTargetList->GetStringSelection();
-		if (target==wxNOT_FOUND)
+		if (target == wxEmptyString)
 			return;
-	
+
 		/* run make with build target */
 		RunMake(*aConfiguration, target);
-			
-		delete aConfiguration;
-	}		
 
+		delete aConfiguration;
+	}
+	//no break
 	case ID_TOOL_RUN:
 	{
 		/* get configuration to run */
 		int n = myConfigList->GetSelection();
-		if (n==wxNOT_FOUND)
+		if (n == wxNOT_FOUND)
 			return;
 		wxFileName* configFile = static_cast<wxFileName*>(myConfigList->GetClientData(n));
 		assert(configFile);
 		AdeConfiguration* aConfiguration = dynamic_cast<AdeConfiguration*>(AdeModelElement::CreateNewElement(*configFile));
 		assert(aConfiguration);
-	
-		/* get make target to run */		
+
+		/* get make target to run */
 		wxString target = myRunTargetList->GetStringSelection();
-		
+		if (target == wxEmptyString)
+			return;
+
 		/* run make with run target */
 		RunMake(*aConfiguration, target);
-				
+
 		delete aConfiguration;
-	}		
+	}
 	break;
-	
+
 	default:
 	break;
 }
