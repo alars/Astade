e8f793fc6956c785db4bf4c06306319a9167561a
Thomas Spitzer <projektleiter@astade.de>
2009-08-26 12:53:27 +0000

    constraints for parameters are coded in the coders (attributes and opperations not yet)
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@1772 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/package_SVSWQPEV/package_YGKSEMEQ/Package_CodeGenerators/classes/class_NVQPDNTJ/operations/operation_MKDGHBOX/code.cpp
+++ Model/package_SVSWQPEV/package_YGKSEMEQ/Package_CodeGenerators/classes/class_NVQPDNTJ/operations/operation_MKDGHBOX/code.cpp
@@ -1,1 +1,23 @@
+std::list<AdeParameter*> parameterlist = op.Parameterlist();
+std::list<wxString> constraints;
 
+for (std::list<AdeParameter*>::iterator it = parameterlist.begin(); it != parameterlist.end(); ++it)
+{
+	wxString constraint = (*it)->GetConstraint();
+	if (!constraint.empty())
+	{
+		constraint.Replace("%s",(*it)->GetName());
+		constraints.push_back("\tCONSTRAINT("+constraint+");");
+	}
+	delete (*it);
+}
+
+if (!constraints.empty())
+{
+	out << std::endl << "#ifdef CONSTRAINT" << std::endl;
+	
+	for (std::list<wxString>::iterator it = constraints.begin(); it != constraints.end(); ++it)
+		out << (*it) << std::endl;
+	
+	out << "#endif" << std::endl << std::endl;
+}
