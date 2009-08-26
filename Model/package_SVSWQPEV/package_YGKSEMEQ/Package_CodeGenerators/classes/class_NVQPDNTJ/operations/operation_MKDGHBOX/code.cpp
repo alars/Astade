7f4f4b946fb44f87face86629e151cc019aaa023
Thomas Spitzer <projektleiter@astade.de>
2009-08-26 13:15:49 +0000

    coding of constraints completed
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@1773 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/package_SVSWQPEV/package_YGKSEMEQ/Package_CodeGenerators/classes/class_NVQPDNTJ/operations/operation_MKDGHBOX/code.cpp
+++ Model/package_SVSWQPEV/package_YGKSEMEQ/Package_CodeGenerators/classes/class_NVQPDNTJ/operations/operation_MKDGHBOX/code.cpp
@@ -1,6 +1,13 @@
 std::list<AdeParameter*> parameterlist = op.Parameterlist();
 std::list<wxString> constraints;
 
+wxString constraint = op.GetConstraint();
+if (!constraint.empty())
+{
+	constraint.Replace("%s",op.GetName());
+	constraints.push_back("\tCONSTRAINT("+constraint+");");
+}
+
 for (std::list<AdeParameter*>::iterator it = parameterlist.begin(); it != parameterlist.end(); ++it)
 {
 	wxString constraint = (*it)->GetConstraint();
@@ -10,6 +17,33 @@ for (std::list<AdeParameter*>::iterator it = parameterlist.begin(); it != parame
 		constraints.push_back("\tCONSTRAINT("+constraint+");");
 	}
 	delete (*it);
+}
+
+wxFileName attributes(source->GetFileName());
+attributes.AppendDir("attributes");
+
+if (wxDir::Exists(attributes.GetPath()))
+{
+	wxDir dir(attributes.GetPath());
+	wxString filename;
+
+	bool cont = dir.GetFirst(&filename, "*.ini");
+	while (cont)
+	{
+		wxFileName FullName(attributes);
+		FullName.SetFullName(filename);
+		const AdeModelElement* pe = AdeModelElement::CreateNewElement(FullName);
+
+		wxString constraint = pe->GetConstraint();
+		if (!constraint.empty())
+		{
+			constraint.Replace("%s",pe->GetName());
+			constraints.push_back("\tCONSTRAINT("+constraint+");");
+		}
+
+		delete pe;
+		cont = dir.GetNext(&filename);
+	}
 }
 
 if (!constraints.empty())
