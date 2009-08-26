79ac67aca2291c2553e993a31428ae0b01ad11d8
Thomas Spitzer <projektleiter@astade.de>
2009-08-26 05:37:42 +0000

    Made the constraint input field a one line field
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@1770 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/package_SVSWQPEV/package_YGKSEMEQ/Package_KCGFWDZT/classes/class_BYZZIWVN/operations/operation_MOFXTRHI/code.cpp
+++ Model/package_SVSWQPEV/package_YGKSEMEQ/Package_KCGFWDZT/classes/class_BYZZIWVN/operations/operation_MOFXTRHI/code.cpp
@@ -1,9 +1,9 @@
 wxBoxSizer* box = new wxStaticBoxSizer( new wxStaticBox(this, -1, wxString("Constraint:")), wxHORIZONTAL );
 
-ConstraintEditField = new wxTextCtrl(this, ID_DESCRIPTIONEDITFIELD, "" , wxDefaultPosition,wxDefaultSize, wxTE_MULTILINE );
+ConstraintEditField = new wxTextCtrl(this, -1);
 ConstraintEditField->SetMaxLength(0x4000);
-box->Add(ConstraintEditField,1,wxEXPAND);
+box->Add(ConstraintEditField,1);
 
-topSizer->Add(box,1,wxEXPAND|wxALL,10);
+topSizer->Add(box,0,wxEXPAND|wxALL,10);
 
 ConstraintEditField->SetValue(wxConfigBase::Get()->Read("Astade/Constraint",wxEmptyString));
