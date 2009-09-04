a80d19d3f8da7dc9b7a7919d2a6c38ce90d68b0d
Thomas Spitzer <projektleiter@astade.de>
2009-09-04 09:51:06 +0000

    round edge is drawn as spline
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@1781 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/package_SVSWQPEV/Package_EWMKSFJL/Package_HMPGOZXK/classes/class_VNONJMSH/operations/operation_NSQUHUSB/code.cpp
+++ Model/package_SVSWQPEV/Package_EWMKSFJL/Package_HMPGOZXK/classes/class_VNONJMSH/operations/operation_NSQUHUSB/code.cpp
@@ -5,6 +5,13 @@ else
 
 dc.DrawLine(absDrawnStartPoint.xCoord(),absDrawnStartPoint.yCoord(),absDrawnEndPoint.xCoord(),absDrawnEndPoint.yCoord());
 
+
 glVector m = absCalculateCenterPoint();
+glVector mv = lastStartPoint + lastEndPoint;
+mv = mv.Dir();
+mv *= radius;
+mv += m;
+
+dc.DrawSpline(absDrawnStartPoint.xCoord(),absDrawnStartPoint.yCoord(), mv.xCoord(),mv.yCoord(), absDrawnEndPoint.xCoord(),absDrawnEndPoint.yCoord());
 
 dc.DrawCircle(m.xCoord(),m.yCoord(),3);
