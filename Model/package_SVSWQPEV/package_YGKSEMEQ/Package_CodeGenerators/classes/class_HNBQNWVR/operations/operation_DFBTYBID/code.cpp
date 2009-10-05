754adba9c183079c53e673e26fb1eaa317c4a3b3
Thomas Spitzer <projektleiter@astade.de>
2009-10-05 12:39:30 +0000

    Bugfix in instrumenting of generated "C" code
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@1811 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/package_SVSWQPEV/package_YGKSEMEQ/Package_CodeGenerators/classes/class_HNBQNWVR/operations/operation_DFBTYBID/code.cpp
+++ Model/package_SVSWQPEV/package_YGKSEMEQ/Package_CodeGenerators/classes/class_HNBQNWVR/operations/operation_DFBTYBID/code.cpp
@@ -1,6 +1,3 @@
-if (traceLevel==0)
-	return aString;
-	
 wxString str = aString;
 
 if (str.Find("\"") != wxNOT_FOUND)
@@ -10,7 +7,13 @@ str.Replace("\t"," ");
 
 str = "  " + str;
 
-if ((str.Find(" return ") != wxNOT_FOUND) || (str.Find(" return(") != wxNOT_FOUND)) 
+if ((traceLevel==0) && ((str.Find(" RETURN ") != wxNOT_FOUND) || (str.Find(" RETURN(") != wxNOT_FOUND))) 
+	return "#error Don't use \"RETURN\" in \"C\" code with tracelevel 0 //" + aString;
+else
+if ((traceLevel==0) && (str.Find(" voidRETURN ") != wxNOT_FOUND))
+	return "#error Don't use \"voidRETURN\" in \"C\" code with tracelevel 0 //" + aString;
+else
+if ((traceLevel!=0) && ((str.Find(" return ") != wxNOT_FOUND) || (str.Find(" return(") != wxNOT_FOUND))) 
 	return "#error Don't use \"return\" in instrumented \"C\" code //" + aString;
 else
 	return aString;
