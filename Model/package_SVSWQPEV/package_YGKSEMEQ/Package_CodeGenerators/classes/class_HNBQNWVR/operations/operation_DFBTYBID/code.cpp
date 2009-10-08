4d323b182e66fa90d124713d057781f6f1dca76c
Thomas Spitzer <projektleiter@astade.de>
2009-10-08 08:23:55 +0000

    bugfix C code instrumentation
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@1813 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/package_SVSWQPEV/package_YGKSEMEQ/Package_CodeGenerators/classes/class_HNBQNWVR/operations/operation_DFBTYBID/code.cpp
+++ Model/package_SVSWQPEV/package_YGKSEMEQ/Package_CodeGenerators/classes/class_HNBQNWVR/operations/operation_DFBTYBID/code.cpp
@@ -13,7 +13,7 @@ else
 if ((traceLevel==0) && (str.Find(" voidRETURN ") != wxNOT_FOUND))
 	return "#error Don't use \"voidRETURN\" in \"C\" code with tracelevel 0 //" + aString;
 else
-if ((traceLevel!=0) && ((str.Find(" return ") != wxNOT_FOUND) || (str.Find(" return(") != wxNOT_FOUND))) 
+if ((traceLevel!=0) && ((str.Find(" return ") != wxNOT_FOUND) || (str.Find(" return;") != wxNOT_FOUND) || (str.Find(" return(") != wxNOT_FOUND))) 
 	return "#error Don't use \"return\" in instrumented \"C\" code //" + aString;
 else
 	return aString;
