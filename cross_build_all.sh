9b16f2510f3e8c226cce7e0ea38e262cde876b93
Thomas Spitzer <projektleiter@astade.de>
2009-10-16 14:09:06 +0000

    continued to develop a script for mingw builds in Ubuntu (daily builds for windows)
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@1828 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- cross_build_all.sh
+++ cross_build_all.sh
@@ -11,9 +11,20 @@ echo "**********************************"
 echo "	bin2c"
 echo "**********************************"
 make -j 2 -C Components_ZFTMQEHP/Component_UIGKBCCK/Config_HFQMMURT/ TARGET=bin2c $1
-
 echo "**********************************"
 echo "	Astade API"
 echo "**********************************"
 make -j 2 -C components_WGNBOFKH/Component_AstadeAPI/Config_ZMSNZKTV/ TARGET=AstadeAPI $1
+echo "**********************************"
+echo "	AstadeGraphLib"
+echo "**********************************"
+make -j 2 -C components_WGNBOFKH/Component_CDFBZLFN/Config_NWPHOJLA/ TARGET=AstadeGraphLib $1
+echo "**********************************"
+echo "	TraceLib"
+echo "**********************************"
+make -j 2 -C Components_AstadeFramework/Component_TraceLib/Config_Windows_StatLib/ TARGET=TraceLib $1
+echo "**********************************"
+echo "	AstadeGenerate"
+echo "**********************************"
+make -j 2 -C components_WGNBOFKH/Component_XZAWFDAN/Config_OPEOVEFK/ TARGET=AstadeGenerate $1
 
