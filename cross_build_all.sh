0dbdb5d0de61c555595748d3176d51f6e2e5d211
Thomas Spitzer <projektleiter@astade.de>
2009-10-18 17:16:52 +0000

    finished mingwCross configurations. cross_build_all.sh cross builds all Astade components for Windows in Linux environment.
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@1833 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- cross_build_all.sh
+++ cross_build_all.sh
@@ -2,29 +2,77 @@
 
 # ./configure --prefix=/usr/local/i586-mingw32 --host=i586-mingw32msvc --build=i686-linux --disable-shared
 
-export CXX=i586-mingw32msvc-g++
-export WXCONFIG=/usr/local/i586-mingw32/bin/wx-config
-
-
 cd Model
 echo "**********************************"
 echo "	bin2c"
 echo "**********************************"
-make -j 2 -C Components_ZFTMQEHP/Component_UIGKBCCK/Config_HFQMMURT/ TARGET=bin2c $1
+make -j 2 -C Components_ZFTMQEHP/Component_UIGKBCCK/Config_HZEMYJPW/ TARGET=bin2c $1
+make -j 2 -C Components_ZFTMQEHP/Component_UIGKBCCK/Config_CDBXRNYG/ TARGET=bin2c $1
 echo "**********************************"
 echo "	Astade API"
 echo "**********************************"
-make -j 2 -C components_WGNBOFKH/Component_AstadeAPI/Config_ZMSNZKTV/ TARGET=AstadeAPI $1
+make -j 2 -C components_WGNBOFKH/Component_AstadeAPI/Config_AQAHLGFG/ TARGET=AstadeAPI $1
 echo "**********************************"
 echo "	AstadeGraphLib"
 echo "**********************************"
-make -j 2 -C components_WGNBOFKH/Component_CDFBZLFN/Config_NWPHOJLA/ TARGET=AstadeGraphLib $1
+make -j 2 -C components_WGNBOFKH/Component_CDFBZLFN/Config_XSIGOWGY/ TARGET=AstadeGraphLib $1
 echo "**********************************"
 echo "	TraceLib"
 echo "**********************************"
-make -j 2 -C Components_AstadeFramework/Component_TraceLib/Config_Windows_StatLib/ TARGET=TraceLib $1
+make -j 2 -C Components_AstadeFramework/Component_TraceLib/Config_QSRQIPTA/ TARGET=TraceLib $1
 echo "**********************************"
 echo "	AstadeGenerate"
 echo "**********************************"
-make -j 2 -C components_WGNBOFKH/Component_XZAWFDAN/Config_OPEOVEFK/ TARGET=AstadeGenerate $1
-
+make -j 2 -C components_WGNBOFKH/Component_XZAWFDAN/Config_MUIULJPX/ TARGET=AstadeGenerate $1
+echo "**********************************"
+echo "	CGenerator"
+echo "**********************************"
+make -j 2 -C components_WGNBOFKH/Component_CGenerator/Config_RQZURJDX/ TARGET=CGenerator $1
+echo "**********************************"
+echo "	CppGenerator"
+echo "**********************************"
+make -j 2 -C components_WGNBOFKH/Component_CppGenerator/Config_QDCKBOPV/ TARGET=CppGenerator $1
+echo "**********************************"
+echo "	StateChartCoder"
+echo "**********************************"
+make -j 2 -C components_WGNBOFKH/Component_RAVGAUPS/Config_ILIZODGP/ TARGET=StateChartCoder $1
+echo "**********************************"
+echo "	StateChartCoderC"
+echo "**********************************"
+make -j 2 -C components_WGNBOFKH/Component_FOGYFECV/Config_JLHBJBHX/ TARGET=StateChartCoderC $1
+echo "**********************************"
+echo "	StateChartCoderVSM"
+echo "**********************************"
+make -j 2 -C Components_ZLRQZKQB/Component_DJLKKOUQ/Config_AVMVYCHR/ TARGET=StateChartCoderVSM $1
+echo "**********************************"
+echo "	StateChartCoderWx"
+echo "**********************************"
+make -j 2 -C Components_ZLRQZKQB/Component_LUAJBDMB/Config_GZUFJNTZ/ TARGET=StateChartCoderWx $1
+echo "**********************************"
+echo "	Astade"
+echo "**********************************"
+make -j 2 -C components_WGNBOFKH/component_KSEQOEET/Config_KKSMNXBE/ TARGET=Astade $1
+echo "**********************************"
+echo "	ResourceEdit"
+echo "**********************************"
+make -j 2 -C components_WGNBOFKH/Component_BZBVDSZR/Config_PYAUPGTA/ TARGET=ResourceEdit $1
+echo "**********************************"
+echo "	OMDgenerator"
+echo "**********************************"
+make -j 2 -C components_WGNBOFKH/Component_OMDgenerator/Config_HRNMFIQH/ TARGET=OMDgenerator $1
+echo "**********************************"
+echo "	StateChartDrawer"
+echo "**********************************"
+make -j 2 -C components_WGNBOFKH/Component_EIFKVZMS/Config_CLRCZBOI/ TARGET=StateChartDrawer $1
+echo "**********************************"
+echo "	AstadeDraw"
+echo "**********************************"
+make -j 2 -C components_WGNBOFKH/Component_GZQBNSVE/Config_QZFNIVMZ/ TARGET=AstadeDraw $1
+echo "**********************************"
+echo "	Trace2UML"
+echo "**********************************"
+make -j 2 -C Components_LGPLTGDP/Component_TYWOOIDA/Config_WISWQCAZ/ TARGET=Trace2UML $1
+echo "**********************************"
+echo "	instrument"
+echo "**********************************"
+make -j 2 -C Components_LGPLTGDP/Component_instrument/Config_LLQXXNNY/ TARGET=instrument $1
