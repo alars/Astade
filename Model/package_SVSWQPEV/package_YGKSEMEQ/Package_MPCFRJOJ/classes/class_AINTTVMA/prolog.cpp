06b6bfd3d6b8a21e1cdf5b2d91a92bc910d485bd
Thomas Spitzer <projektleiter@astade.de>
2009-10-22 13:45:54 +0000

    Added the two generated frame files for code edit component. Tested it. It compiles for Linux. I want to leave this component to Andreas responsibility. Let's add all features you are missing ;-)
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@1834 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/package_SVSWQPEV/package_YGKSEMEQ/Package_MPCFRJOJ/classes/class_AINTTVMA/prolog.cpp
+++ Model/package_SVSWQPEV/package_YGKSEMEQ/Package_MPCFRJOJ/classes/class_AINTTVMA/prolog.cpp
@@ -141,7 +141,7 @@ const LanguageInfo g_LanguagePrefs [] = {
       {-1, NULL}},
      mySTC_FOLD_COMMENTPY | mySTC_FOLD_QUOTESPY},
     // * (any)
-    {_T(DEFAULT_LANGUAGE),
+    {(wxChar *)DEFAULT_LANGUAGE,
      _T("*.*"),
      wxSTC_LEX_PROPERTIES,
      {{mySTC_TYPE_DEFAULT, NULL},
