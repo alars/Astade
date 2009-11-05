808dca3fca918059c2be75a746e5fe6aa3b8d80c
Thomas Spitzer <projektleiter@astade.de>
2009-11-05 20:26:25 +0000

    There where problems with wxSplashScreen in Windows. Astade is not running at all in wxWidgets 2.8  and it is damn slow in wxWidgets 2.9. I removed the splash screen, at the moment (eventually for ever). Things seem to run much better again ;-)
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@1858 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/package_SVSWQPEV/package_YGKSEMEQ/package_TLNCNYLI/classes/class_EPTOVJJL/operations/operation_UCVUAXVF/code.cpp
+++ Model/package_SVSWQPEV/package_YGKSEMEQ/package_TLNCNYLI/classes/class_EPTOVJJL/operations/operation_UCVUAXVF/code.cpp
@@ -1,13 +1,13 @@
 #include "splash_png.h"
 wxInitAllImageHandlers();
 
-wxMemoryInputStream istream(splash_png, sizeof splash_png);
-wxImage myimage(istream, wxBITMAP_TYPE_PNG);
+//wxMemoryInputStream istream(splash_png, sizeof splash_png);
+//wxImage myimage(istream, wxBITMAP_TYPE_PNG);
 
-wxSplashScreen splash(wxBitmap(myimage),
-          wxSPLASH_CENTRE_ON_SCREEN|wxSPLASH_TIMEOUT,
-          20000, NULL, -1, wxDefaultPosition, wxDefaultSize,
-          wxSIMPLE_BORDER|wxSTAY_ON_TOP);
+//wxSplashScreen splash(wxBitmap(myimage),
+//          wxSPLASH_CENTRE_ON_SCREEN|wxSPLASH_TIMEOUT,
+//          20000, NULL, -1, wxDefaultPosition, wxDefaultSize,
+//          wxSIMPLE_BORDER|wxSTAY_ON_TOP);
 
 wxConfigBase::Set(new wxFileConfig("Astade.ini"));
 
