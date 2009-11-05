06c6a978b15ac1e6b70b9c27fe47e535f57bdfe5
Thomas Spitzer <projektleiter@astade.de>
2009-11-05 20:38:50 +0000

    removed unused variable
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@1859 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/package_SVSWQPEV/package_YGKSEMEQ/package_TLNCNYLI/classes/class_EPTOVJJL/operations/operation_UCVUAXVF/code.cpp
+++ Model/package_SVSWQPEV/package_YGKSEMEQ/package_TLNCNYLI/classes/class_EPTOVJJL/operations/operation_UCVUAXVF/code.cpp
@@ -1,5 +1,5 @@
-#include "splash_png.h"
-wxInitAllImageHandlers();
+//#include "splash_png.h"
+//wxInitAllImageHandlers();
 
 //wxMemoryInputStream istream(splash_png, sizeof splash_png);
 //wxImage myimage(istream, wxBITMAP_TYPE_PNG);
