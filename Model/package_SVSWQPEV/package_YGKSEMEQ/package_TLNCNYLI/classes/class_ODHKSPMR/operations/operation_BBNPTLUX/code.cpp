c19b94370a3ba4c237f596d57723742024332764
Andreas Pflanz <apflanz@gmx.de>
2009-11-09 10:47:29 +0000

    fixed the <Compile isn't stoppable>-Problem under Windows
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@1863 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/package_SVSWQPEV/package_YGKSEMEQ/package_TLNCNYLI/classes/class_ODHKSPMR/operations/operation_BBNPTLUX/code.cpp
+++ Model/package_SVSWQPEV/package_YGKSEMEQ/package_TLNCNYLI/classes/class_ODHKSPMR/operations/operation_BBNPTLUX/code.cpp
@@ -1,1 +1,7 @@
+#ifdef __WXMSW__
+// under Windows, Processes are created independent from their childs
+// so if we want to kill them we have to do this seperately and bloody...
+wxKill(runningMakePid, wxSIGKILL,NULL,wxKILL_CHILDREN);
+#endif
+
 wxProcess::Kill(runningMakePid, wxSIGTERM, wxKILL_CHILDREN);
