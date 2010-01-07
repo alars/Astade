27f608dcb3c951e0b4735c1364f75647ebf48fcd
Thomas Spitzer <projektleiter@astade.de>
2010-01-07 07:41:41 +0000

    ACF: Removed the unneeded global: "RunningMachine"
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@1892 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/package_SVSWQPEV/Package_OXXWDRJY/Package_SPBAOVTE/classes/class_TWUSLFUZ/operations/constructor_EPFJWSGZ/code.cpp
+++ Model/package_SVSWQPEV/Package_OXXWDRJY/Package_SPBAOVTE/classes/class_TWUSLFUZ/operations/constructor_EPFJWSGZ/code.cpp
@@ -1,8 +1,7 @@
 me->HandleFunction = aHandleFunction;
 if (me->TraceOption)
 {
-    ACF_trace(ACF_runningMachine);
-    ACF_trace((char*)" (!) ");
+    ACF_trace((char*)"~ (!) ");
     ACF_trace(me->Name);
     ACF_trace((char*)"\n");
 }
--- Model/package_SVSWQPEV/Package_OXXWDRJY/Package_SPBAOVTE/classes/class_TWUSLFUZ/operations/destructor/code.cpp
+++ Model/package_SVSWQPEV/Package_OXXWDRJY/Package_SPBAOVTE/classes/class_TWUSLFUZ/operations/destructor/code.cpp
@@ -1,7 +1,6 @@
 if (me->TraceOption)
 {
-    ACF_trace(ACF_runningMachine);
-    ACF_trace((char*)" (X) ");
+    ACF_trace((char*)"~ (X) ");
     ACF_trace(me->Name);
     ACF_trace((char*)"\n");
 }
