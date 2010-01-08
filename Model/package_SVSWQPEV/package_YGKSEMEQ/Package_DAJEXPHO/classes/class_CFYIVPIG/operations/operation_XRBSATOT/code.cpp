a69adf0ba3be44fa985d5bb7828e287120161c03
Thomas Spitzer <projektleiter@astade.de>
2010-01-08 15:27:37 +0000

    StateChartCoderACF: The lifeline of the take event is traced, too.
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@1899 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/package_SVSWQPEV/package_YGKSEMEQ/Package_DAJEXPHO/classes/class_CFYIVPIG/operations/operation_XRBSATOT/code.cpp
+++ Model/package_SVSWQPEV/package_YGKSEMEQ/Package_DAJEXPHO/classes/class_CFYIVPIG/operations/operation_XRBSATOT/code.cpp
@@ -12,10 +12,33 @@ fprintf(implementationFile, "void %s_TakeEvent(%s* me, %s* theEvent)\n{\n",
                             (const char*)theStatechart.GetName().c_str(),
                             (const char*)theStatechart.GetName().c_str(),
                             (const char*)theStatechart.GetEventType().c_str());
+                            
+fprintf(implementationFile, "\t#ifdef _TRACE_\n");
+fprintf(implementationFile, "\t\tACF_trace(me->MessageReceiver_base.me_text);\n");
+fprintf(implementationFile, "\t\tACF_trace((char*)\":\");\n");
+fprintf(implementationFile, "\t\tACF_trace(me->MessageReceiver_base.Name);\n");
+fprintf(implementationFile, "\t\tACF_trace((char*)\" ==> \");\n");
+fprintf(implementationFile, "\t\tACF_trace(me->MessageReceiver_base.me_text);\n");
+fprintf(implementationFile, "\t\tACF_trace((char*)\":\");\n");
+fprintf(implementationFile, "\t\tACF_trace(me->MessageReceiver_base.Name);\n");
+fprintf(implementationFile, "\t\tACF_trace((char*)\" TakeEvent\\n\");\n");
+fprintf(implementationFile, "\t#endif\n");
+                            
 fprintf(implementationFile, "\t(me->theState)(me, theEvent);\n");
 
 fprintf(implementationFile, "\t// Call the state enter function\n");
 fprintf(implementationFile, "\t%s_EnterState(me, theEvent);\n",
                             (const char*)theStatechart.GetName().c_str());
+
+fprintf(implementationFile, "\t#ifdef _TRACE_\n");
+fprintf(implementationFile, "\t\tACF_trace(me->MessageReceiver_base.me_text);\n");
+fprintf(implementationFile, "\t\tACF_trace((char*)\":\");\n");
+fprintf(implementationFile, "\t\tACF_trace(me->MessageReceiver_base.Name);\n");
+fprintf(implementationFile, "\t\tACF_trace((char*)\" <== \");\n");
+fprintf(implementationFile, "\t\tACF_trace(me->MessageReceiver_base.me_text);\n");
+fprintf(implementationFile, "\t\tACF_trace((char*)\":\");\n");
+fprintf(implementationFile, "\t\tACF_trace(me->MessageReceiver_base.Name);\n");
+fprintf(implementationFile, "\t\tACF_trace((char*)\"\\n\");\n");
+fprintf(implementationFile, "\t#endif\n");
 
 fprintf(implementationFile, "}\n\n");
