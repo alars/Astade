e797632681a93f56b4b3ed4c0052a05ac39030fb
Jonatan Antoni <jantoni@web.de>
2010-01-09 17:12:07 +0000

    AstadeDraw: Fixed relation edges to end only at classes.
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@1904 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/package_SVSWQPEV/Package_EWMKSFJL/Package_BTYQFKAB/classes/class_EUTOSQXM/operations/operation_OJPQNAXZ/code.cpp
+++ Model/package_SVSWQPEV/Package_EWMKSFJL/Package_BTYQFKAB/classes/class_EUTOSQXM/operations/operation_OJPQNAXZ/code.cpp
@@ -1,6 +1,6 @@
 glNode* toNode = getMouseOverNode();
 
-if ((toNode == NULL) || (toNode == this))
+if ((toNode == NULL) || (toNode == this) || (dynamic_cast<glClass*>(toNode) == NULL))
     return;
 
 for (std::set<glEdge*>::iterator it = myEdges.begin(); it != myEdges.end(); it++)
