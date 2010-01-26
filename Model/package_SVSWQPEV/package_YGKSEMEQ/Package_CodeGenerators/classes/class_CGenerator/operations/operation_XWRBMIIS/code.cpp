cf04b2bd979fbef8e13ef5c82a89f76a4790d391
Anders Larsen <al@alarsen.net>
2010-01-26 22:03:35 +0000

    Worked around a crash in ~wxStringTokenizer() On Linux/GTK/UTF-8/STL around r63252, ~wxStringTokenizer() crashes mysteriously.
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@1939 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/package_SVSWQPEV/package_YGKSEMEQ/Package_CodeGenerators/classes/class_CGenerator/operations/operation_XWRBMIIS/code.cpp
+++ Model/package_SVSWQPEV/package_YGKSEMEQ/Package_CodeGenerators/classes/class_CGenerator/operations/operation_XWRBMIIS/code.cpp
@@ -1,3 +1,5 @@
-wxStringTokenizer tkz(theClassInclude, ",;");
+// FIXME: wxWidgets/UTF-8/STL around r63252 crashes in ~wxStringTokenizer()
+static wxStringTokenizer tkz;
+tkz.SetString(theClassInclude, ",;");
 while (tkz.HasMoreTokens())
     filenames.insert(tkz.GetNextToken().Trim(true).Trim(false));
