13f7d336395a1bcd45f69c3443753fa65ff79586
Anders Larsen <al@alarsen.net>
2010-01-29 09:03:00 +0000

    Completed r1942 to also work on Windoze. (AstadeTree: Only offer Revision Control Systems that are available on the user's system)
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@1946 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/package_SVSWQPEV/package_OPTPAIKK/Package_CLYWIPRI/classes/class_FJELKFWM/operations/operation_IsAvailable/code.cpp
+++ Model/package_SVSWQPEV/package_OPTPAIKK/Package_CLYWIPRI/classes/class_FJELKFWM/operations/operation_IsAvailable/code.cpp
@@ -1,3 +1,8 @@
+#ifdef __WXMSW__
+#  define EXE_EXT ".exe"
+#else
+#  define EXE_EXT
+#endif
 wxPathList path;
 path.AddEnvList("PATH");
-return !path.FindValidPath("pj").IsEmpty();
+return !path.FindValidPath("pj" EXE_EXT).IsEmpty();
--- Model/package_SVSWQPEV/package_OPTPAIKK/Package_CLYWIPRI/classes/class_JNSOHXMC/operations/operation_IsAvailable/code.cpp
+++ Model/package_SVSWQPEV/package_OPTPAIKK/Package_CLYWIPRI/classes/class_JNSOHXMC/operations/operation_IsAvailable/code.cpp
@@ -1,3 +1,8 @@
+#ifdef __WXMSW__
+#  define EXE_EXT ".exe"
+#else
+#  define EXE_EXT
+#endif
 wxPathList path;
 path.AddEnvList("PATH");
-return !path.FindValidPath("svn").IsEmpty();
+return !path.FindValidPath("svn" EXE_EXT).IsEmpty();
