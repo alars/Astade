0afd07a97194023dd405b17da78616fb5fab45db
Thomas Spitzer <projektleiter@astade.de>
2010-02-04 16:41:58 +0000

    repared the DrawDiamond methode fo both, glRelation and glSelfRelation
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@1950 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/package_SVSWQPEV/Package_EWMKSFJL/Package_HMPGOZXK/classes/class_ADIMNVVV/operations/operation_GQPHMNJB/code.cpp
+++ Model/package_SVSWQPEV/Package_EWMKSFJL/Package_HMPGOZXK/classes/class_ADIMNVVV/operations/operation_GQPHMNJB/code.cpp
@@ -1,19 +1,28 @@
-glVector diff;
-glVector endcenter;
-glVector end = absGetStartPoint();
+glVector dir = myNode.absGetPosition() -  absGetPosition();
+dir = dir.Dir();
+dir *= my_Radius;
+int count = 0;
 
-diff =  absGetEndPoint() - end;
+while((++count<360) && (myNode.isInElement(absGetPosition()+dir)))
+    dir = dir.RotateDegree(1);
+
+glVector end = absGetPosition() + dir;
+glVector endcenter = absGetPosition() + dir.RotateDegree(40);
+
+glVector diff = end - endcenter;
 diff =  diff.Dir();
-diff *= 10;
+diff *= -10;
 
-glVector u = end - diff.RotateDegree(30);
-glVector v = end - diff.RotateDegree(-30);
-glVector w = v - diff.RotateDegree(30);
+glVector orto = diff.Rotate90Degree().Dir();
+orto *= 6;
+
+glVector u = end + diff - orto;
+glVector v = end + diff + orto;
 
 wxPoint points[4] = {
-    wxPoint(u.xCoord(),u.yCoord()),
-    wxPoint(w.xCoord(),w.yCoord()),
     wxPoint(v.xCoord(),v.yCoord()),
+    wxPoint(endcenter.xCoord(),endcenter.yCoord()),
+    wxPoint(u.xCoord(),u.yCoord()),
     wxPoint(end.xCoord(),end.yCoord())
 };
 
--- Model/package_SVSWQPEV/Package_EWMKSFJL/Package_HMPGOZXK/classes/class_JLNJGYOF/operations/operation_OOFEDPQK/code.cpp
+++ Model/package_SVSWQPEV/Package_EWMKSFJL/Package_HMPGOZXK/classes/class_JLNJGYOF/operations/operation_OOFEDPQK/code.cpp
@@ -1,34 +1,23 @@
-glVector diff;
-glVector endcenter;
 glVector end = absGetStartPoint();
+glVector endcenter = absCalculateCenterPoint() ;
 
-if (width==0)
-{
-    diff =  absGetEndPoint() - end;
-    diff =  diff.Dir();
-    diff *= 10;
-}
-else
-{
-    endcenter = absCalculateCenterPoint() ;
+glVector diff = end - endcenter;
+glVector diff2 = diff.RotateDegree(40*30/diff.Mod());
 
-    diff = end - endcenter;
-    diff =  diff.Dir();
-    diff *= 10;
-    diff = diff.Rotate90Degree();
-}
+glVector orto = diff.Dir();
+orto *= 6;
 
-if (width > 0)
-    diff *= -1;
+diff =  diff.Rotate90Degree().Dir();
+diff *= 10;
 
-glVector u = end - diff.RotateDegree(30);
-glVector v = end - diff.RotateDegree(-30);
-glVector w = v - diff.RotateDegree(30);
+glVector u = end + diff - orto;
+glVector v = end + diff + orto;
+glVector w = endcenter + diff2;
 
 wxPoint points[4] = {
-    wxPoint(u.xCoord(),u.yCoord()),
-    wxPoint(w.xCoord(),w.yCoord()),
     wxPoint(v.xCoord(),v.yCoord()),
+    wxPoint(w.xCoord(),w.yCoord()),
+    wxPoint(u.xCoord(),u.yCoord()),
     wxPoint(end.xCoord(),end.yCoord())
 };
 
