d8925cdeaa123f21f554f92890eb73cca7337ace
Anders Larsen <al@alarsen.net>
2010-02-11 20:26:00 +0000

    Updated the build-scripts to reflect the renamed model directories
    git-svn-id: http://trace2uml.tigris.org/svn/trace2uml/trunk@155 89f8ffb8-d1fc-0310-92ca-d9a8b2f27bcf

--- regenerate_all.sh
+++ regenerate_all.sh
@@ -4,8 +4,8 @@ cd Model
 echo "**********************************"
 echo "	Trace2UML"
 echo "**********************************"
-AstadeGenerate -X -c Components_LGPLTGDP/Component_TYWOOIDA/
+AstadeGenerate -X -c Components_Trace2UML_components/Component_Trace2UML/
 echo "**********************************"
 echo "	instrument"
 echo "**********************************"
-AstadeGenerate -X -c Components_LGPLTGDP/Component_instrument/
+AstadeGenerate -X -c Components_Trace2UML_components/Component_instrument/
