f19fe1ff70b558af4b7852545f997dad877a9904
Anders Larsen <al@alarsen.net>
2010-02-16 15:07:02 +0000

    Started to refactor common state-chart coder functionality into a base class. Work In Progress!
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@1998 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/Package_Astade/Package_Astade_Suite/Package_StateChartCoders/classes/class_StateChartCoder/operations/operation_CodeStatechart/code.cpp
+++ Model/Package_Astade/Package_Astade_Suite/Package_StateChartCoders/classes/class_StateChartCoder/operations/operation_CodeStatechart/code.cpp
@@ -1,7 +1,7 @@
 // get implementation prolog
-InsertFile(implementationFile,"prolog.cpp",theStatechart);
+InsertFile(implementationFile,"prolog.cpp",*myAdeStatechart);
 
-fprintf(implementationFile, "#include \"%s.h\"\n\n", (const char*)theStatechart.GetName().c_str());
+fprintf(implementationFile, "#include \"%s.h\"\n\n", (const char*)myAdeStatechart->GetName().c_str());
 
 wxArrayString nativeTypes;
 
@@ -34,66 +34,66 @@ nativeTypes.Add("signed long long");
 nativeTypes.Add("unsigned long long");
 
 // get specification prolog
-InsertFile(specificationFile,"prolog.h",theStatechart);
+InsertFile(specificationFile,"prolog.h",*myAdeStatechart);
 
-if (theStatechart.GetEventType() == "std::string")
+if (myAdeStatechart->GetEventType() == "std::string")
 {
 	fprintf(specificationFile, "// include declaration of event class\n");
 	fprintf(specificationFile, "#include <string>\n\n");
 }
-else if (nativeTypes.Index(theStatechart.GetEventType().c_str()) == wxNOT_FOUND)
+else if (nativeTypes.Index(myAdeStatechart->GetEventType().c_str()) == wxNOT_FOUND)
 {
 	fprintf(specificationFile, "// forward declaration of event class\n");
-	fprintf(specificationFile, "class %s;\n\n", (const char*)theStatechart.GetEventType().c_str());
+	fprintf(specificationFile, "class %s;\n\n", (const char*)myAdeStatechart->GetEventType().c_str());
 }
 
 fprintf(specificationFile, "/**@dot\n");
-StateChartDrawer::drawStatechart(theStatechart, specificationFile);
+StateChartDrawer::drawStatechart(*myAdeStatechart, specificationFile);
 fprintf(specificationFile, "@enddot\n\n");
 
-wxString description(theStatechart.GetDescription());
+wxString description(myAdeStatechart->GetDescription());
 if (!description.empty())
     fprintf(specificationFile, "%s\n*/\n", (const char*)description.c_str());
 else
     fprintf(specificationFile, "*/\n");
 
-fprintf(specificationFile, "class %s\n{\n", (const char*)theStatechart.GetName().c_str());
+fprintf(specificationFile, "class %s\n{\n", (const char*)myAdeStatechart->GetName().c_str());
 
 fprintf(specificationFile, "\tpublic:\n");
-CodeTriggerIDs(theStatechart);
-CodeConstructor(theStatechart);
-CodeInitialize(theStatechart);
-CodeTakeEvent(theStatechart);
+CodeTriggerIDs(*myAdeStatechart);
+CodeConstructor(*myAdeStatechart);
+CodeInitialize(*myAdeStatechart);
+CodeTakeEvent(*myAdeStatechart);
 
 AdeElementIterator it;
-for (it = theStatechart.begin(); it != theStatechart.end(); ++it)
+for (it = myAdeStatechart->begin(); it != myAdeStatechart->end(); ++it)
 {
 	AdeModelElement* aElement = it.CreateNewElement();
 	if ((aElement->GetType() & ITEM_TYPE_MASK) == ITEM_IS_STATE)
-		CodeIsInStateFunction(theStatechart, *static_cast<AdeState*>(aElement));
+		CodeIsInStateFunction(*myAdeStatechart, *static_cast<AdeState*>(aElement));
 	delete aElement;
 }
 
 fprintf(specificationFile, "\n\tprotected:\n");
 
-CodeActions(theStatechart);
-CodeGuards(theStatechart);
+CodeActions(*myAdeStatechart);
+CodeGuards(*myAdeStatechart);
 
 fprintf(specificationFile, "\n\tprivate:\n");
 
-CodeNoState(theStatechart);
-CodeState(theStatechart);
-CodeEnterPointer(theStatechart);
-CodeEnterFunction(theStatechart);
+CodeNoState(*myAdeStatechart);
+CodeState(*myAdeStatechart);
+CodeEnterPointer(*myAdeStatechart);
+CodeEnterFunction(*myAdeStatechart);
 
-for (it = theStatechart.begin(); it != theStatechart.end(); ++it)
+for (it = myAdeStatechart->begin(); it != myAdeStatechart->end(); ++it)
 {
 	AdeModelElement* aElement = it.CreateNewElement();
 	if ((aElement->GetType() & ITEM_TYPE_MASK) == ITEM_IS_STATE)
 	{
 		AdeState* aState = static_cast<AdeState*>(aElement);
-		CodeStateFunction(theStatechart, *aState);
-		CodeEnterState(theStatechart, *aState);
+		CodeStateFunction(*myAdeStatechart, *aState);
+		CodeEnterState(*myAdeStatechart, *aState);
 	}
 	delete aElement;
 }
@@ -101,8 +101,8 @@ for (it = theStatechart.begin(); it != theStatechart.end(); ++it)
 fprintf(specificationFile,"};\n");
 /////////////////////////////
 // get specification epilog
-InsertFile(specificationFile,"epilog.h",theStatechart);
+InsertFile(specificationFile,"epilog.h",*myAdeStatechart);
 /////////////////////////////
 // get implementation epilog
-InsertFile(implementationFile,"epilog.cpp",theStatechart);
+InsertFile(implementationFile,"epilog.cpp",*myAdeStatechart);
 ////////
