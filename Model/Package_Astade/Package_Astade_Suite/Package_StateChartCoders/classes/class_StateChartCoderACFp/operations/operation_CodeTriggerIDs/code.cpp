421a1b614da6bde62567cce749655492386e0633
Anders Larsen <al@alarsen.net>
2010-02-21 20:25:02 +0000

    The StateChartCoderACF/ACF++ now generate a header with the event definitions. You can include this header to save some typing (or leave it alone if you don't care).
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@2044 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/Package_Astade/Package_Astade_Suite/Package_StateChartCoders/classes/class_StateChartCoderACFp/operations/operation_CodeTriggerIDs/code.cpp
+++ Model/Package_Astade/Package_Astade_Suite/Package_StateChartCoders/classes/class_StateChartCoderACFp/operations/operation_CodeTriggerIDs/code.cpp
@@ -2,12 +2,21 @@ std::set<wxString> aSet = myAdeStatechart->GetTrigger();
 if (aSet.empty())
     return;
 
+events << "// event definitions:" << std::endl;
 impl << "//************* events ***************************" << std::endl;
 
 for (std::set<wxString>::iterator iter = aSet.begin(); iter != aSet.end(); ++iter)
+{
+	if ((*iter) != "ACF_timeout")
+		events << "const char* "
+			<< (*iter).c_str()
+			<< " = \""
+			<< (*iter).c_str()
+			<< "\";"
+			<< std::endl;
 	impl << "extern const char* "
 		<< (*iter).c_str()
 		<< ";"
 		<< std::endl;
-
+}
 impl << std::endl;
