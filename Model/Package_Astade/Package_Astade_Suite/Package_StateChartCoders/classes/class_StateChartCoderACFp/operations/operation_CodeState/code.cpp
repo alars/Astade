5bb2723d794b55b9610fe4933a1eb4f7ed9e78ab
Anders Larsen <al@alarsen.net>
2010-02-23 21:28:14 +0000

    StateChartCoderACF++: Added an accessor to the ACF_MessageReceiver structure; without it, you can't send messages to the state machine.
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@2055 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/Package_Astade/Package_Astade_Suite/Package_StateChartCoders/classes/class_StateChartCoderACFp/operations/operation_CodeState/code.cpp
+++ Model/Package_Astade/Package_Astade_Suite/Package_StateChartCoders/classes/class_StateChartCoderACFp/operations/operation_CodeState/code.cpp
@@ -1,4 +1,4 @@
-spec << "\t//! @brief The state-chart is an ACF_MessageReceiver so we need this member here first." << std::endl;
+spec << "\t//! @brief The state-chart is an ACF_MessageReceiver." << std::endl;
 spec << "\tACF_MessageReceiver MessageReceiver_base;\n" << std::endl;
 
 spec << "\t//! @brief The pointer holding the current state." << std::endl;
