0d90b0acbf19f6e21ae0b32bcdea5e2ec22ad74e
Anders Larsen <al@alarsen.net>
2010-02-25 21:24:39 +0000

    Implemented renaming of 'transition' elements (and renamed all such elements in the Model)
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@2065 aaa30fad-34f9-0310-8aa9-877cc762ee2e

---
+++ Model/Package_Astade/Package_Astade_API/Package_Model_Elements/Package_Element_Classes/classes/class_AdeTransition/operations/operation_Rename/code.cpp
@@ -0,0 +1,8 @@
+wxFileName newFilename = GuessGoodFilename("transition_" + GetName());
+
+if (GetFileName() != newFilename)
+{
+	int rc = AdeRevisionControlBase::GetRevisionControlObject()->Move(GetFileName(), newFilename);
+	if (rc == 0)
+		SetFileName(newFilename);
+}
