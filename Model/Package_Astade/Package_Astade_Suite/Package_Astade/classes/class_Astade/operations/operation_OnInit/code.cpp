7f243685e41a51c4947c1a40eb24d4b7c6de81d3
Anders Larsen <al@alarsen.net>
2010-03-17 12:37:39 +0000

    removed a redundant call of ::wxInitAllImageHandlers()
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@2098 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/Package_Astade/Package_Astade_Suite/Package_Astade/classes/class_Astade/operations/operation_OnInit/code.cpp
+++ Model/Package_Astade/Package_Astade_Suite/Package_Astade/classes/class_Astade/operations/operation_OnInit/code.cpp
@@ -1,14 +1,3 @@
-//#include "splash_png.h"
-wxInitAllImageHandlers();
-
-//wxMemoryInputStream istream(splash_png, sizeof splash_png);
-//wxImage myimage(istream, wxBITMAP_TYPE_PNG);
-
-//wxSplashScreen splash(wxBitmap(myimage),
-//          wxSPLASH_CENTRE_ON_SCREEN|wxSPLASH_TIMEOUT,
-//          20000, NULL, -1, wxDefaultPosition, wxDefaultSize,
-//          wxSIMPLE_BORDER|wxSTAY_ON_TOP);
-
 wxConfigBase::Set(new wxFileConfig("Astade.ini"));
 
 InitializeFrame();
