7c0db77d46e1e67aedc7b027fbe29cdac37813af
Anders Larsen <al@alarsen.net>
2010-04-27 20:18:25 +0000

    Enabled the context menu 'Up' and 'Down' for attributes in completion of Junde Lin's patch.
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@2107 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/Package_Astade/Package_Astade_Suite/Package_TreeItemClasses/classes/class_AstadeAttribute/operations/operation_AppendMenuItems/code.cpp
+++ Model/Package_Astade/Package_Astade_Suite/Package_TreeItemClasses/classes/class_AstadeAttribute/operations/operation_AppendMenuItems/code.cpp
@@ -2,6 +2,13 @@ aPopUp.Append(ID_FEATURES,"features",wxEmptyString, wxITEM_NORMAL);
 aPopUp.AppendSeparator();
 AppendCutnPaste(aPopUp, COPY_SOURCE);
 aPopUp.AppendSeparator();
+aPopUp.Append(ID_UP,"up",wxEmptyString, wxITEM_NORMAL);
+if (!ourTree->GetPrevSibling(GetId()).IsOk())
+	aPopUp.Enable(ID_UP, false);
+aPopUp.Append(ID_DOWN,"down",wxEmptyString, wxITEM_NORMAL);
+if (!ourTree->GetNextSibling(GetId()).IsOk())
+	aPopUp.Enable(ID_DOWN, false);
+aPopUp.AppendSeparator();
 
 if (AdeRevisionControlBase::GetRevisionControlObject()->IsRenameSupported())
 {
