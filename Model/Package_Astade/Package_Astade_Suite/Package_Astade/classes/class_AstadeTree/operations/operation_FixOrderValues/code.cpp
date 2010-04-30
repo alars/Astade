8b44f1535a63367e0adabc3271909c60fe8bee28
Thomas Spitzer <projektleiter@astade.de>
2010-04-30 06:23:20 +0000

    FixOrderValues is now called for attributes, too.
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@2109 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/Package_Astade/Package_Astade_Suite/Package_Astade/classes/class_AstadeTree/operations/operation_FixOrderValues/code.cpp
+++ Model/Package_Astade/Package_Astade_Suite/Package_Astade/classes/class_AstadeTree/operations/operation_FixOrderValues/code.cpp
@@ -1,5 +1,5 @@
-// We do this for parameters only
-if (dynamic_cast<AdeParameter*>(GetItem(aID)) == 0)
+// We do this for parameters and attributes only
+if ((dynamic_cast<AdeParameter*>(GetItem(aID)) == 0) && (dynamic_cast<AdeAttribute*>(GetItem(aID)) == 0))
 	return;
 
 wxTreeItemId search = aID;
