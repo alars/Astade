bd01be151f27912026efa78b5e85d897adbaa27f
Anders Larsen <al@alarsen.net>
2010-06-11 09:34:57 +0000

    pack-source.sh: take an (optional) argument specifying the build number
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@2130 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- pack-source.sh
+++ pack-source.sh
@@ -1,11 +1,12 @@
 #!/bin/bash --
 
+[ -n "${1}" ] && BUILD=-${1}
 VERSION=`awk -F '"' '{print $2}' Model/components_WGNBOFKH/component_KSEQOEET/manual/AstadeVersion.h`
 
 find -L ${PWD}/{Model,Source/{AstadeView,Icons,icons_png,Templates},build_{all,mac}*,*.txt} \
 	\( -name .svn -o -name html \) -prune -o \
 	-type f \! -name ".*" \! -name "*.[ao]" -a \
 	\( -name "*.py" -o -name "*.sh" -o \! -perm +111 \) -print \
-	|pax -ws,^${PWD},astade-${VERSION}, |gzip >Source/astade-${VERSION}.tar.gz
+	|pax -ws,^${PWD},astade-${VERSION}, |gzip >Source/astade-${VERSION}${BUILD}.tar.gz
 
 zip -qrj Source/Templates/ACF.zip Model/Components_AstadeFramework/Component_ACF/ -i '*.c' -i '*.h'
