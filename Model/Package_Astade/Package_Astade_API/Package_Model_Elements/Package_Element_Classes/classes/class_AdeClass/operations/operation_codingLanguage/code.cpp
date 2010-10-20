231429243e897b2e4d4d0d468a0cd56d5585f8b5
Anders Larsen <al@alarsen.net>
2010-10-20 14:19:54 +0000

    Aargh! My commit r2162 from two months ago broke C coding, and nobody noticed...
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@2175 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/Package_Astade/Package_Astade_API/Package_Model_Elements/Package_Element_Classes/classes/class_AdeClass/operations/operation_codingLanguage/code.cpp
+++ Model/Package_Astade/Package_Astade_API/Package_Model_Elements/Package_Element_Classes/classes/class_AdeClass/operations/operation_codingLanguage/code.cpp
@@ -1,8 +1,8 @@
 //~~ wxString codingLanguage() [AdeClass] ~~
 
 wxString theLanguage;
-if (!wxConfigBase::Get()->Read("Astade/CodingLanguage", &theLanguage))
-	theLanguage = wxConfigBase::Get()->Read("Astade/CCoded") == "yes" ? CODE_C : CODE_CPlusPlus;
+if (!myConfig->Read("Astade/CodingLanguage", &theLanguage))
+	theLanguage = myConfig->Read("Astade/CCoded") == "yes" ? CODE_C : CODE_CPlusPlus;
 if (theLanguage == "Ansi C")
 	theLanguage = CODE_C;
 
