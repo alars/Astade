e9d23f7bbbe15971334606ed170acf4ac356792a
Anders Larsen <al@alarsen.net>
2010-11-18 09:57:20 +0000

    Show the build-date in the About: box
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@2184 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/Package_Astade/Package_Astade_Suite/Package_Astade/classes/class_AstadeFrame/operations/operation_DisplayAbout/code.cpp
+++ Model/Package_Astade/Package_Astade_Suite/Package_Astade/classes/class_AstadeFrame/operations/operation_DisplayAbout/code.cpp
@@ -1,5 +1,7 @@
+//~~ void DisplayAbout(wxCommandEvent& event) [AstadeFrame] ~~
+
 #if !defined(wxS)
 #  define wxS(x) wxT(x)
 #endif
 
-wxMessageDialog(this, wxS("Astade UML utility,\n" COPYRIGHT "\nRelease: " AstadeVersion "\nBuilt against ") wxVERSION_STRING, "about:").ShowModal();
+wxMessageDialog(this, wxS("Astade UML utility,\n" COPYRIGHT "\nRelease " AstadeVersion "\nBuilt against ") wxVERSION_STRING wxS("\nBuild date " __DATE__), "about:").ShowModal();
