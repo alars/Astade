a0770b9a904533132ab2c25b08b1ff780d2aa109
Anders Larsen <al@alarsen.net>
2010-11-30 14:18:25 +0000

    Killed some zombies.
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@2192 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/Package_Astade/Package_Astade_API/Package_Model_Elements/Package_Base_Classes/classes/class_AdeRelationBase/operations/operation_QWEVTDAJ/code.cpp
+++
@@ -1,7 +0,0 @@
-wxFileName toFileName = AdeModelElement::Move(parentPath);
-
-wxFileName partnerFileName = GetPartnerFile();
-wxFileConfig partnerConfig(wxEmptyString, wxEmptyString, partnerFileName.GetFullPath());
-partnerConfig.Write("Astade/PartnerPath", toFileName.GetFullPath(wxPATH_UNIX));
-
-return toFileName;
