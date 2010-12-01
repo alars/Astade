6099857f404f561cc07f6e9ee9321b6df56fd298
Anders Larsen <al@alarsen.net>
2010-12-01 09:46:50 +0000

    Escape ampersands in the "overload operation" menu.
    A menu item of the "overload operation" menu consists of the signature
    of the overloadable operation.  Since a parameter could be declared as
    reference, we must escape (double) the ampersand to prevent the wxMenu
    from interpreting it as an accelerator.
    
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@2194 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/Package_Astade/Package_Astade_Suite/Package_TreeItemClasses/classes/class_AstadeOperations/operations/operation_CreateOverloadMenu/code.cpp
+++ Model/Package_Astade/Package_Astade_Suite/Package_TreeItemClasses/classes/class_AstadeOperations/operations/operation_CreateOverloadMenu/code.cpp
@@ -1,4 +1,5 @@
 //~~ wxMenu* CreateOverloadMenu() [AstadeOperations] ~~
+
 wxMenu* aSubUp = new wxMenu(wxEmptyString);
 
 overloadIDs.clear(); // clear cache
@@ -8,28 +9,30 @@ wxASSERT_MSG(parentClass, "AstadeOperations::CreateOverloadMenu(): Parent is not
 
 std::map<wxString, wxString> ops = parentClass->GetOverloadableOperations();
 
-delete(parentClass);
+delete parentClass;
 
 std::set<wxString> alreadyOverloaded;
 
 for (AdeElementIterator it = dynamic_cast<AdeOperations*>(myModelElement)->begin(); it != dynamic_cast<AdeOperations*>(myModelElement)->end(); ++it)
 {
-	AdeModelElement* aElement = it.CreateNewElement();
-	AdeOperationBase* aOperation = dynamic_cast<AdeOperationBase*>(aElement);
-	if (aOperation)
-		alreadyOverloaded.insert(alreadyOverloaded.begin(),aOperation->GetSignature());
-	delete aElement;
+	AdeModelElement* anElement = it.CreateNewElement();
+	AdeOperationBase* anOperation = dynamic_cast<AdeOperationBase*>(anElement);
+	if (anOperation)
+		alreadyOverloaded.insert(alreadyOverloaded.begin(), anOperation->GetSignature());
+	delete anElement;
 }
 
 int id;
 std::map<wxString, wxString>::iterator it;
-for(it=ops.begin(), id = ID_OVERLOADMIN; it!=ops.end() && id <= ID_OVERLOADMAX;	++it, id++)
+for (it = ops.begin(), id = ID_OVERLOADMIN; it != ops.end() && id <= ID_OVERLOADMAX; ++it, ++id)
 {
-	aSubUp->Append(id, (*it).first, wxEmptyString, wxITEM_NORMAL);
+	wxString item((*it).first);
+	item.Replace(wxS("&"), wxS("&&"), true);
+	aSubUp->Append(id, item, wxEmptyString, wxITEM_NORMAL);
 	overloadIDs[id] = (*it).second;
 	aSubUp->Enable(id,alreadyOverloaded.find((*it).first) == alreadyOverloaded.end());
-	if (((*it).first.find("~")==0) && (static_cast<AdeDirectoryElement*>(myModelElement)->HasDestructor()))
-		aSubUp->Enable(id,false);
+	if ((*it).first.find(wxS("~")) == 0 && static_cast<AdeDirectoryElement*>(myModelElement)->HasDestructor())
+		aSubUp->Enable(id, false);
 }
 
 return aSubUp;
