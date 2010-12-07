alars/Astade

commit 6012414c39937944ba8691df15a360d52769d4c0

Set the time of modification to the generated files.
The coders now force the time of the generated files to be equal to the time
of the last modification of the class.

The header gets the last time of the model elements excluding action code;
the source gets the last time of all of the class sub-elements.

This should make the traffic light and exclamation mark behave as expected
across all operating systems as well as ease the stress on make, since a
header doesn't get a new time if only the action code was modified.

CAVEAT: Compiles, but otherwise completely untested!

git-svn-id: http://astade.tigris.org/svn/astade/trunk@2203 aaa30fad-34f9-0310-8aa9-877cc762ee2e

Model/Package_Astade/Package_Astade_Suite/Package_StateChartCoders/classes/class_StateChartCoderACF/operations/operation_DoIt/code.cpp
@@ -1,4 +1,5 @@
 //~~ int DoIt(int argc, char* const* argv) [StateChartCoderACF] ~~
+
 wxCmdLineParser CmdLineParser(argc, const_cast<char**>(argv));
 CmdLineParser.AddParam("DIRNAME",wxCMD_LINE_VAL_STRING,wxCMD_LINE_OPTION_MANDATORY);
 CmdLineParser.AddParam("TARGETFILE",wxCMD_LINE_VAL_STRING,wxCMD_LINE_OPTION_MANDATORY);
@@ -28,8 +29,9 @@ spec.open(myFilename.GetFullPath().c_str());
 InsertFile(spec, aPrologue.GetFullPath());
 PrintHeader(spec, myFilename.GetFullName());
 
-myFilename.SetExt("c");
-impl.open(myFilename.GetFullPath().c_str());
+wxFileName myImplname(myFilename);
+myImplname.SetExt("c");
+impl.open(myImplname.GetFullPath().c_str());
 aPrologue.SetExt("cpp");
 InsertFile(impl, aPrologue.GetFullPath());
 PrintHeader(impl, myFilename.GetFullName());
@@ -40,5 +42,12 @@ aPrologue.SetFullName("epilog.h");
 InsertFile(spec, aPrologue.GetFullPath());
 aPrologue.SetExt("cpp");
 InsertFile(impl, aPrologue.GetFullPath());
+
+spec.close();
+impl.close();
+
+wxDateTime theTime(myAdeStatechart->GetModificationTime());
+myFilename.SetTimes(&theTime, &theTime, &theTime);
+myImplname.SetTimes(&theTime, &theTime, &theTime);
 
 return EXIT_SUCCESS;

Model/Package_Astade/Package_Astade_Suite/Package_StateChartCoders/classes/class_StateChartCoderACFp/operations/operation_DoIt/code.cpp
@@ -1,3 +1,5 @@
+//~~ int DoIt(int argc, char* const* argv) [StateChartCoderACFp] ~~
+
 wxCmdLineParser CmdLineParser(argc, const_cast<char**>(argv));
 CmdLineParser.AddParam("DIRNAME",wxCMD_LINE_VAL_STRING,wxCMD_LINE_OPTION_MANDATORY);
 CmdLineParser.AddParam("TARGETFILE",wxCMD_LINE_VAL_STRING,wxCMD_LINE_OPTION_MANDATORY);
@@ -27,8 +29,9 @@ spec.open(myFilename.GetFullPath().c_str());
 InsertFile(spec, aPrologue.GetFullPath());
 PrintHeader(spec, myFilename.GetFullName());
 
-myFilename.SetExt("cpp");
-impl.open(myFilename.GetFullPath().c_str());
+wxFileName myImplname(myFilename);
+myImplname.SetExt("cpp");
+impl.open(myImplname.GetFullPath().c_str());
 aPrologue.SetExt("cpp");
 InsertFile(impl, aPrologue.GetFullPath());
 PrintHeader(impl, myFilename.GetFullName());
@@ -39,5 +42,12 @@ aPrologue.SetFullName("epilog.h");
 InsertFile(spec, aPrologue.GetFullPath());
 aPrologue.SetExt("cpp");
 InsertFile(impl, aPrologue.GetFullPath());
+
+spec.close();
+impl.close();
+
+wxDateTime theTime(myAdeStatechart->GetModificationTime());
+myFilename.SetTimes(&theTime, &theTime, &theTime);
+myImplname.SetTimes(&theTime, &theTime, &theTime);
 
 return EXIT_SUCCESS;

Model/Package_Astade/Package_Astade_Suite/Package_StateChartCoders/classes/class_StateChartCoderWx/operations/operation_DoIt/code.cpp
@@ -1,3 +1,5 @@
+//~~ int DoIt(int argc, char* const* argv) [StateChartCoderWx] ~~
+
 wxCmdLineParser CmdLineParser(argc, const_cast<char**>(argv));
 CmdLineParser.AddParam("DIRNAME",wxCMD_LINE_VAL_STRING,wxCMD_LINE_OPTION_MANDATORY);
 CmdLineParser.AddParam("TARGETFILE",wxCMD_LINE_VAL_STRING,wxCMD_LINE_OPTION_MANDATORY);
@@ -16,21 +18,22 @@ if ((element->GetType() & ITEM_TYPE_MASK) != ITEM_IS_STATECHART)
 myAdeStatechart = dynamic_cast<AdeStatechart*>(element);
 assert(myAdeStatechart);
 
-wxFileName theFileName = CmdLineParser.GetParam(1);
-wxFileName aPrologue(theFileName);
+myFilename = CmdLineParser.GetParam(1);
+wxFileName aPrologue(myFilename);
 aPrologue.RemoveLastDir();
 aPrologue.SetFullName("prolog.h");
 
-theFileName.SetExt("h");
-spec.open(theFileName.GetFullPath().c_str());
+myFilename.SetExt("h");
+spec.open(myFilename.GetFullPath().c_str());
 InsertFile(spec, aPrologue.GetFullPath());
-PrintHeader(spec, theFileName.GetFullName());
+PrintHeader(spec, myFilename.GetFullName());
 
-theFileName.SetExt("cpp");
-impl.open(theFileName.GetFullPath().c_str());
+wxFileName myImplname(myFilename);
+myImplname.SetExt("cpp");
+impl.open(myFilename.GetFullPath().c_str());
 aPrologue.SetExt("cpp");
 InsertFile(impl, aPrologue.GetFullPath());
-PrintHeader(impl, theFileName.GetFullName());
+PrintHeader(impl, myFilename.GetFullName());
 
 CodeStatechart();
 
@@ -38,5 +41,12 @@ aPrologue.SetFullName("epilog.h");
 InsertFile(spec, aPrologue.GetFullPath());
 aPrologue.SetExt("cpp");
 InsertFile(impl, aPrologue.GetFullPath());
+
+spec.close();
+impl.close();
+
+wxDateTime theTime(myAdeStatechart->GetModificationTime());
+myFilename.SetTimes(&theTime, &theTime, &theTime);
+myImplname.SetTimes(&theTime, &theTime, &theTime);
 
 return EXIT_SUCCESS;

Model/Package_Astade/Package_Astade_API/Package_Model_Elements/Package_Element_Classes/classes/class_AdeClass/operations/operation_GetCodeModificationTime/code.cpp
@@ -1,0 +1,35 @@
+//~~ wxDateTime GetCodeModificationTime() [AdeClass] ~~
+
+wxDateTime lastModified = GetModificationTime();
+
+if (!HasOperations())
+	return lastModified;
+
+wxFileName dirName(myFileName);
+dirName.AppendDir(wxS("operations"));
+dirName.SetFullName(wxEmptyString);
+
+wxDir aDir(dirName.GetPath());
+
+wxString currentFilename;
+if (dirName.DirExists() && aDir.GetFirst(&currentFilename, wxEmptyString, wxDIR_DIRS))
+	do
+	{
+		wxFileName anOperation(dirName);
+		anOperation.SetFullName(wxS("ModelNode.ini"));
+		anOperation.AppendDir(currentFilename);
+		if (anOperation.FileExists())
+		{
+			AdeModelElement* anElement = AdeModelElement::CreateNewElement(anOperation);
+			AdeOperationBase* theOperation = dynamic_cast<AdeOperationBase*>(anElement);
+			if (theOperation)
+			{
+				wxDateTime aModified = theOperation->GetActionCodeFile().GetModificationTime();
+				if (aModified > lastModified)
+					lastModified = aModified;
+			}
+			delete anElement;
+		}
+	} while (aDir.GetNext(&currentFilename));
+
+return lastModified;

Model/Package_Astade/Package_Astade_API/Package_Model_Elements/Package_Element_Classes/classes/class_AdeClass/operations/operation_GetImpGenerationTime/code.cpp
@@ -1,4 +1,5 @@
 //~~ wxDateTime GetImpGenerationTime() [AdeClass] ~~
+
 wxFileName ImpFileName = GetImpFileName();
 
 if (!ImpFileName.FileExists())

Model/Package_Astade/Package_Astade_API/Package_Model_Elements/Package_Element_Classes/classes/class_AdeClass/operations/operation_GetModificationTime/code.cpp
@@ -2,11 +2,10 @@
 
 wxDateTime lastModified = wxDateTime(static_cast<time_t>(0));
 
-wxString currentFilename;
 wxFileName dirName(myFileName);
-
 wxDir myDir(dirName.GetPath());
 
+wxString currentFilename;
 if (myDir.GetFirst(&currentFilename, wxEmptyString, wxDIR_FILES))
 	do
 	{
@@ -19,33 +18,5 @@ if (myDir.GetFirst(&currentFilename, wxEmptyString, wxDIR_FILES))
 				lastModified = aModified;
 		}
 	} while (myDir.GetNext(&currentFilename));
-
-if (!HasOperations())
-	return lastModified;
-
-dirName.AppendDir(wxS("operations"));
-dirName.SetFullName(wxEmptyString);
-
-wxDir aDir(dirName.GetPath());
-
-if (dirName.DirExists() && aDir.GetFirst(&currentFilename, wxEmptyString, wxDIR_DIRS))
-	do
-	{
-		wxFileName anOperation(dirName);
-		anOperation.SetFullName(wxS("ModelNode.ini"));
-		anOperation.AppendDir(currentFilename);
-		if (anOperation.FileExists())
-		{
-			AdeModelElement* anElement = AdeModelElement::CreateNewElement(anOperation);
-			AdeOperationBase* theOperation = dynamic_cast<AdeOperationBase*>(anElement);
-			if (theOperation)
-			{
-				wxDateTime aModified = theOperation->GetActionCodeFile().GetModificationTime();
-				if (aModified > lastModified)
-					lastModified = aModified;
-			}
-			delete anElement;
-		}
-	} while (aDir.GetNext(&currentFilename));
 
 return lastModified;

Model/Package_Astade/Package_Astade_API/Package_Model_Elements/Package_Element_Classes/classes/class_AdeClass/operations/operation_IsChanged/code.cpp
@@ -1,3 +1,4 @@
-bool retVal = (GetModificationTime() > GetImpGenerationTime());
-retVal |= (GetModificationTime() > GetSpecGenerationTime());
-return retVal;
+//~~ bool IsChanged() [AdeClass] ~~
+
+return GetModificationTime()     > GetSpecGenerationTime() ||
+       GetCodeModificationTime() > GetImpGenerationTime();

Model/Package_Astade/Package_Astade_Suite/Package_CodeGenerators/classes/class_CGenerator/operations/operation_doC/code.cpp
@@ -1,5 +1,4 @@
 //~~ void doC() [CGenerator] ~~
-/* vi: set tabstop=4: */
 
 target.SetExt("c");
 std::ofstream out(target.GetFullPath().c_str());
@@ -142,5 +141,6 @@ if (Gpostfixtext.IsOpened() && Gpostfixtext.GetLineCount() > 0)
 
 target.SetExt("c");
 out.close();
-wxDateTime aTime(wxDateTime::Now());
-target.SetTimes(&aTime,&aTime,&aTime);
+
+wxDateTime theTime(source->GetCodeModificationTime());
+target.SetTimes(&theTime, &theTime, &theTime);

commit a8ff591bb2d89bbde6a5304fc24444b682403d3c

Repaired the statechart-coders.
Commit 6012414c3993 (SVN r2203) broke the statechart-coders slightly
(and the Wx badly).
Anyway, I warned you in the commit message that the change was 100% untested...

git-svn-id: http://astade.tigris.org/svn/astade/trunk@2215 aaa30fad-34f9-0310-8aa9-877cc762ee2e

Model/Package_Astade/Package_Astade_Suite/Package_StateChartCoders/classes/class_StateChartCoderACF/operations/operation_DoIt/code.cpp
@@ -34,7 +34,7 @@ myImplname.SetExt("c");
 impl.open(myImplname.GetFullPath().c_str());
 aPrologue.SetExt("cpp");
 InsertFile(impl, aPrologue.GetFullPath());
-PrintHeader(impl, myFilename.GetFullName());
+PrintHeader(impl, myImplname.GetFullName());
 
 CodeStatechart();
 

Model/Package_Astade/Package_Astade_Suite/Package_StateChartCoders/classes/class_StateChartCoderACFp/operations/operation_DoIt/code.cpp
@@ -34,7 +34,7 @@ myImplname.SetExt("cpp");
 impl.open(myImplname.GetFullPath().c_str());
 aPrologue.SetExt("cpp");
 InsertFile(impl, aPrologue.GetFullPath());
-PrintHeader(impl, myFilename.GetFullName());
+PrintHeader(impl, myImplname.GetFullName());
 
 CodeStatechart();
 

Model/Package_Astade/Package_Astade_Suite/Package_StateChartCoders/classes/class_StateChartCoderWx/operations/operation_DoIt/code.cpp
@@ -30,10 +30,10 @@ PrintHeader(spec, myFilename.GetFullName());
 
 wxFileName myImplname(myFilename);
 myImplname.SetExt("cpp");
-impl.open(myFilename.GetFullPath().c_str());
+impl.open(myImplname.GetFullPath().c_str());
 aPrologue.SetExt("cpp");
 InsertFile(impl, aPrologue.GetFullPath());
-PrintHeader(impl, myFilename.GetFullName());
+PrintHeader(impl, myImplname.GetFullName());
 
 CodeStatechart();
 