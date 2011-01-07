c3a036bcaa422bebcf9cef921337bbb6741ca174
Anders Larsen <al@alarsen.net>
2011-01-07 12:07:29 +0000

    Whitespace cleanup
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@2283 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/Package_Astade/Package_Astade_Suite/Package_CodeGenerators/classes/class_GeneratorBase/operations/operation_CodePortConnections/code.cpp
+++ Model/Package_Astade/Package_Astade_Suite/Package_CodeGenerators/classes/class_GeneratorBase/operations/operation_CodePortConnections/code.cpp
@@ -1,30 +1,28 @@
 //~~ void CodePortConnections(std::ofstream& out) [GeneratorBase] ~~
+
 AdeConnections* theConnections = source->GetConnections();
 
 if (theConnections)
 {
     out << "\n\t//connecting the inner ports" << std::endl;
-	for(AdeElementIterator it = theConnections->begin(); it != theConnections->end(); ++it)
+	for (AdeElementIterator it = theConnections->begin(); it != theConnections->end(); ++it)
 	{
-        AdeModelElement* aElement = it.CreateNewElement();
-		wxASSERT(aElement);
-		AdeConnection* aConnection = dynamic_cast<AdeConnection*>(aElement);
+        AdeModelElement* anElement = it.CreateNewElement();
+		wxASSERT(anElement);
+		AdeConnection* aConnection = dynamic_cast<AdeConnection*>(anElement);
 		wxASSERT_MSG(aConnection, "the Connections folder should only contain connections");
 
         wxString InputObject = aConnection->GetInputObject();
         wxString InputPort = aConnection->GetInputPort();
         wxString OutputObject = aConnection->GetOutputObject();
         wxString OutputPort = aConnection->GetOutputPort();
-        
-        out << "\tCONNECT_PORTS(" << source->GetObject(InputObject, InputPort) << ", " 
-                                  << source->GetObjectPort(InputObject, InputPort) << ", " 
-                                  << source->GetObject(OutputObject, OutputPort) << ", " 
+
+        out << "\tCONNECT_PORTS(" << source->GetObject(InputObject, InputPort) << ", "
+                                  << source->GetObjectPort(InputObject, InputPort) << ", "
+                                  << source->GetObject(OutputObject, OutputPort) << ", "
                                   << source->GetObjectPort(OutputObject, OutputPort) << ");" << std::endl;
-        
 
-		delete(aElement);
-
+		delete anElement;
 	}
     delete theConnections;
 }
-
