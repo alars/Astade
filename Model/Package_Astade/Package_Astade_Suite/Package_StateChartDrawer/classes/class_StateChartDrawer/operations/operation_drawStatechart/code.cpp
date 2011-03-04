18b8feef34eea5fe9cda819331e71b6827b62c1c
Anders Larsen <al@alarsen.net>
2011-03-04 22:16:49 +0000

    Removed a dead operation.
    The statechart-coders have all been refactored to use STL streams, so remove the
    now redundant operation StateChartDrawer::DrawStatechart(AdeStateChart&, FILE*)
    
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@2324 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/Package_Astade/Package_Astade_Suite/Package_StateChartDrawer/classes/class_StateChartDrawer/operations/operation_drawStatechart/code.cpp
+++ Model/Package_Astade/Package_Astade_Suite/Package_StateChartDrawer/classes/class_StateChartDrawer/operations/operation_drawStatechart/code.cpp
@@ -1,43 +1,65 @@
-//~~ void drawStatechart(AdeStatechart& theStatechart, FILE* stream) [StateChartDrawer] ~~
-fprintf(stream,"digraph G {\n");
-fprintf(stream,"\tfontsize=10, labelloc = \"t\", labeljust = \"l\";\n");
-fprintf(stream,"\tlabel = \"%s\n%s\";\n",theStatechart.GetName().c_str(), theStatechart.GetDescription().c_str());
+//~~ std::ostream& drawStatechart(std::ostream& stream, AdeStatechart& theStatechart) [StateChartDrawer] ~~
+stream << "digraph G {" << std::endl;
 
-fprintf(stream,"\tNOSTATE [shape=circle label=\"\" width=0.2 style=filled fillcolor=black color=black];\n");
+stream << "\tfontsize=10\n\tlabelloc = \"t\"\n\tlabeljust = \"l\"" << std::endl;
+stream << "\tlabel = \"" << theStatechart.GetName() << "\\n" << theStatechart.GetDescription() << "\"" << std::endl;
 
+stream << "\tNOSTATE [shape=circle label=\"\" width=0.2 style=filled fillcolor=black color=black];" << std::endl;
 
-fprintf(stream,"\tNOSTATE->%s [label=\"%s\", color=black, fontname=arial, fontsize=10,  arrowhead=vee];\n", (const char*)theStatechart.GetInitialState().c_str(), (const char*)theStatechart.GetDiagramLabel().c_str());
+stream << "\tNOSTATE->"
+	<< theStatechart.GetInitialState().c_str()
+	<< " [label=\""
+	<< theStatechart.GetDiagramLabel().c_str()
+	<< "\", color=black, fontname=arial, fontsize=10,  arrowhead=vee];"
+	<< std::endl;
 
 AdeElementIterator it;
 for (it = theStatechart.begin(); it != theStatechart.end(); ++it)
 {
-	AdeModelElement* aElement = it.CreateNewElement();
-	if ((aElement->GetType() & ITEM_TYPE_MASK) == ITEM_IS_STATE)
+	AdeModelElement* anElement = it.CreateNewElement();
+	if ((anElement->GetType() & ITEM_TYPE_MASK) == ITEM_IS_STATE)
 	{
-		AdeState* aState = static_cast<AdeState*>(aElement);
-		fprintf(stream,"\t%s [label=\"{%s}\", shape=Mrecord, color=darkviolet, fontname=arial, fontsize=12];\n", (const char*)aState->GetName().c_str(), (const char*)aState->GetDiagramLabel().c_str());
+		AdeState* aState = dynamic_cast<AdeState*>(anElement);
+		stream << "\t"
+			<< aState->GetName().c_str()
+			<< " [label=\"{"
+			<< aState->GetDiagramLabel().c_str()
+			<< "}\", shape=Mrecord, color=darkviolet, fontname=arial, fontsize=12];"
+			<< std::endl;
 
 		AdeElementIterator it2;
 		for (it2 = aState->begin(); it2 != aState->end(); ++it2)
 		{
-			AdeModelElement* aElement2 = it2.CreateNewElement();
-			if ((aElement2->GetType() & ITEM_TYPE_MASK) == ITEM_IS_TRANSITION)
+			AdeModelElement* anElement2 = it2.CreateNewElement();
+			if ((anElement2->GetType() & ITEM_TYPE_MASK) == ITEM_IS_TRANSITION)
 			{
-				AdeTransition* aTransition = static_cast<AdeTransition*>(aElement2);
-				if (aTransition->IsNormalTransition())
-				{
-					if (!aTransition->GetDestination().empty())
-						fprintf(stream,"\t%s->%s [label=\"%s\", color=black, fontname=arial, fontsize=10,  arrowhead=vee];\n", (const char*)aState->GetName().c_str(), (const char*)aTransition->GetDestination().c_str(), (const char*)aTransition->GetDiagramLabel().c_str());
-				}
-				if (aTransition->IsSelfTransition())
-				{
-					fprintf(stream,"\t%s->%s [label=\"%s\", color=black, fontname=arial, fontsize=10,  arrowhead=vee];\n", (const char*)aState->GetName().c_str(), (const char*)aState->GetName().c_str(), (const char*)aTransition->GetDiagramLabel().c_str());
-				}
+				AdeTransition* aTransition = dynamic_cast<AdeTransition*>(anElement2);
+				if (aTransition->IsNormalTransition() &&
+					!aTransition->GetDestination().empty())
+					stream << "\t"
+						<< aState->GetName().c_str()
+						<< "->"
+						<< aTransition->GetDestination().c_str()
+						<< " [label=\""
+						<< aTransition->GetDiagramLabel().c_str()
+						<< "\", color=black, fontname=arial, fontsize=10,  arrowhead=vee];"
+						<< std::endl;
+				else if (aTransition->IsSelfTransition())
+					stream << "\t"
+						<< aState->GetName().c_str()
+						<< "->"
+						<< aState->GetName().c_str()
+						<< " [label=\""
+						<< aTransition->GetDiagramLabel().c_str()
+						<< "\", color=black, fontname=arial, fontsize=10,  arrowhead=vee];"
+						<< std::endl;
 			}
-			delete aElement2;
+			delete anElement2;
 		}
 	}
-	delete aElement;
+	delete anElement;
 }
 
-fprintf(stream,"}\n");
+stream << "}" << std::endl;
+
+return stream;
