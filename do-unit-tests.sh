48b081545156b9838cc7899b70e118035852eec0
Anders Larsen <al@alarsen.net>
2011-03-31 12:12:19 +0000

    Inadvertently renamed the top-level unit test directory...
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@2353 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- do-unit-tests.sh
+++ do-unit-tests.sh
@@ -7,80 +7,80 @@ ASTADEGENERATE="components_WGNBOFKH/Component_XZAWFDAN/Config_ZUJGZNUN/AstadeGen
 echo "**********************************"
 echo "	generate ACF_test"
 echo "**********************************"
-${ASTADEGENERATE} -S components_WGNBOFKH/Component_CBEUPFRX/Config_EAEBAKKD/StateChartCoder -X -c Components_UnitTests/Component_ACF_test/
+${ASTADEGENERATE} -S components_WGNBOFKH/Component_CBEUPFRX/Config_EAEBAKKD/StateChartCoder -X -c Components_Astade_Unit_Tests/Component_ACF_test/
 echo "**********************************"
 echo "	build ACF_test"
 echo "**********************************"
-make -j 2 -C Components_UnitTests/Component_ACF_test/Config_Linux/ TARGET=ACF_test $1
+make -j 2 -C Components_Astade_Unit_Tests/Component_ACF_test/Config_Linux/ TARGET=ACF_test $1
 echo "**********************************"
 echo "	generate ACF_test++"
 echo "**********************************"
-${ASTADEGENERATE} -S components_WGNBOFKH/Component_StateChartCoderACF++/Config_Linux_Debug/StateChartCoder -X -c Components_UnitTests/Component_ACF_test++/
+${ASTADEGENERATE} -S components_WGNBOFKH/Component_StateChartCoderACF++/Config_Linux_Debug/StateChartCoder -X -c Components_Astade_Unit_Tests/Component_ACF_test++/
 echo "**********************************"
 echo "	build ACF_test++"
 echo "**********************************"
-make -j 2 -C Components_UnitTests/Component_ACF_test++/Config_Linux/ TARGET=ACF_test++ $1
+make -j 2 -C Components_Astade_Unit_Tests/Component_ACF_test++/Config_Linux/ TARGET=ACF_test++ $1
 echo "**********************************"
 echo "	generate crossroad"
 echo "**********************************"
-${ASTADEGENERATE} -S components_WGNBOFKH/Component_StateChartCoderACF++/Config_Linux_Debug/StateChartCoder -X -c Components_UnitTests/Component_crossroad/
+${ASTADEGENERATE} -S components_WGNBOFKH/Component_StateChartCoderACF++/Config_Linux_Debug/StateChartCoder -X -c Components_Astade_Unit_Tests/Component_crossroad/
 echo "**********************************"
 echo "	build crossroad"
 echo "**********************************"
-make -j 2 -C Components_UnitTests/Component_crossroad/Config_Linux/ TARGET=crossroad $1
+make -j 2 -C Components_Astade_Unit_Tests/Component_crossroad/Config_Linux/ TARGET=crossroad $1
 echo "**********************************"
 echo "	generate ACF_Multithread"
 echo "**********************************"
-${ASTADEGENERATE} -S components_WGNBOFKH/Component_StateChartCoderACF++/Config_Linux_Debug/StateChartCoder -X -c Components_UnitTests/Component_ACF_Multithread/
+${ASTADEGENERATE} -S components_WGNBOFKH/Component_StateChartCoderACF++/Config_Linux_Debug/StateChartCoder -X -c Components_Astade_Unit_Tests/Component_ACF_Multithread/
 echo "**********************************"
 echo "	build ACF_Multithread"
 echo "**********************************"
-make -j 2 -C Components_UnitTests/Component_ACF_Multithread/Config_Linux/ TARGET=ACF_Multithread $1
+make -j 2 -C Components_Astade_Unit_Tests/Component_ACF_Multithread/Config_Linux/ TARGET=ACF_Multithread $1
 echo "**********************************"
 echo "	generate GraphicDrawer"
 echo "**********************************"
-${ASTADEGENERATE} -X -c Components_UnitTests/Component_GraphicDrawer/
+${ASTADEGENERATE} -X -c Components_Astade_Unit_Tests/Component_GraphicDrawer/
 echo "**********************************"
 echo "	build GraphicDrawer"
 echo "**********************************"
-make -j 2 -C Components_UnitTests/Component_GraphicDrawer/Config_Linux_Debug/ TARGET=GraphicDrawer $1
+make -j 2 -C Components_Astade_Unit_Tests/Component_GraphicDrawer/Config_Linux_Debug/ TARGET=GraphicDrawer $1
 echo "**********************************"
 echo "	generate HalloC"
 echo "**********************************"
-${ASTADEGENERATE} -S components_WGNBOFKH/Component_FOGYFECV/Config_HEYGDBJI/StateChartCoder -X -c Components_UnitTests/Component_HalloC/
+${ASTADEGENERATE} -S components_WGNBOFKH/Component_FOGYFECV/Config_HEYGDBJI/StateChartCoder -X -c Components_Astade_Unit_Tests/Component_HalloC/
 echo "**********************************"
 echo "	build HalloC"
 echo "**********************************"
-make -j 2 -C Components_UnitTests/Component_HalloC/Config_Linux/ TARGET=HalloC $1
+make -j 2 -C Components_Astade_Unit_Tests/Component_HalloC/Config_Linux/ TARGET=HalloC $1
 echo "**********************************"
 echo "	generate ReverseEngineer"
 echo "**********************************"
-${ASTADEGENERATE} -X -c Components_UnitTests/Component_ReverseEngineer/
+${ASTADEGENERATE} -X -c Components_Astade_Unit_Tests/Component_ReverseEngineer/
 echo "**********************************"
 echo "	build ReverseEngineer"
 echo "**********************************"
-make -j 2 -C Components_UnitTests/Component_ReverseEngineer/Config_Linux/ TARGET=ReverseEngineer $1
+make -j 2 -C Components_Astade_Unit_Tests/Component_ReverseEngineer/Config_Linux/ TARGET=ReverseEngineer $1
 echo "**********************************"
 echo "	generate RevisionControl"
 echo "**********************************"
-${ASTADEGENERATE} -X -c Components_UnitTests/Component_RevisionControl/
+${ASTADEGENERATE} -X -c Components_Astade_Unit_Tests/Component_RevisionControl/
 echo "**********************************"
 echo "	build RevisionControl"
 echo "**********************************"
-make -j 2 -C Components_UnitTests/Component_RevisionControl/Config_Linux_Debug/ TARGET=RevisionControl $1
+make -j 2 -C Components_Astade_Unit_Tests/Component_RevisionControl/Config_Linux_Debug/ TARGET=RevisionControl $1
 echo "**********************************"
 echo "	generate social"
 echo "**********************************"
-${ASTADEGENERATE} -X -c Components_UnitTests/Component_social/
+${ASTADEGENERATE} -X -c Components_Astade_Unit_Tests/Component_social/
 echo "**********************************"
 echo "	build social"
 echo "**********************************"
-make -j 2 -C Components_UnitTests/Component_social/Config_Linux_Debug/ TARGET=social $1
+make -j 2 -C Components_Astade_Unit_Tests/Component_social/Config_Linux_Debug/ TARGET=social $1
 echo "**********************************"
 echo "	generate Staircase"
 echo "**********************************"
-${ASTADEGENERATE} -S components_WGNBOFKH/Component_StateChartCoderWx/Config_Linux_Debug/StateChartCoder -X -c Components_UnitTests/Component_Staircase/
+${ASTADEGENERATE} -S components_WGNBOFKH/Component_StateChartCoderWx/Config_Linux_Debug/StateChartCoder -X -c Components_Astade_Unit_Tests/Component_Staircase/
 echo "**********************************"
 echo "	build Staircase"
 echo "**********************************"
-make -j 2 -C Components_UnitTests/Component_Staircase/Config_Linux_Release/ TARGET=Staircase $1
+make -j 2 -C Components_Astade_Unit_Tests/Component_Staircase/Config_Linux_Release/ TARGET=Staircase $1
