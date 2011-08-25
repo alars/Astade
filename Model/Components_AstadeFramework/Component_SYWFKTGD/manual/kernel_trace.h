d726d1c8b45367ab74eb93d5b6d36e114c39c278
Thomas Spitzer <projektleiter@astade.de>
2011-08-25 09:48:03 +0000

    some name changes
    git-svn-id: http://astade.tigris.org/svn/astade/trunk@2380 aaa30fad-34f9-0310-8aa9-877cc762ee2e

--- Model/Components_AstadeFramework/Component_SYWFKTGD/manual/kernel_trace.h
+++ Model/Components_AstadeFramework/Component_SYWFKTGD/manual/kernel_trace.h
@@ -1,5 +1,5 @@
-#ifndef HBM_KERNEL_TRACE_H
-#define HBM_KERNEL_TRACE_H
+#ifndef KERNEL_TRACE_H
+#define KERNEL_TRACE_H
 
 extern void trcDriver_notify_function_call(int traceLevel, char* objectName, char* functionName, void* objectID);
 extern void trcDriver_notifyReturn(void);
@@ -7,13 +7,9 @@ extern void trcDriver_notify_constructor(int traceLevel, char* objectName, void*
 extern void trcDriver_notify_destructor(int traceLevel, char* objectName, void* objectID);
 
 #define NOTIFY_FUNCTION_CALL(a,b,c,d,e,f) trcDriver_notify_function_call(b,c,d,a);
-
 #define RETURN(a) do {trcDriver_notifyReturn(); return(a);} while(0);
-
 #define voidRETURN do {trcDriver_notifyReturn(); return;} while(0);
-
 #define NOTIFY_CONSTRUCTOR(a,b,c) trcDriver_notify_constructor(a,b,me);
-
 #define NOTIFY_DESTRUCTOR(a,b) trcDriver_notify_destructor(a,b,me);
 
 #endif
