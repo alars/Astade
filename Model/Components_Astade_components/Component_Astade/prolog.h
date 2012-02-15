d917f422df694c870c3ea3bf0cdb4e3393140ffd
Anders Larsen <al@alarsen.net>
2012-02-15 21:01:20 +0100

    Updated the copyright notices to 2012

--- Model/Components_Astade_components/Component_Astade/prolog.h
+++ Model/Components_Astade_components/Component_Astade/prolog.h
@@ -1,6 +1,6 @@
 /*
  * Astade is a UML tool for C++
- * Copyright (C) 2005-2011 Thomas Spitzer, Anders Larsen et.al.
+ * Copyright (C) 2005-2012 Thomas Spitzer, Anders Larsen et.al.
  *
  * Astade is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
