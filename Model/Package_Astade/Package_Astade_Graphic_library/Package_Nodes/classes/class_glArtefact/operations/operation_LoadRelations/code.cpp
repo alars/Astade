e82bd9dcdbe5ae68af1b771d26cc2df28767fcb3
Thomas Spitzer <projektleiter@astade.de>
2012-02-23 09:34:15 +0100

    Artefact dependencies are load and saved

--- Model/Package_Astade/Package_Astade_Graphic_library/Package_Nodes/classes/class_glArtefact/operations/operation_LoadRelations/code.cpp
+++ Model/Package_Astade/Package_Astade_Graphic_library/Package_Nodes/classes/class_glArtefact/operations/operation_LoadRelations/code.cpp
@@ -18,3 +18,20 @@ while (configObject.Exists(useName))
     count++;
     useName.Printf(wxS("CreateArtefact%03d"), count);
 }
+
+count = 1;
+useName.Printf(wxS("ArtefactDependency%03d"), count);
+
+while (configObject.Exists(useName))
+{
+    int UseID;
+    configObject.Read(useName, &UseID);
+    
+    glNode* partnerNode = glNode::getNodeById(UseID);
+    
+    if (partnerNode)
+        new glArtefactDependency(myParent, *this, *partnerNode);
+    
+    count++;
+    useName.Printf(wxS("ArtefactDependency%03d"), count);
+}
--- Model/Package_Astade/Package_Astade_Graphic_library/Package_Nodes/classes/class_glArtefact/operations/operation_Save/code.cpp
+++ Model/Package_Astade/Package_Astade_Graphic_library/Package_Nodes/classes/class_glArtefact/operations/operation_Save/code.cpp
@@ -5,6 +5,7 @@ configObject.Write(wxS("Label"), myLabel);
 configObject.Write(wxS("ClassName"), wxS("artefact"));
 
 int artefactCount = 0;
+int dependencyCount = 0;
 
 for (std::set<glEdge*>::iterator it = myEdges.begin(); it != myEdges.end(); ++it)
     if (&(*it)->GetStartNode() == this)
@@ -16,5 +17,14 @@ for (std::set<glEdge*>::iterator it = myEdges.begin(); it != myEdges.end(); ++it
             wxString aString;
             aString.Printf(wxS("CreateArtefact%03d"), ++artefactCount);
             configObject.Write(aString, anArtefact->GetEndNode().id);
+        }
+        
+        glArtefactDependency* aDependency = dynamic_cast<glArtefactDependency*>(*it);
+       
+        if (aDependency)
+        {
+            wxString aString;
+            aString.Printf(wxS("ArtefactDependency%03d"), ++dependencyCount);
+            configObject.Write(aString, aDependency->GetEndNode().id);
         }
     }
