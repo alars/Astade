058096dfb4f3931a7e8c377f27096e0325cad67c
Jonatan Antoni <jonatan@familie-antoni.de>
2012-12-21 17:08:27 +0100

    Enhanced Ubuntu packaging scripts to support multiple distri versions and patch levels.

--- Source/Packages/ubuntu/create.sh
+++ Source/Packages/ubuntu/create.sh
@@ -1,35 +1,39 @@
 #!/bin/bash
+#
+#
 
 DIRNAME=$(dirname $0)
-MAINDIST="precise"
-DIST="precise"
 CHANGELOG="debian/changelog"
-CHANGELOG_NAME="Astade Developers"
-CHANGELOG_EMAIL="dev@astade.tigris.org"
 
+# include version info
 source version
 
+# include command line options
 source ${DIRNAME}/getopts.sh
 
-if [[ $DIST == $MAINDIST ]]; then
-        SUFFIX=""
-else
-        SUFFIX="~${DIST}1"
-fi
+# include distribution info
+source ${DIRNAME}/dist.sh
 
+# clean up
+[ -d astade-${VERSION} ] && rm -rf astade-${VERSION}
+[ -f astade_${VERSION}.orig.tar.gz ] && rm astade_${VERSION}.orig.tar.gz
+
+# unpack and prepare
 tar xzf astade_${VERSION}.tar.gz
 ln -s astade_${VERSION}.tar.gz astade_${VERSION}.orig.tar.gz
 cp -r Source/Packages/ubuntu/${DIST}/debian astade-${VERSION}/
 
 pushd astade-${VERSION}
 
-echo  > ${CHANGELOG} "astade (${VERSION}-0ubuntu1${SUFFIX}) ${DIST}; urgency=low"
+# create changelog
+echo  > ${CHANGELOG} "astade (${VERSION}-0ubuntu${PATCHLEVEL}${SUFFIX}) ${DIST}; urgency=low"
 echo >> ${CHANGELOG}
 echo >> ${CHANGELOG} "  * The full changelog can be found at git hub."
 echo >> ${CHANGELOG}
 echo >> ${CHANGELOG} " -- ${CHANGELOG_NAME} <${CHANGELOG_EMAIL}>  $(date -R)"
 echo >> ${CHANGELOG}
 
+# build source package
 debuild -S
 
 popd
--- Source/Packages/ubuntu/publish.sh
+++ Source/Packages/ubuntu/publish.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
+#
+#
 
 DIRNAME=$(dirname $0)
-MAINDIST="precise"
-DIST="precise"
 PPA_DAILY="astade/daily"
 PPA_RELEASE="astade/release"
 DAILYEXP='^([0-9]+\.){3}[0-9]+$'
 
+# include version info
 source version
 
+# include command line options
 source ${DIRNAME}/getopts.sh
 
-if [[ $DIST == $MAINDIST ]]; then
-        SUFFIX=""
-else
-        SUFFIX="~${DIST}1"
-fi
+# include distribution info
+source ${DIRNAME}/dist.sh
 
+# if version is in form
+# major.minor.patch.change~commit
+# (e.g. 1.1.5.1-g3cf0abb)
+# upload to daily ppa
 if [[ $VERSION =~ $DAILYEXP ]]; then
     PPA=${PPA_DAILY}
 else
     PPA=${PPA_RELEASE}
 fi
 
-dput ppa:${PPA} astade_${VERSION}-0ubuntu1${SUFFIX}_source.changes
+# upload
+dput ppa:${PPA} astade_${VERSION}-0ubuntu${PATCHLEVEL}${SUFFIX}_source.changes
 
+exit 0
