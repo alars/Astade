328e8d9c4b896e299aae35352cde74db34c9bd4f
Jonatan Antoni <jonatan@familie-antoni.de>
2012-12-29 21:41:59 +0100

    Fixed ubuntu publish script daily regex.

--- Source/Packages/ubuntu/publish.sh
+++ Source/Packages/ubuntu/publish.sh
@@ -5,7 +5,7 @@
 DIRNAME=$(dirname $0)
 PPA_DAILY="astade/daily"
 PPA_RELEASE="astade/release"
-DAILYEXP='^([0-9]+\.){3}[0-9]+$'
+DAILYEXP='^([0-9]+\.){3}[0-9]+~[a-z0-9]{8}'
 
 # include version info
 source version
@@ -18,7 +18,7 @@ source ${DIRNAME}/dist.sh
 
 # if version is in form
 # major.minor.patch.change~commit
-# (e.g. 1.1.5.1-g3cf0abb)
+# (e.g. 1.1.5.1~g3cf0abb)
 # upload to daily ppa
 if [[ $VERSION =~ $DAILYEXP ]]; then
     PPA=${PPA_DAILY}
