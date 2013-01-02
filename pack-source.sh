c5fc826d61456adf1d49d156a6546266176b44b7
Anders Larsen <al@alarsen.net>
2013-01-02 17:49:34 +0100

    pack-source.sh: set Trace2UMLVersion = AstadeVersion

--- pack-source.sh
+++ pack-source.sh
@@ -3,6 +3,7 @@
 VERSION=$(git describe 2> /dev/null | sed "s/^release//" | sed "s/^v//" | sed "s/-\([0-9]*\)-\(g[0-9a-f]*\)/.\1~\2/")
 
 echo "#define AstadeVersion \"${VERSION}\"" > Model/Components_Astade_components/Component_Astade/manual/AstadeVersion.h
+echo "#define Trace2UMLVersion \"${VERSION}\"" > Model/Components_Trace2UML_components/Component_Trace2UML/manual/Trace2UMLVersion.h
 
 find -L ${PWD}/{Model,Source/{AstadeView,Icons,icons_png,Templates,freedesktop},*.sh,AstadeBuildserver.iss,*.txt} \
 	\( -name .svn -o -name html \) -prune -o \
