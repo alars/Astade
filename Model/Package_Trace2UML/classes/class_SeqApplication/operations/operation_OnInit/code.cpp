aaa54bf8733940c71f09eb7e70257298172b20fa
Thomas Spitzer <projektleiter@astade.de>
2013-04-04 12:05:41 +0200

    Reenable the -o command line option (got lost, for whatever reason)

--- Model/Package_Trace2UML/classes/class_SeqApplication/operations/operation_OnInit/code.cpp
+++ Model/Package_Trace2UML/classes/class_SeqApplication/operations/operation_OnInit/code.cpp
@@ -4,6 +4,7 @@ wxCmdLineParser aParser(argc, argv);
 aParser.AddSwitch(wxS("h"), wxS("help"), wxS("shows this help"), wxCMD_LINE_OPTION_HELP);
 aParser.AddOption(wxS("T"), wxS("type"), wxS("Has no effect, only for compatibility with mscgen."), wxCMD_LINE_VAL_STRING, wxCMD_LINE_PARAM_OPTIONAL);
 aParser.AddOption(wxS("i"), wxS("infile"), wxS("Input file."), wxCMD_LINE_VAL_STRING, wxCMD_LINE_PARAM_OPTIONAL);
+aParser.AddOption(wxS("o"), wxS("outfile"), wxS("The file the graphic is written to (PNG format). If this parameter is not given, the software starts in dialogue mode."), wxCMD_LINE_VAL_STRING, wxCMD_LINE_PARAM_OPTIONAL);
 aParser.AddParam(wxS("Input file"), wxCMD_LINE_VAL_STRING, wxCMD_LINE_PARAM_OPTIONAL);
 
 if (aParser.Parse() != 0)
