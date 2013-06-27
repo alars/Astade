3a51053f72cc1dd029dceb3cd1a931b5d019929e
Thomas-Astade <projectleiter@astade.de>
2013-06-27 08:46:03 +0200

    fix in CSDGenerator. Adapted to newer dot versions

--- Model/Package_Astade/Package_Astade_Suite/Package_Diagram_generators/classes/class_CSDgenerator/operations/operation_CodeCapsuledObjects/code.cpp
+++ Model/Package_Astade/Package_Astade_Suite/Package_Diagram_generators/classes/class_CSDgenerator/operations/operation_CodeCapsuledObjects/code.cpp
@@ -54,7 +54,7 @@ if (theConnections)
 		wxASSERT_MSG(aConnection, wxS("the connections folder should only contain connections"));
 
         std::cout << aConnection->GetOutputObject().utf8_str() << " -> "
-                  << aConnection->GetInputObject().utf8_str() << "[fontname = arial, fontsize=8, arrowhead=obox, arrowtail=obox, "
+                  << aConnection->GetInputObject().utf8_str() << "[fontname = arial, fontsize=8, arrowhead=obox, arrowtail=obox, dir=both, "
                   << "taillabel=\"" << aConnection->GetOutputPort().utf8_str() << "\", "
                   << "headlabel=\"" << aConnection->GetInputPort().utf8_str() << "\"]" << std::endl;
 
