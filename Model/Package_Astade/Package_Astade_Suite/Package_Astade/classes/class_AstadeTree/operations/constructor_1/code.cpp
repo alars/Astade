ed64c8020f335fa70a87bcd1b7091323949250f1
Thomas Spitzer <projectleiter@astade.de>
2013-10-16 17:50:00 +0200

    increment the save version to 5

--- Model/Package_Astade/Package_Astade_Suite/Package_Astade/classes/class_AstadeTree/operations/constructor_1/code.cpp
+++ Model/Package_Astade/Package_Astade_Suite/Package_Astade/classes/class_AstadeTree/operations/constructor_1/code.cpp
@@ -20,7 +20,7 @@ AdeModel* aModel = new AdeModel(modelDir);
 // Check whether the model is newer (modified by a newer version of Astade)
 int saveVersion = aModel->GetSaveVersion();
 
-if (saveVersion > 4) // Model is not compatible with this Astade version
+if (saveVersion > 5) // Model is not compatible with this Astade version
 {
     if (wxMessageBox(wxS("This model seems to have been modified by a newer version of Astade. If you continue, things might not work propperly.\nYou had better get the newest version at http://www.astade.de\n\nDo you really want to try working with this Astade version (on your own risk)?"),
             wxS("Model has newer Version!"), wxICON_QUESTION  | wxYES_NO) == wxNO)
@@ -30,9 +30,9 @@ if (saveVersion > 4) // Model is not compatible with this Astade version
     }
 }
 
-if (saveVersion < 4) // if we continue here, we have at least save version 3
+if (saveVersion < 5) 
 {
-    aModel->SetSaveVersion(4);
+    aModel->SetSaveVersion(5);
 }
 
 SetItemData(myRootItem, AstadeTreeItemBase::CreateNewElement(aModel));
