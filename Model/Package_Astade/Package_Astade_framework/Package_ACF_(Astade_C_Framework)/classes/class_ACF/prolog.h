6747b8146d2e7e9766754a77cd0e137f8b1b236d
Thomas Spitzer <projectleiter@astade.de>
2013-11-13 08:10:59 +0100

    fixed a unit test

--- Model/Package_Astade/Package_Astade_framework/Package_ACF_(Astade_C_Framework)/classes/class_ACF/prolog.h
+++ Model/Package_Astade/Package_Astade_framework/Package_ACF_(Astade_C_Framework)/classes/class_ACF/prolog.h
@@ -1,3 +1,6 @@
 //declare the macros for the port concept
 #define DECLARE_PORT(port) ACF_MessageReceiver* port;
-#define CONNECT_PORTS(inObject, inPort, outObject, outPort) outPort = (ACF_MessageReceiver*)&inObject; inPort = (ACF_MessageReceiver*)&outObject;
+#define CONNECT_CPORTS(inObject, inPort, outObject, outPort) outPort = (ACF_MessageReceiver*)&inObject; inPort = (ACF_MessageReceiver*)&outObject;
+#define CONNECT_PORTS(inObject, inPort, outObject, outPort) outPort = inObject.MessageReceiver(); inPort = outObject.MessageReceiver();
+#define CONNECT_CPORT(inObject, outObject, outPort) outPort = (ACF_MessageReceiver*)&inObject;
+#define CONNECT_PORT(inObject, outObject, outPort) outPort = inObject.MessageReceiver();
