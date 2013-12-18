9f2cc56b793dd9e130c080383cfe99375cbde466
Thomas Spitzer <projectleiter@astade.de>
2013-12-18 13:14:34 +0100

    formate the output

--- Model/Package_CZZVHXSO/classes/class_YVFTTPCE/operations/operation_OKXQUVKV/code.cpp
+++ Model/Package_CZZVHXSO/classes/class_YVFTTPCE/operations/operation_OKXQUVKV/code.cpp
@@ -1,2 +1,13 @@
 //~~ void generateNode(wxConfigBase& config, const wxString& nodename) [CMain] ~~
-std::cout << "\tnode " << nodename.utf8_str() << ";" << std::endl;
+std::cout << "\t" << nodename.utf8_str() << "[";
+
+wxString ClassName = config.Read(wxS("ClassName"));
+
+if (ClassName == wxS("usecase"))
+    std::cout << "shape=oval, style=filled, fillcolor=grey, ";
+
+wxString label = config.Read(wxS("Label"));
+label.Replace(wxS("\""),wxS("\\\""));
+std::cout << "label=\"" << label.utf8_str() << "\"]";
+
+std::cout << ";" << std::endl;
