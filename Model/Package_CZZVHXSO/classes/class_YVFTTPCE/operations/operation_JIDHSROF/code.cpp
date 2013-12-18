edc1d031c52724a5c2212090cd322364f979ac55
Thomas Spitzer <projectleiter@astade.de>
2013-12-18 14:28:17 +0100

    another node and edge

---
+++ Model/Package_CZZVHXSO/classes/class_YVFTTPCE/operations/operation_JIDHSROF/code.cpp
@@ -0,0 +1,20 @@
+//~~ void generateAttach(wxConfigBase& config, const wxString& nodename) [CMain] ~~
+uint num = 1;
+wxString name;
+name.Printf(wxS("Attach%03d"),num);
+
+while (config.Exists(name))
+{
+    int dest;
+    config.Read(name,&dest,0);
+    if (dest > 0)
+    {
+        wxString destname;
+        destname.Printf(wxS("Node%03d"),dest);
+        std::cout << "\t" << nodename.utf8_str() << " -> " << destname.utf8_str() << "[";
+        std::cout << "color=black, arrowhead=none, style=dashed";
+        std::cout << "];" << std::endl;
+    }
+    num++;
+    name.Printf(wxS("Attach%03d"),num);
+}
--- Model/Package_CZZVHXSO/classes/class_YVFTTPCE/operations/operation_OKXQUVKV/code.cpp
+++ Model/Package_CZZVHXSO/classes/class_YVFTTPCE/operations/operation_OKXQUVKV/code.cpp
@@ -9,6 +9,9 @@ if (ClassName == wxS("usecase"))
 if (ClassName == wxS("actor"))
     std::cout << "shape=pentagon, color=red, style=filled, fillcolor=grey, ";
 
+if (ClassName == wxS("note"))
+    std::cout << "shape=note, color=green, style=filled, fillcolor=grey, ";
+
 wxString label = config.Read(wxS("Label"));
 label.Replace(wxS("\""),wxS("\\\""));
 std::cout << "label=\"" << optimizedString(label).utf8_str() << "\"]";
@@ -19,3 +22,4 @@ generateGeneralisations(config,nodename);
 generateExtend(config,nodename);
 generateInclude(config,nodename);
 generateAssociation(config,nodename);
+generateAttach(config,nodename);
