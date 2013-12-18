e7c59e19390f60b2509dd615cfe4fd3482b6365d
Thomas Spitzer <projectleiter@astade.de>
2013-12-18 14:43:03 +0100

    new nodes and edges

--- Model/Package_CZZVHXSO/classes/class_YVFTTPCE/operations/operation_OKXQUVKV/code.cpp
+++ Model/Package_CZZVHXSO/classes/class_YVFTTPCE/operations/operation_OKXQUVKV/code.cpp
@@ -12,6 +12,9 @@ if (ClassName == wxS("actor"))
 if (ClassName == wxS("note"))
     std::cout << "shape=note, color=green, style=filled, fillcolor=grey, ";
 
+if (ClassName == wxS("component"))
+    std::cout << "shape=component, color=blue, ";
+
 wxString label = config.Read(wxS("Label"));
 label.Replace(wxS("\""),wxS("\\\""));
 std::cout << "label=\"" << optimizedString(label).utf8_str() << "\"]";
@@ -23,3 +26,4 @@ generateExtend(config,nodename);
 generateInclude(config,nodename);
 generateAssociation(config,nodename);
 generateAttach(config,nodename);
+generateCUse(config,nodename);
---
+++ Model/Package_CZZVHXSO/classes/class_YVFTTPCE/operations/operation_WATQEPVT/code.cpp
@@ -0,0 +1,20 @@
+//~~ void generateCUse(wxConfigBase& config, const wxString& nodename) [CMain] ~~
+uint num = 1;
+wxString name;
+name.Printf(wxS("ComponentUse%03d"),num);
+
+while (config.Exists(name))
+{
+    int dest;
+    config.Read(name,&dest,0);
+    if (dest > 0)
+    {
+        wxString destname;
+        destname.Printf(wxS("Node%03d"),dest);
+        std::cout << "\t" << nodename.utf8_str() << " -> " << destname.utf8_str() << "[";
+        std::cout << "color=blue, style=dashed, label=\"<<use>>\", arrowhead=vee";
+        std::cout << "];" << std::endl;
+    }
+    num++;
+    name.Printf(wxS("ComponentUse%03d"),num);
+}
