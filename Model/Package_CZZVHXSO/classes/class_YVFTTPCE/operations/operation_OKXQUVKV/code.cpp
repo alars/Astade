e05736f14f2e63dc9947b20ab3d9d0968d8d0e85
Thomas Spitzer <projectleiter@astade.de>
2013-12-18 16:18:31 +0100

    finished the converter

--- Model/Package_CZZVHXSO/classes/class_YVFTTPCE/operations/operation_OKXQUVKV/code.cpp
+++ Model/Package_CZZVHXSO/classes/class_YVFTTPCE/operations/operation_OKXQUVKV/code.cpp
@@ -41,3 +41,6 @@ generateAttach(config,nodename);
 generateCUse(config,nodename);
 generateIUse(config,nodename);
 generateProvide(config,nodename);
+generateCreate(config,nodename);
+generateComposition(config,nodename);
+
