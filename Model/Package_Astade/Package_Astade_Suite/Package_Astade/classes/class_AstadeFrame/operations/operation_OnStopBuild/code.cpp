9761cb683133a287053bdab514b0cc82a3c72ea1
Thomas Spitzer <projectleiter@astade.de>
2014-01-16 16:54:22 +0100

    if you switch tracing on or off, the class is touched, so it is recompiled the next build

--- Model/Package_Astade/Package_Astade_Suite/Package_Astade/classes/class_AstadeFrame/operations/operation_OnStopBuild/code.cpp
+++ Model/Package_Astade/Package_Astade_Suite/Package_Astade/classes/class_AstadeFrame/operations/operation_OnStopBuild/code.cpp
@@ -1,3 +1,4 @@
+//~~ void OnStopBuild(wxCommandEvent& event) [AstadeFrame] ~~
 #ifdef __WXMSW__
 // under Windows, Processes are created independent from their childs
 // so if we want to kill them we have to do this seperately and bloody...
--- Model/Package_Astade/Package_Astade_Suite/Package_Astade/classes/class_AstadeFrame/operations/operation_OnTraceOnOff/code.cpp
+++ Model/Package_Astade/Package_Astade_Suite/Package_Astade/classes/class_AstadeFrame/operations/operation_OnTraceOnOff/code.cpp
@@ -4,7 +4,10 @@ AdeModelElement* element = myTree->GetItem(anId);
 AdeClass* aClass = dynamic_cast<AdeClass*>(element);
 
 if (aClass!=NULL)
+{
     aClass->SetIsTraced(event.GetId() == ID_TRACE_ON);
-    
+    aClass->Touch();
+}
+
 myTree->UpdateItem(anId);
 myTree->UpdateAll(myTree->GetRootItem());
