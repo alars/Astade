276feccb1980787029c6e9c93f349268f007b25f
Thomas Spitzer <t.spitzer@consulting.astade.de>
2014-01-18 10:15:21 +0100

    fixed a drawing error in Trace2UML. After a take event calls from Global where drawn with a false origin.

--- Model/Package_Trace2UML/classes/class_SeqDataBase/operations/operation_runningObject/code.cpp
+++ Model/Package_Trace2UML/classes/class_SeqDataBase/operations/operation_runningObject/code.cpp
@@ -1,12 +1,17 @@
 //~~ int runningObject(const wxString& threadID) [SeqDataBase] ~~
+int level = 0;
 for (std::vector<SeqEvent>::reverse_iterator it = itsEvents.rbegin(); it != itsEvents.rend(); it++)
 {
     if ((*it).threadID == threadID)
     {
         if (((*it).eventID == ID_CALL) || ((*it).eventID == ID_GLOBALCALL) || ((*it).eventID == ID_SELFCALL))
-            return (*it).destinationObject;
+        {
+            if (level <= 0)
+                return (*it).destinationObject;
+            level--;
+        }
         if (((*it).eventID == ID_RETURN) || ((*it).eventID == ID_GLOBALRETURN) || ((*it).eventID == ID_SELFRETURN))
-            return (*it).sourceObject;
+            level++;
     }
 }
 return wxNOT_FOUND;
