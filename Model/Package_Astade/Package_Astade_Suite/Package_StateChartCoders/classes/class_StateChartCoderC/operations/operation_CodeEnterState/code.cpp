c382923f5c9dc2f2d035faf51a16c8046f5452ca
Thomas Spitzer <projectleiter@astade.de>
2014-02-13 10:30:18 +0100

    add a macro to the generated StatechartCoderC code to notify state

--- Model/Package_Astade/Package_Astade_Suite/Package_StateChartCoders/classes/class_StateChartCoderC/operations/operation_CodeEnterState/code.cpp
+++ Model/Package_Astade/Package_Astade_Suite/Package_StateChartCoders/classes/class_StateChartCoderC/operations/operation_CodeEnterState/code.cpp
@@ -1,86 +1,90 @@
 //~~ void CodeEnterState(AdeState& theState) [StateChartCoderC] ~~
 
 spec << "//! @brief This is the enter function for state "
-	<< theState.GetName().utf8_str()
-	<< "."
-	<< std::endl;
+    << theState.GetName().utf8_str()
+    << "."
+    << std::endl;
 spec << "void "
-	<< myAdeStatechart->GetName().utf8_str()
-	<< "_Enter_"
-	<< theState.GetName().utf8_str()
-	<< "("
-	<< myAdeStatechart->GetName().utf8_str()
-	<< "* me, const "
-	<< myAdeStatechart->GetEventType().utf8_str()
-	<< "* theEvent);\n"
-	<< std::endl;
+    << myAdeStatechart->GetName().utf8_str()
+    << "_Enter_"
+    << theState.GetName().utf8_str()
+    << "("
+    << myAdeStatechart->GetName().utf8_str()
+    << "* me, const "
+    << myAdeStatechart->GetEventType().utf8_str()
+    << "* theEvent);\n"
+    << std::endl;
 
 impl << "void "
-	<< myAdeStatechart->GetName().utf8_str()
-	<< "_Enter_"
-	<< theState.GetName().utf8_str()
-	<< "("
-	<< myAdeStatechart->GetName().utf8_str()
-	<< "* me, const "
-	<< myAdeStatechart->GetEventType().utf8_str()
-	<< "* theEvent)\n{"
-	<< std::endl;
+    << myAdeStatechart->GetName().utf8_str()
+    << "_Enter_"
+    << theState.GetName().utf8_str()
+    << "("
+    << myAdeStatechart->GetName().utf8_str()
+    << "* me, const "
+    << myAdeStatechart->GetEventType().utf8_str()
+    << "* theEvent)\n{"
+    << std::endl;
 
 wxString EntryAction = theState.GetEntryAction();
 if (!EntryAction.empty())
 {
-	impl << "\t// Call Entry Action." << std::endl;
-	impl << "\t"
-		<< myAdeStatechart->GetName().utf8_str()
-		<< "_impl_"
-		<< EntryAction.utf8_str()
-		<< "(me->myHandler, theEvent);"
-		<< std::endl;
+    impl << "\t// Call Entry Action." << std::endl;
+    impl << "\t"
+        << myAdeStatechart->GetName().utf8_str()
+        << "_impl_"
+        << EntryAction.utf8_str()
+        << "(me->myHandler, theEvent);"
+        << std::endl;
 }
 
 wxString aTimeout = theState.GetTimeout();
 if (!aTimeout.empty())
 {
-	impl << "\t// Start Timer." << std::endl;
-	impl << "\t"
-		<< myAdeStatechart->GetName().utf8_str()
-		<< "_impl_StartTimer(me->myHandler, "
-		<< aTimeout.utf8_str()
-		<< ");"
-		<< std::endl;
+    impl << "\t// Start Timer." << std::endl;
+    impl << "\t"
+        << myAdeStatechart->GetName().utf8_str()
+        << "_impl_StartTimer(me->myHandler, "
+        << aTimeout.utf8_str()
+        << ");"
+        << std::endl;
 }
 
 impl << "\t//Set the new state." << std::endl;
 impl << "\tme->theState = &"
-	<< myAdeStatechart->GetName().utf8_str()
-	<< "_"
-	<< theState.GetName().utf8_str()
-	<< ";"
-	<< std::endl;
+    << myAdeStatechart->GetName().utf8_str()
+    << "_"
+    << theState.GetName().utf8_str()
+    << ";"
+    << std::endl;
+
+impl << "\t#ifdef NOTIFY_STATE" << std::endl;
+impl << "\t\tNOTIFY_STATE(me, \"" << myAdeStatechart->GetName().utf8_str() << "\", \"" << theState.GetName().utf8_str() << "\")" << std::endl;
+impl << "\t#endif" << std::endl;
 
 AdeElementIterator it;
 for (it = theState.begin(); it != theState.end(); ++it)
 {
-	AdeModelElement* anElement = it.CreateNewElement();
-	if ((anElement->GetType() & ITEM_TYPE_MASK) == ITEM_IS_TRANSITION)
-	{
-		AdeTransition* aTransition = static_cast<AdeTransition*>(anElement);
-		if (!aTransition->GetGuard().empty())
-			CodeEventlessTransition(theState, *aTransition);
-	}
-	delete anElement;
+    AdeModelElement* anElement = it.CreateNewElement();
+    if ((anElement->GetType() & ITEM_TYPE_MASK) == ITEM_IS_TRANSITION)
+    {
+        AdeTransition* aTransition = static_cast<AdeTransition*>(anElement);
+        if (!aTransition->GetGuard().empty())
+            CodeEventlessTransition(theState, *aTransition);
+    }
+    delete anElement;
 }
 
 for (it = theState.begin(); it != theState.end(); ++it)
 {
-	AdeModelElement* anElement = it.CreateNewElement();
-	if ((anElement->GetType() & ITEM_TYPE_MASK) == ITEM_IS_TRANSITION)
-	{
-		AdeTransition* aTransition = static_cast<AdeTransition*>(anElement);
-		if (aTransition->GetGuard().empty())
-			CodeEventlessTransition(theState, *aTransition);
-	}
-	delete anElement;
+    AdeModelElement* anElement = it.CreateNewElement();
+    if ((anElement->GetType() & ITEM_TYPE_MASK) == ITEM_IS_TRANSITION)
+    {
+        AdeTransition* aTransition = static_cast<AdeTransition*>(anElement);
+        if (aTransition->GetGuard().empty())
+            CodeEventlessTransition(theState, *aTransition);
+    }
+    delete anElement;
 }
 
 impl << "\tme->nextState = 0; // We stay in this state" << std::endl;
