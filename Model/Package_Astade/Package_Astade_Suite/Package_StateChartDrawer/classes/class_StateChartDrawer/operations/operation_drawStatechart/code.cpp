b93a00f110b33f5326e63b6bfb9f96ab3b9ce889
Thomas Spitzer <projectleiter@astade.de>
2014-02-18 12:51:41 +0100

    changed the statechart drawer to draw decitions correct

--- Model/Package_Astade/Package_Astade_Suite/Package_StateChartDrawer/classes/class_StateChartDrawer/operations/operation_drawStatechart/code.cpp
+++ Model/Package_Astade/Package_Astade_Suite/Package_StateChartDrawer/classes/class_StateChartDrawer/operations/operation_drawStatechart/code.cpp
@@ -8,71 +8,78 @@ stream << "\tlabel = \"" << theStatechart.GetName().utf8_str() << "\\n" << theSt
 stream << "\tNOSTATE [shape=circle label=\"\" width=0.2 style=filled fillcolor=black color=black];" << std::endl;
 
 stream << "\tNOSTATE->"
-	<< theStatechart.GetInitialState().utf8_str()
-	<< " [label=\""
-	<< theStatechart.GetDiagramLabel().utf8_str()
-	<< "\", color=black, fontname=arial, fontsize=10,  arrowhead=vee];"
-	<< std::endl;
+    << theStatechart.GetInitialState().utf8_str()
+    << " [label=\""
+    << theStatechart.GetDiagramLabel().utf8_str()
+    << "\", color=black, fontname=arial, fontsize=10,  arrowhead=vee];"
+    << std::endl;
 
 bool haveTerminate = false;
 
 AdeElementIterator it;
 for (it = theStatechart.begin(); it != theStatechart.end(); ++it)
 {
-	AdeModelElement* anElement = it.CreateNewElement();
-	if ((anElement->GetType() & ITEM_TYPE_MASK) == ITEM_IS_STATE)
-	{
-		AdeState* aState = dynamic_cast<AdeState*>(anElement);
-		stream << "\t"
-			<< aState->GetName().utf8_str()
-			<< " [label=\"{"
-			<< aState->GetDiagramLabel().utf8_str()
-			<< "}\", shape=Mrecord, color=darkviolet, fontname=arial, fontsize=12];"
-			<< std::endl;
+    AdeModelElement* anElement = it.CreateNewElement();
+    if ((anElement->GetType() & ITEM_TYPE_MASK) == ITEM_IS_STATE)
+    {
+        AdeState* aState = dynamic_cast<AdeState*>(anElement);
+        stream << "\t"
+            << aState->GetName().utf8_str();
 
-		AdeElementIterator it2;
-		for (it2 = aState->begin(); it2 != aState->end(); ++it2)
-		{
-			AdeModelElement* anElement2 = it2.CreateNewElement();
-			if ((anElement2->GetType() & ITEM_TYPE_MASK) == ITEM_IS_TRANSITION)
-			{
-				AdeTransition* aTransition = dynamic_cast<AdeTransition*>(anElement2);
-				if (aTransition->IsNormalTransition() &&
-					!aTransition->GetDestination().empty())
-					stream << "\t"
-						<< aState->GetName().utf8_str()
-						<< "->"
-						<< aTransition->GetDestination().utf8_str()
-						<< " [label=\""
-						<< aTransition->GetDiagramLabel().utf8_str()
-						<< "\", color=black, fontname=arial, fontsize=10,  arrowhead=vee];"
-						<< std::endl;
-				else if (aTransition->IsSelfTransition())
-					stream << "\t"
-						<< aState->GetName().utf8_str()
-						<< "->"
-						<< aState->GetName().utf8_str()
-						<< " [label=\""
-						<< aTransition->GetDiagramLabel().utf8_str()
-						<< "\", color=black, fontname=arial, fontsize=10,  arrowhead=vee];"
-						<< std::endl;
-				else if (aTransition->IsTerminateTransition())
+            if (!aState->isIf())
+                stream << " [label=\"{"
+                    << aState->GetDiagramLabel().utf8_str()
+                    << "}\", shape=Mrecord, color=darkviolet, fontname=arial, fontsize=12];";
+            else
+                stream << " [label=\""
+                    << aState->GetDiagramLabel().utf8_str()
+                    << "\", shape=diamond, color=blue, fontname=arial, fontsize=10];";
+
+            stream << std::endl;
+
+        AdeElementIterator it2;
+        for (it2 = aState->begin(); it2 != aState->end(); ++it2)
+        {
+            AdeModelElement* anElement2 = it2.CreateNewElement();
+            if ((anElement2->GetType() & ITEM_TYPE_MASK) == ITEM_IS_TRANSITION)
+            {
+                AdeTransition* aTransition = dynamic_cast<AdeTransition*>(anElement2);
+                if (aTransition->IsNormalTransition() &&
+                    !aTransition->GetDestination().empty())
+                    stream << "\t"
+                        << aState->GetName().utf8_str()
+                        << "->"
+                        << aTransition->GetDestination().utf8_str()
+                        << " [label=\""
+                        << aTransition->GetDiagramLabel().utf8_str()
+                        << "\", color=black, fontname=arial, fontsize=10,  arrowhead=vee];"
+                        << std::endl;
+                else if (aTransition->IsSelfTransition())
+                    stream << "\t"
+                        << aState->GetName().utf8_str()
+                        << "->"
+                        << aState->GetName().utf8_str()
+                        << " [label=\""
+                        << aTransition->GetDiagramLabel().utf8_str()
+                        << "\", color=black, fontname=arial, fontsize=10,  arrowhead=vee];"
+                        << std::endl;
+                else if (aTransition->IsTerminateTransition())
                 {
                     haveTerminate = true;
-					stream << "\t"
-						<< aState->GetName().utf8_str()
-						<< "->"
-						<< "TERMINATESTATE"
-						<< " [label=\""
-						<< aTransition->GetDiagramLabel().utf8_str()
-						<< "\", color=black, fontname=arial, fontsize=10,  arrowhead=vee];"
-						<< std::endl;
+                    stream << "\t"
+                        << aState->GetName().utf8_str()
+                        << "->"
+                        << "TERMINATESTATE"
+                        << " [label=\""
+                        << aTransition->GetDiagramLabel().utf8_str()
+                        << "\", color=black, fontname=arial, fontsize=10,  arrowhead=vee];"
+                        << std::endl;
                 }
-			}
-			delete anElement2;
-		}
-	}
-	delete anElement;
+            }
+            delete anElement2;
+        }
+    }
+    delete anElement;
 }
 
 if (haveTerminate)
