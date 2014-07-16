e0e91abfdfaa31f666bd5e77dd52351a01a49638
thomas <thomas@L049E025.(none)>
2014-07-16 11:51:53 +0200

    add the parameter parser for the command line tool AstadeConvert

--- Model/Components_Helper_components/Component_QZFMBFUY/manual/main.cpp
+++ Model/Components_Helper_components/Component_QZFMBFUY/manual/main.cpp
@@ -1,4 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+
+void print_usage()
+{
+    printf("\nAstadeConvert: utility to convert Astade models from directory structer to DSL formate\n");
+    printf("Usage: AstadeConvert -m <modeldir>\n");
+    printf("\nOptions:\n");
+    printf("--help              Display this information\n");
+    printf("-m, --model         converts the selected model.\n");
+}
+
 int main(int argc, char **argv)
 {
-    return 0;
+    static struct option long_options[] = {
+        {"help"       ,  no_argument,        0,  'h' },
+        {"model"      ,  required_argument,  0,  'm' },
+        {0,          0,                      0,   0  }
+    };
+    
+    int long_index =0;
+    int opt;
+    
+    while ((opt = getopt_long(argc, argv, "hm:", long_options, &long_index )) != -1) {
+        switch (opt) {
+         case 'h':
+            print_usage();
+            exit(EXIT_SUCCESS);
+            break;
+        default:
+            print_usage();
+            exit (EXIT_FAILURE);
+            break;
+        }
+    }
+    return EXIT_SUCCESS;
 }
