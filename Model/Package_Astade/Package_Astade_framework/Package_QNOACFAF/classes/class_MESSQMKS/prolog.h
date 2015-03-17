a78fbb4b896fc00b24f3746203978bd8365422f7
Thomas Spitzer <projektleiter@astade.de>
2015-03-17 14:49:48 +0100

    add another connect macro

--- Model/Package_Astade/Package_Astade_framework/Package_QNOACFAF/classes/class_MESSQMKS/prolog.h
+++ Model/Package_Astade/Package_Astade_framework/Package_QNOACFAF/classes/class_MESSQMKS/prolog.h
@@ -3,3 +3,4 @@
 #define DECLARE_PORT(p) public: AQF_MessageReceiver* p;
 
 #define CONNECT_PORTS(inObject, inPort, outObject, outPort) outPort = &inObject; inPort = &outObject;
+#define CONNECT_PORT(inObject, outObject, outPort) outPort = &inObject;
