5683d68b7ef71ba015ba5515788517b7066e94ac
Thomas Spitzer <projektleiter@astade.de>
2015-03-18 16:16:01 +0100

    bugfix in trace filter

--- Model/Package_Astade/Package_Astade_framework/Package_AQF/classes/class_AQF_MessageReceiver/operations/operation_sendMessage/code.cpp
+++ Model/Package_Astade/Package_Astade_framework/Package_AQF/classes/class_AQF_MessageReceiver/operations/operation_sendMessage/code.cpp
@@ -1,13 +1,13 @@
 //~~ void sendMessage(AQF_MessageReceiver* port, const AQF_Message& event) [AQF_MessageReceiver] ~~
-if (m_tracefile)
+if (port->m_tracefile)
 {
-    if (_semaphore)
-        while (sem_wait(_semaphore));
-    *m_tracefile << (void*)this << ":" << getName(this) << " >-- "
+    if (port->_semaphore)
+        while (sem_wait(port->_semaphore));
+    *port->m_tracefile << (void*)this << ":" << getName(this) << " >-- "
                  << (void*)port << ":" << getName(port) << " "
                  << event.getID() << std::endl;
-    if (_semaphore)
-        sem_post(_semaphore);
+    if (port->_semaphore)
+        sem_post(port->_semaphore);
 }
 QMetaObject::invokeMethod(port, "TakeEventSlot", Qt::QueuedConnection,
                            Q_ARG(void*, this),
