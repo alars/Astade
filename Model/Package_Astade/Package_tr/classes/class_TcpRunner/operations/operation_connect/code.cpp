6f0ff35d90ad0742252f9686188d7b617fc75278
Thomas Spitzer <projektleiter@astade.de>
2015-04-13 09:37:52 +0200

    use gethostbyname

--- Model/Package_Astade/Package_tr/classes/class_TcpRunner/operations/operation_connect/code.cpp
+++ Model/Package_Astade/Package_tr/classes/class_TcpRunner/operations/operation_connect/code.cpp
@@ -6,27 +6,23 @@ if (mFD < 0) {
     exit(1);
 }
 
-#if 0
 struct hostent *hoststruct;     /* host information */
-
-struct in_addr h_addr;          /* internet address */
 
 if ((hoststruct = gethostbyname(host)) == NULL) {
   fprintf(stderr, "(mini) nslookup failed on '%s'\n", host);
   exit(2);
 }
-#endif
 
 struct sockaddr_in server;
 
 /* Construct the server sockaddr_in structure */
 memset(&server, 0, sizeof(server));           /* Clear struct */
 server.sin_family = AF_INET;                  /* Internet/IP */
-server.sin_addr.s_addr = inet_addr(host);     /* IP address */
+memcpy(&server.sin_addr.s_addr,hoststruct->h_addr_list[0],sizeof(server.sin_addr.s_addr));     /* IP address */
 server.sin_port = htons(port);                /* server port */
 
 /* Establish connection */
 if (::connect(mFD, (struct sockaddr *) &server, sizeof(server)) < 0) {
   fprintf(stderr, "Failed to connect to server:'%s', port:%d\n",host,port);
-  exit(2);
+  exit(3);
 }
