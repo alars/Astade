1b8afc6a0bf39a83f37de92cf8c4922f3c58d965
Anders Larsen <al@alarsen.net>
2015-04-23 11:07:02 +0200

    tr::RunnerInterface.run() : cleanup indentation, remove redundant ';'

--- Model/Package_Astade/Package_tr/classes/class_RunnerInterface/operations/operation_run/code.cpp
+++ Model/Package_Astade/Package_tr/classes/class_RunnerInterface/operations/operation_run/code.cpp
@@ -1,41 +1,38 @@
 //~~ void run() [RunnerInterface] ~~
+
 if (mReportFile)
-    (*mReportFile) << "<ActionList>" << std::endl;
-    
-while (currentSection->hasLines()) 
+    *mReportFile << "<ActionList>" << std::endl;
+
+while (currentSection->hasLines())
 {
-        int stillToWait = 1000*3600*24;
-        unsigned int timeout = currentSection->getTimeout();
-        if (timeout > 0)
-        {
-            unsigned int tick = getTimeTick();
-            unsigned int alreadyWait = tick-lastTriggerTime;
-            stillToWait = timeout - alreadyWait;
-        }
-        
-        if (stillToWait>0)
-        {
-            while(currentSection->checkTrigger(inputBuffer))
-                lastTriggerTime=getTimeTick();
-            ;
-            
-            if (!currentSection->hasLines())
-                currentSection->nextSection();
-            else
-            {
-  
-                    inputBuffer += readChar(stillToWait);
-            }
+    int stillToWait = 1000*3600*24;
+    unsigned int timeout = currentSection->getTimeout();
+    if (timeout > 0)
+    {
+        unsigned int tick = getTimeTick();
+        unsigned int alreadyWait = tick-lastTriggerTime;
+        stillToWait = timeout - alreadyWait;
+    }
 
-            while(currentSection->checkTrigger(inputBuffer))
-                lastTriggerTime=getTimeTick();
-            ;
+    if (stillToWait > 0)
+    {
+        while (currentSection->checkTrigger(inputBuffer))
+            lastTriggerTime = getTimeTick();
 
-        } else {
-            currentSection->checkTimeout(inputBuffer,true);
-            lastTriggerTime=getTimeTick();
-        }
-};
+        if (!currentSection->hasLines())
+            currentSection->nextSection();
+        else
+            inputBuffer += readChar(stillToWait);
+
+        while (currentSection->checkTrigger(inputBuffer))
+            lastTriggerTime = getTimeTick();
+    }
+    else
+    {
+        currentSection->checkTimeout(inputBuffer,true);
+        lastTriggerTime=getTimeTick();
+    }
+}
 
 if (mReportFile)
-    (*mReportFile) << "</ActionList>" << std::endl;
+    *mReportFile << "</ActionList>" << std::endl;
