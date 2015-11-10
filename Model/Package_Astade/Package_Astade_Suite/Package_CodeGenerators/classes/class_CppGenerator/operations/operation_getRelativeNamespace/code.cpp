55982bbcf9b015faa4d15cc367b72e5fcc5c6aeb
thomas <t.spitzer@astade.de>
2015-11-10 12:42:58 +0100

    fixed an assert in wxWidgets 3.0

--- Model/Package_Astade/Package_Astade_Suite/Package_CodeGenerators/classes/class_CppGenerator/operations/operation_getRelativeNamespace/code.cpp
+++ Model/Package_Astade/Package_Astade_Suite/Package_CodeGenerators/classes/class_CppGenerator/operations/operation_getRelativeNamespace/code.cpp
@@ -6,5 +6,6 @@ unsigned int ix;
 for (ix = 0; ix < source_space.GetCount() && ix < target_space.GetCount(); ++ix)
 	if (source_space[ix] != target_space[ix])
 		break;
-target_space.RemoveAt(0, ix);
+if (ix > 0)
+    target_space.RemoveAt(0, ix);
 return target_space;
