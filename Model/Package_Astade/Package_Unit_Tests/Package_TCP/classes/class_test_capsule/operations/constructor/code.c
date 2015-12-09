3830b8417465031012414954a86adf806466e458
Thomas Spitzer <t.spitzer@astade.de>
2015-12-09 11:11:12 +0100

    made some port connections

--- Model/Package_Astade/Package_Unit_Tests/Package_TCP/classes/class_test_capsule/operations/constructor/code.c
+++ Model/Package_Astade/Package_Unit_Tests/Package_TCP/classes/class_test_capsule/operations/constructor/code.c
@@ -1,3 +1,6 @@
 //~~ test_capsule(ACF* anACF) [test_capsule] ~~
-test_receiver_impl_Constructor(&me->my_test_receiver_impl, anACF);
-test_sender_impl_Constructor(&me->my_test_sender_impl, anACF);
+tcp_impl_Constructor(&me->my_tcp_receiver, anACF);
+tcp_impl_Constructor(&me->my_tcp_sender, anACF);
+
+test_receiver_impl_Constructor(&me->my_test_receiver, anACF);
+test_sender_impl_Constructor(&me->my_test_sender, anACF);
--- Model/Package_Astade/Package_Unit_Tests/Package_TCP/classes/class_test_capsule/operations/operation_Initialize/code.c
+++ Model/Package_Astade/Package_Unit_Tests/Package_TCP/classes/class_test_capsule/operations/operation_Initialize/code.c
@@ -1,3 +1,6 @@
 //~~ void Initialize() [test_capsule] ~~
-test_receiver_Initialize(&me->my_test_receiver_impl.test_receiver_base,0);
-test_sender_Initialize(&me->my_test_sender_impl.test_sender_base,0);
+tcp_Initialize(&me->my_tcp_receiver.tcp_base,0);
+tcp_Initialize(&me->my_tcp_sender.tcp_base,0);
+
+test_receiver_Initialize(&me->my_test_receiver.test_receiver_base,0);
+test_sender_Initialize(&me->my_test_sender.test_sender_base,0);
