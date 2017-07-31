91222c71875850e5f43da42d206e8f3560bad988
Anders Larsen <al@alarsen.net>
2017-07-31 14:19:59 +0200

    Code "IsIn"-functions for ACF (C) state-charts as "static inline"

--- Model/Package_Astade/Package_Astade_Suite/Package_StateChartCoders/classes/class_StateChartCoderACF/operations/operation_CodeIsInStateFunction/code.cpp
+++ Model/Package_Astade/Package_Astade_Suite/Package_StateChartCoders/classes/class_StateChartCoderACF/operations/operation_CodeIsInStateFunction/code.cpp
@@ -6,7 +6,7 @@ spec << "//! @brief This checks if the machine is in state \""
 	<< std::endl;
 spec << "//! @param me A pointer to the statechart instance." << std::endl;
 spec << "//! @return is in state." << std::endl;
-spec << "inline int "
+spec << "static inline int "
 	<< myAdeStatechart->GetName().utf8_str()
 	<< "_IsIn"
 	<< theState.GetName().utf8_str()
