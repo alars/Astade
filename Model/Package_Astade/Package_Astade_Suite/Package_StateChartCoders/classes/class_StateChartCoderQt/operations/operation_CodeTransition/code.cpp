fe43a3a2bb10ecfd8828513c7db54354f28e8edb
Thomas Spitzer <t.spitzer@consulting.astade.de>
2018-07-31 11:48:09 +0200

    bugfix in the exti action for At statecharts

--- Model/Package_Astade/Package_Astade_Suite/Package_StateChartCoders/classes/class_StateChartCoderQt/operations/operation_CodeTransition/code.cpp
+++ Model/Package_Astade/Package_Astade_Suite/Package_StateChartCoders/classes/class_StateChartCoderQt/operations/operation_CodeTransition/code.cpp
@@ -47,7 +47,7 @@ if (!theTransition.IsInternalTransition())
 		impl << "\t\t// exit action" << std::endl;
 		impl << "\t\t"
 			<< theState.GetExitAction().utf8_str()
-			<< "(theEvent);"
+			<< "(port, theEvent);"
 			<< std::endl;
 	}
 	if ((!theState.GetTimeout().empty()) && (event != wxS("AQF_timeout")))
