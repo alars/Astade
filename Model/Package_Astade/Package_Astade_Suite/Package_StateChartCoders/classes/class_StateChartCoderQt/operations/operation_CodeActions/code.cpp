37483916b18eaf2b62cd75a157d8fef5a29c4863
Thomas Spitzer <thomassp@external.miltenyibiotec.de>
2019-05-09 10:08:06 +0200

    add some docu generation

--- Model/Package_Astade/Package_Astade_Suite/Package_StateChartCoders/classes/class_StateChartCoderQt/operations/operation_CodeActions/code.cpp
+++ Model/Package_Astade/Package_Astade_Suite/Package_StateChartCoders/classes/class_StateChartCoderQt/operations/operation_CodeActions/code.cpp
@@ -5,9 +5,14 @@ spec << "\t//************* actions **************************" << std::endl;
 std::set<wxString> aSet = myAdeStatechart->GetActions();
 
 for (std::set<wxString>::iterator iter = aSet.begin(); iter != aSet.end(); ++iter)
+{
+    spec << "\t/// @brief virtual action which has to be overload by the implementer of the state chart." << std::endl;
+    spec << "\t/// @param port The machine which send this message." << std::endl;
+    spec << "\t/// @param theEvent The event passed to the actions and guards." << std::endl;
 	spec << "\tvirtual void "
 		<< iter->utf8_str()
 		<< "(const AQF_MessageReceiver* port, const "
 		<< myAdeStatechart->GetEventType().utf8_str()
 		<< "& theEvent) = 0;"
 		<< std::endl;
+}
--- Model/Package_Astade/Package_Astade_Suite/Package_StateChartCoders/classes/class_StateChartCoderQt/operations/operation_CodeGuards/code.cpp
+++ Model/Package_Astade/Package_Astade_Suite/Package_StateChartCoders/classes/class_StateChartCoderQt/operations/operation_CodeGuards/code.cpp
@@ -5,9 +5,15 @@ spec << "\t//************* guards **************************" << std::endl;
 std::set<wxString> aSet = myAdeStatechart->GetGuards();
 
 for (std::set<wxString>::iterator iter = aSet.begin(); iter != aSet.end(); ++iter)
+{
+    spec << "\t/// @brief virtual guard which has to be overload by the implementer of the state chart." << std::endl;
+    spec << "\t/// @param port The machine which send this message." << std::endl;
+    spec << "\t/// @param theEvent The event passed to the actions and guards." << std::endl;
+    spec << "\t/// @return true, if the guard is true." << std::endl;
 	spec << "\tvirtual bool "
 		<< iter->utf8_str()
 		<< "(const AQF_MessageReceiver* port, const "
 		<< myAdeStatechart->GetEventType().utf8_str()
 		<< "& theEvent) = 0;"
 		<< std::endl;
+}
