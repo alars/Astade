c5cb0f0c7ebfd1a72451a58f3681c7982f66f075
Anders Larsen <al@alarsen.net>
2021-01-06 11:01:04 +0100

    Bump the copyright year to 2020

--- Model/Package_Trace2UML/classes/class_SeqFrame/operations/operation_OnAbout/code.cpp
+++ Model/Package_Trace2UML/classes/class_SeqFrame/operations/operation_OnAbout/code.cpp
@@ -3,7 +3,7 @@
 wxString message = wxS( \
 "    Trace2UML is a program to convert tracefiles (text format) to UML diagrams.\n" \
 "\n" \
-"    Copyright (C) 2005-2019 Thomas Spitzer et al.\n\n" \
+"    Copyright (C) 2005-2020 Thomas Spitzer et al.\n\n" \
 "    To contact the author look at the project homepage,\n" \
 "        http://www.astade.de/\n" \
 "    or mail to: dev@astade.tigris.org\n" \
@@ -300,7 +300,7 @@ wxString message = wxS( \
 "		     END OF TERMS AND CONDITIONS\n");
 
 
-wxDialog aDialog(this, -1, wxS("Trace2UML - Copyright (C) 2005-2019 Thomas Spitzer et al - Release " Trace2UMLVersion), wxDefaultPosition, wxDefaultSize, wxRESIZE_BORDER | wxDEFAULT_DIALOG_STYLE);
+wxDialog aDialog(this, -1, wxS("Trace2UML - Copyright (C) 2005-2020 Thomas Spitzer et al - Release " Trace2UMLVersion), wxDefaultPosition, wxDefaultSize, wxRESIZE_BORDER | wxDEFAULT_DIALOG_STYLE);
 wxTextCtrl aText(&aDialog, -1, message, wxDefaultPosition, wxDefaultSize, wxTE_MULTILINE | wxTE_READONLY);
 aDialog.SetSize(-1, -1, 650, 400);
 aDialog.Center();
