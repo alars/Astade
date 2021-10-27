c2a6e64138e15a11ce5ff85d01ea9239882de48f
Thomas Spitzer <t.spitzer@astade.de>
2021-10-27 09:31:37 +0200

    fixed deprecated script command

--- pack-source.sh
+++ pack-source.sh
@@ -8,7 +8,7 @@ echo "#define Trace2UMLVersion \"${VERSION}\"" > Model/Components_Trace2UML_comp
 find -L ${PWD}/{Model,Source/{AstadeView,Icons,icons_png,Templates,freedesktop},*.sh,AstadeBuildserver.iss,*.txt} \
 	\( -name .svn -o -name html \) -prune -o \
 	-type f \! -name ".*" \! -name "*.[ao]" -a \
-	\( -name "*.py" -o -name "*.sh" -o \! -perm +111 \) -print \
+	\( -name "*.py" -o -name "*.sh" -o \! -perm /111 \) -print \
 	|pax -ws,^${PWD},astade-${VERSION}, | gzip > astade_${VERSION}.tar.gz
 
 zip -qrj Source/Templates/ACF.zip Model/Components_AstadeFramework/Component_ACF/ -i '*.c' -i '*.h'
