ac7ee4921f9704c6b54bdb8212e62aeaad04fbe7
Thomas Spitzer <t.spitzer@astade.de>
2022-03-28 12:49:25 +0200

    use case nodes a drawn correct

---
+++ Model/Package_Astade/Package_Astade_Graphic_library/Package_Nodes/classes/class_glUsecase/operations/operation_CCTYVODZ/code.cpp
@@ -0,0 +1,17 @@
+//~~ void SaveDrawIo(std::ofstream& cr) [glUsecase] ~~
+
+cr << "        <mxCell id=\"Node:"
+   << id
+   << "\" value=\""
+   << myLabel
+   << "\" style=\"ellipse;whiteSpace=wrap;html=1;fillColor=#E6E6E6;\" vertex=\"1\" parent=\"1\">\n";
+cr << "          <mxGeometry x=\""
+   << absGetDrawPosition().xCoord() - my_XRadius
+   << "\" y=\""
+   << absGetDrawPosition().yCoord() - my_YRadius
+   << "\" width=\""
+   << 2 * my_XRadius
+   << "\" height=\""
+   << 2 * my_YRadius
+   << "\" as=\"geometry\" />\n";
+cr << "        </mxCell>\n";
